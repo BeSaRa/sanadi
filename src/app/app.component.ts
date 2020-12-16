6bb8ef06a6443ebcc8aeae32c5aa0c734ff13482
BeSaRa <besara@windowslive.com>
2020-12-16 16:35:13 +0300

    add language changer for development purpose.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sanadi';
+  availableLanguages = ['ar', 'en'];
+  directions = ['rtl', 'ltr'];
+
+  private static setLanguage(lang: string): void {
+    lang === 'en' ? AppComponent.setEnglishLang() : AppComponent.setArabicLang();
+  }
+
+  private static setEnglishLang(): void {
+    const html = document.querySelector('html') as HTMLElement;
+    const style: HTMLLinkElement = document.querySelector('link[href="bootstrap-rtl.css"]') as HTMLLinkElement;
+    html.dir = 'ltr';
+    html.lang = 'en';
+    style.href = 'bootstrap.css';
+  }
+
+  private static setArabicLang(): void {
+    const html = document.querySelector('html') as HTMLElement;
+    const style: HTMLLinkElement = document.querySelector('link[href="bootstrap.css"]') as HTMLLinkElement;
+    html.dir = 'rtl';
+    html.lang = 'ar';
+    style.href = 'bootstrap-rtl.css';
+  }
+
+  @HostListener('window:keydown', ['$event'])
+  languageChangeDetection({ctrlKey, altKey, which, keyCode}: KeyboardEvent): void {
+    if ((keyCode === 76 || which === 76) && ctrlKey && altKey) {
+      const lang = document.querySelector('html')?.lang;
+      AppComponent.setLanguage(lang === 'en' ? 'ar' : 'en');
+    }
+  }
+
+
 }
