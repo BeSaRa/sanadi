d2dbbd10e023e79a89fba4e740b2d6d0c70a5056
BeSaRa <besara@windowslive.com>
2020-12-20 18:25:45 +0300

    finish the main design structure for the home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,52 @@
+import {Component, ElementRef, HostListener, OnInit, ViewChild} from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  // main navbar to use it later to attach/detach
+  @ViewChild('mainNavbar') element: ElementRef | undefined;
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  /**
+   * @description eventListener callback to listen to scroll event on the window.
+   */
+  @HostListener('window:scroll')
+  onScroll(): void {
+    const {pageYOffset: scroll} = window;
+    if (scroll >= 40) {
+      this._attachSticky();
+    } else {
+      this._detachSticky();
+    }
+  }
+
+  /**
+   * @description private method to attach "Sticky" class to main navbar.
+   * @private
+   */
+  private _attachSticky(): void {
+    if (!this.element?.nativeElement.classList.contains('sticky')) {
+      this.element?.nativeElement.classList.add('sticky');
+    }
+  }
+
+  /**
+   * @description private method to detach "Sticky" class from main navbar.
+   * @private
+   */
+  private _detachSticky(): void {
+    if (this.element?.nativeElement.classList.contains('sticky')) {
+      this.element?.nativeElement.classList.remove('sticky');
+    }
+  }
+
+}
