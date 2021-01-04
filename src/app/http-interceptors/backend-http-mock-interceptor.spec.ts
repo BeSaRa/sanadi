b0ebaef5cb6ae75e326df47c92e65a1ed6cc61d3
BeSaRa <besara@windowslive.com>
2021-01-04 19:01:39 +0300

    add httpInterceptors array to plug it later in AppModule

---
+++ src/app/http-interceptors/backend-http-mock-interceptor.spec.ts
@@ -0,0 +1,7 @@
+import { BackendHttpMockInterceptor } from './backend-http-mock-interceptor';
+
+describe('BackendHttpInterceptor', () => {
+  it('should create an instance', () => {
+    expect(new BackendHttpMockInterceptor()).toBeTruthy();
+  });
+});
