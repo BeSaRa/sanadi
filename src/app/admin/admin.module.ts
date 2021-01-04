c604e188c5de95979839e2b8849c5e6ada164f99
BeSaRa <besara@windowslive.com>
2021-01-04 19:07:10 +0300

    update admin-home.component to make list dynamic based on  ServiceItem model

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminHomeComponent } from './pages/admin-home/admin-home.component';
+import {SharedModule} from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [AdminHomeComponent],
-  imports: [
-    CommonModule,
-    AdminRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        AdminRoutingModule,
+        SharedModule
+    ]
 })
 export class AdminModule { }
--- src/app/admin/pages/admin-home/admin-home.component.ts
+++ src/app/admin/pages/admin-home/admin-home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ServiceItem} from '../../../shared/models/service-item';
 
 @Component({
   selector: 'app-admin-home',
@@ -6,8 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-home.component.scss']
 })
 export class AdminHomeComponent implements OnInit {
+  serviceList: ServiceItem[] = [
+    new ServiceItem(1, 'Aid', 'Aid', './aid', 'mdi-hand-heart'),
+    new ServiceItem(2, 'Organizations', 'Organizations', './organizations', 'mdi-office-building'),
+    new ServiceItem(3, 'Users', 'Users', './users', 'mdi-account-group'),
+    new ServiceItem(4, 'Localization', 'Localization', './localization', 'mdi-google-translate'),
+    new ServiceItem(4, 'Custom Role', 'Custom Role', './custom-role', 'mdi-card-account-details-star-outline'),
+  ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
