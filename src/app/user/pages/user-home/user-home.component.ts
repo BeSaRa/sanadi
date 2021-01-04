8aa5ad6dce4c9852cad26529defe2e49ce398616
BeSaRa <besara@windowslive.com>
2021-01-04 19:05:41 +0300

    update user-home.component to make list dynamic based on ServiceItem Model

--- src/app/user/pages/user-home/user-home.component.ts
+++ src/app/user/pages/user-home/user-home.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {ServiceItem} from '../../../shared/models/service-item';
 
 @Component({
   selector: 'app-user-home',
@@ -6,11 +7,19 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./user-home.component.scss']
 })
 export class UserHomeComponent implements OnInit {
+  serviceList: ServiceItem[] = [
+    new ServiceItem(2, 'Inquiry', 'Inquiry', './inquiry', 'mdi-database-search'),
+    new ServiceItem(1, 'Request', 'Request', './request', 'mdi-database-plus'),
+    new ServiceItem(3, 'Admin', 'Admin', '../admin', 'mdi-application-cog')
+  ];
 
   constructor() {
   }
 
   ngOnInit(): void {
+    setTimeout(() => {
+      console.log('this.serviceList[0].arName', this.serviceList[0].arName);
+    }, 5000);
   }
 
 }
