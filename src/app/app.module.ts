de0261ceabc3e564959a0fce08d0859289f1f7fb
BeSaRa <besara@windowslive.com>
2021-01-04 19:04:29 +0300

    add APP_INITIALIZER to AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-
+import {APP_INITIALIZER, NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {LoginComponent} from './pages/login/login.component';
 import {HomeComponent} from './pages/home/home.component';
 import {SharedModule} from './shared/shared.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {CommonModule} from '@angular/common';
 import {OverlayModule} from '@angular/cdk/overlay';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {forkJoin, Observable} from 'rxjs';
+import {IAppConfig} from './interfaces/i-app-config';
+import {ConfigurationService} from './services/configuration.service';
+import {UrlService} from './services/url.service';
+import {IAppUrls} from './interfaces/i-app-urls';
+import {httpInterceptors} from './http-interceptors/http-interceptors';
 
 @NgModule({
   declarations: [
@@ -17,16 +22,40 @@ import {OverlayModule} from '@angular/cdk/overlay';
     HomeComponent
   ],
   imports: [
-    CommonModule,
     BrowserModule,
     OverlayModule,
     SharedModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {provide: APP_INITIALIZER, useFactory: AppModule.AppInit, multi: true, deps: [HttpClient, ConfigurationService, UrlService]},
+    httpInterceptors
+  ],
   exports: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
+  static CONFIG_FILE = 'CONFIGURATION.json';
+  static URLS_FILE = 'URLS.json';
+  static http: HttpClient;
+
+  static AppInit(http: HttpClient, configurationService: ConfigurationService, urlService: UrlService): () => Promise<unknown> {
+    AppModule.http = http;
+    return () => {
+      return forkJoin({
+        urls: AppModule.loadResource<IAppUrls>(AppModule.URLS_FILE),
+        config: AppModule.loadResource<IAppConfig>(AppModule.CONFIG_FILE)
+      })
+        .toPromise().then((latest) => {
+          configurationService.setConfigurations(latest.config);
+          urlService.prepareUrls(latest.urls);
+        });
+    };
+  }
+
+  static loadResource<T>(fileName: string): Observable<T> {
+    return AppModule.http.get<T>(fileName);
+  }
 }
