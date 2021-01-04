96d6fda60a4140810b4ab5493b0d7e01dc0e3d44
BeSaRa <besara@windowslive.com>
2021-01-04 18:52:13 +0300

    add FactoryService to shared module

---
+++ src/app/services/factory.service.ts
@@ -0,0 +1,25 @@
+interface ServiceInterface<T> {
+  [index: string]: T;
+}
+
+export class FactoryService {
+  static services: ServiceInterface<any> = {};
+
+  static registerService<T>(serviceName: string, service: T): FactoryService {
+    if (!serviceName || serviceName.trim() === '') {
+      throw Error('Failed to register service');
+    }
+    if (serviceName[0] !== serviceName[0].toUpperCase()) {
+      throw Error('Service name should start with upper case');
+    }
+    FactoryService.services[serviceName] = service;
+    return FactoryService;
+  }
+
+  static getService<T>(serviceName: string): T {
+    if (!FactoryService.services.hasOwnProperty(serviceName)) {
+      throw Error('Service not register yet yet ');
+    }
+    return FactoryService.services[serviceName];
+  }
+}
