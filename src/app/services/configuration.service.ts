d4adeeea15eade6835af71744d9dc2ec7ab1e7d8
BeSaRa <besara@windowslive.com>
2021-01-04 18:53:33 +0300

    add configurationService

---
+++ src/app/services/configuration.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import {IAppConfig} from '../interfaces/i-app-config';
+import {FactoryService} from './factory.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConfigurationService {
+  public BASE_URL = '';
+  public CONFIG = {} as IAppConfig;
+
+  constructor() {
+    FactoryService.registerService('AppConfigurationService', this);
+  }
+
+  public setConfigurations(configurations: IAppConfig): ConfigurationService {
+    this.CONFIG = configurations;
+    this.prepareBaseUrl();
+    return this;
+  }
+
+  private prepareBaseUrl(): string {
+    if (
+      !this.CONFIG.hasOwnProperty('ENVIRONMENTS_URLS') ||
+      !this.CONFIG.ENVIRONMENTS_URLS.length
+    ) {
+      throw Error('There is no ENVIRONMENTS_URLS Property or empty provided inside app-configuration.json file Kindly check it');
+    }
+
+    if (typeof this.CONFIG.BASE_ENVIRONMENT_INDEX === 'undefined') {
+      throw Error('there is no BASE_ENVIRONMENT_INDEX provided inside app-configuration.json file');
+    }
+
+    if (typeof this.CONFIG.ENVIRONMENTS_URLS[this.CONFIG.BASE_ENVIRONMENT_INDEX] === 'undefined') {
+      throw Error('the provided BASE_ENVIRONMENT_INDEX not exists inside ENVIRONMENTS_URLS array in app-configuration.json file');
+    }
+    this.BASE_URL = this.CONFIG.ENVIRONMENTS_URLS[this.CONFIG.BASE_ENVIRONMENT_INDEX];
+    return this.BASE_URL;
+  }
+}
