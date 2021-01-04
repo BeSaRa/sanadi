4836ab628fcd15432da38ec6df108ab7700e1f90
BeSaRa <besara@windowslive.com>
2021-01-04 18:55:54 +0300

    add serviceItem model

---
+++ src/app/shared/models/service-item.spec.ts
@@ -0,0 +1,7 @@
+import { ServiceItem } from './service-item';
+
+describe('ServiceItem', () => {
+  it('should create an instance', () => {
+
+  });
+});
---
+++ src/app/shared/models/service-item.ts
@@ -0,0 +1,10 @@
+export class ServiceItem {
+  constructor(public id: number,
+              public arName: string,
+              public enName: string,
+              public route: string,
+              public icon: string
+  ) {
+
+  }
+}
