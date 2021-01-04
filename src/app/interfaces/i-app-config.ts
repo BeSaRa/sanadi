fd72d17f3bf9bcba60586bde317f2c07e63ff37f
BeSaRa <besara@windowslive.com>
2021-01-04 19:00:00 +0300

    add IAppConfig interface

---
+++ src/app/interfaces/i-app-config.ts
@@ -0,0 +1,8 @@
+export interface IAppConfig {
+  ENVIRONMENTS_URLS: string [];
+  BASE_ENVIRONMENT_INDEX: number;
+  BASE_URL: string;
+  TOKEN_HEADER_KEY: string;
+  TOKEN_STORE_KEY: string;
+  EXTERNAL_PROTOCOLS: string[];
+}
