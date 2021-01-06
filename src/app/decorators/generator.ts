ffd38925b9dc253081685b50344512a227d08879
BeSaRa <besara@windowslive.com>
2021-01-06 15:09:28 +0300

    add generator decorator.

---
+++ src/app/decorators/generator.ts
@@ -0,0 +1,41 @@
+import {map} from 'rxjs/operators';
+import {map as _map} from 'lodash';
+
+function _generateModel(data: any, model: any, property?: string, receiveCallback?: any): any {
+  let finalData;
+  if (!property) {
+    finalData = Object.assign(new model(), data);
+  } else {
+    finalData = Object.assign(new model(), data[property]);
+  }
+  return receiveCallback ? receiveCallback(finalData) : finalData;
+}
+
+function _generateCollection(collection: any, model: any, property?: string, receiveCallback?: any): any {
+  if (!property) {
+    // @ts-ignore
+    return _map(collection, item => _generateModel(item, model, null, receiveCallback));
+  } else {
+    // @ts-ignore
+    return _map(collection[property], item => _generateModel(item, model, null, receiveCallback));
+  }
+}
+
+// @ts-ignore
+export function Generator(model, isCollection = false, options?: { property?: string, interceptReceive?: any }): any {
+  // @ts-ignore
+  return (target, property, descriptor: PropertyDescriptor) => {
+    const original = descriptor.value;
+    // tslint:disable-next-line:only-arrow-functions
+    // @ts-ignore
+    descriptor.value = function(...args): any {
+      return original.apply(this, args).pipe(
+        map(data => isCollection
+          ? _generateCollection(data, model, options?.property, options?.interceptReceive)
+          : _generateModel(data, model, options?.property, options?.interceptReceive))
+      );
+    };
+  };
+}
+
+
