46810548c5383ca93c949bb2fed186f4acf4ea35
BeSaRa <besara@windowslive.com>
2021-01-07 19:07:43 +0300

    change type of direction inside Language Model

--- src/app/models/language.ts
+++ src/app/models/language.ts
@@ -1,6 +1,7 @@
 import {Styles} from '../enums/styles.enum';
+import {Direction} from '@angular/cdk/bidi';
 
 export class Language {
-  constructor(public  id: number, public  name: string, public code: string, public direction: string, public style: Styles) {
+  constructor(public  id: number, public  name: string, public code: string, public direction: Direction, public style: Styles) {
   }
 }
