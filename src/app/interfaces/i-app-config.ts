a8698ea1b9680f6c24fdf112b71473d28bc27f13
Navjot Singh <navjotsandha@gmail.com>
2021-01-18 16:17:04 +0300

    Update configuration

--- src/app/interfaces/i-app-config.ts
+++ src/app/interfaces/i-app-config.ts
@@ -2,6 +2,7 @@ export interface IAppConfig {
   ENVIRONMENTS_URLS: string [];
   BASE_ENVIRONMENT_INDEX: number;
   BASE_URL: string;
+  API_VERSION: string;
   TOKEN_HEADER_KEY: string;
   TOKEN_STORE_KEY: string;
   EXTERNAL_PROTOCOLS: string[];
