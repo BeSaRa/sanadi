acc062a88502cf258115f0c3bcb52fd9f04b8cbf
Navjot Singh <navjotsandha@gmail.com>
2021-01-24 01:56:21 +0300

    fix: hide add button from org-branch-user tab

--- src/app/shared/components/page-header/page-header.component.ts
+++ src/app/shared/components/page-header/page-header.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {ILanguageKeys} from '../../../interfaces/i-language-keys';
 import {LangService} from '../../../services/lang.service';
 import {BehaviorSubject, Subject} from 'rxjs';
+import {isEmptyObject} from '../../../helpers/utils';
 
 @Component({
   selector: 'app-page-header',
@@ -13,15 +14,24 @@ export class PageHeaderComponent implements OnInit, OnDestroy {
   @Input() clickOnReload$: BehaviorSubject<any> = {} as BehaviorSubject<any>;
   @Input() clickOnNew$: Subject<any> = {} as Subject<any>;
 
+  isReloadAvailable = false;
+  isAddAvailable = false;
+
   constructor(public langService: LangService) {
   }
 
   ngOnInit(): void {
+    this.isAddAvailable = !isEmptyObject(this.clickOnNew$);
+    this.isReloadAvailable = !isEmptyObject(this.clickOnReload$);
   }
 
   ngOnDestroy(): void {
-    this.clickOnReload$.complete();
-    this.clickOnNew$.complete();
+    if (this.isReloadAvailable) {
+      this.clickOnReload$.complete();
+    }
+    if (this.isAddAvailable) {
+      this.clickOnNew$.complete();
+    }
   }
 
 }
