775c65f32ba81e4cfbfdd6ba5d5e9d658d294e2d
BeSaRa <besara@windowslive.com>
2021-01-29 17:24:31 +0300

    allow extender to accept instance or constructor.

--- src/app/helpers/extender.ts
+++ src/app/helpers/extender.ts
@@ -1,8 +1,14 @@
 import {cloneDeep} from 'lodash';
 
 export const extender: <T>(model: any, ...values: any) => T = <T>(model: any, ...values: any): T => {
-  // @ts-ignore
-  return cloneDeep(Object.assign.apply(this, [new model(), ...values])) as T;
+  let myModel;
+  try {
+    myModel = new model();
+  } catch (e) {
+    myModel = new model.constructor;
+  }
+
+  return cloneDeep(Object.assign.apply(this, [myModel, ...values])) as T;
 };
 
 
