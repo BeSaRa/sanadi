0f5cfd526e8009f770d081b9010867761c47f0c1
BeSaRa <besara@windowslive.com>
2021-02-06 04:27:48 +0300

    update subvention-request.ts properties

--- src/app/models/subvention-request.ts
+++ src/app/models/subvention-request.ts
@@ -1,10 +1,35 @@
 import {Observable} from 'rxjs';
 import {BaseModel} from './base-model';
+import {SubventionRequestService} from '../services/subvention-request.service';
+import {FactoryService} from '../services/factory.service';
+import {SubventionRequestAidService} from '../services/subvention-request-aid.service';
 
 export class SubventionRequest extends BaseModel<SubventionRequest> {
+  id!: number;
+  requestSerial!: number;
+  requestFullSerial!: string;
+  requestChannel!: number;
+  requestType!: number;
+  requestedAidAmount!: number;
+  requestYear!: number;
+  requestSummary!: string;
+  charityRefNo!: string;
+  charitySerialNo!: string;
+  creationDate!: string;
+  approvalIndicator!: number;
+  status!: number;
+  statusDateModified!: number;
+  orgBranchId!: number;
+  orgId!: number;
+  benId!: number;
+  // not belongs to the Model
+  service: SubventionRequestService;
+  subventionRequestAidService: SubventionRequestAidService;
 
   constructor() {
     super();
+    this.service = FactoryService.getService('SubventionRequestService');
+    this.subventionRequestAidService = FactoryService.getService('SubventionRequestAidService');
   }
 
   create(): Observable<SubventionRequest> {
