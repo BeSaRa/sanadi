ef5fba20adae3fc387f7f382afbe8a58586c0398
Navjot Singh <navjotsandha@gmail.com>
2021-02-13 22:45:57 +0300

    fix: localization component: 1. removed add, delete, row selection
    Bug 38755:Admin -localization - Unable to add Notes "ملاحظات" to localizations

--- src/app/administration/pages/localization/localization.component.ts
+++ src/app/administration/pages/localization/localization.component.ts
@@ -8,9 +8,7 @@ import {DialogRef} from '../../../shared/models/dialog-ref';
 import {DialogService} from '../../../services/dialog.service';
 import {UserClickOn} from '../../../enums/user-click-on.enum';
 import {PageComponentInterface} from '../../../interfaces/page-component-interface';
-import {IGridAction} from '../../../interfaces/i-grid-action';
-import {cloneDeep as _deepClone} from 'lodash';
-import {filterList, generateHtmlList} from '../../../helpers/utils';
+import {filterList} from '../../../helpers/utils';
 import {IKeyValue} from '../../../interfaces/i-key-value';
 
 @Component({
@@ -21,7 +19,7 @@ import {IKeyValue} from '../../../interfaces/i-key-value';
 export class LocalizationComponent implements OnInit, OnDestroy, PageComponentInterface<Localization> {
   localization: Localization[] = [];
   localizationsClone: Localization[] = [];
-  displayedColumns: string[] = ['rowSelection', 'localizationKey', 'arName', 'enName', 'actions'];
+  displayedColumns: string[] = ['localizationKey', 'arName', 'enName', 'actions'];
   reloadSubscription!: Subscription;
   addSubscription!: Subscription;
   reload$ = new BehaviorSubject<any>(null);
@@ -32,59 +30,6 @@ export class LocalizationComponent implements OnInit, OnDestroy, PageComponentIn
     arName: 'arName',
     enName: 'enName'
   };
-
-  selectedRecords: Localization[] = [];
-  actionsList: IGridAction[] = [
-    {
-      langKey: 'btn_delete',
-      icon: 'mdi-close-box',
-      callback: ($event: MouseEvent) => {
-        this.deleteBulk($event);
-      }
-    }
-  ];
-
-  private _addSelected(record: Localization): void {
-    this.selectedRecords.push(_deepClone(record));
-  }
-
-  private _removeSelected(record: Localization): void {
-    const index = this.selectedRecords.findIndex((item) => {
-      return item.id === record.id;
-    });
-    this.selectedRecords.splice(index, 1);
-  }
-
-  get isIndeterminateSelection(): boolean {
-    return this.selectedRecords.length > 0 && this.selectedRecords.length < this.localization.length;
-  }
-
-  get isFullSelection(): boolean {
-    return this.selectedRecords.length === this.localization.length;
-  }
-
-  isSelected(record: Localization): boolean {
-    return !!this.selectedRecords.find((item) => {
-      return item.id === record.id;
-    });
-  }
-
-  onSelect($event: Event, record: Localization): void {
-    const checkBox = $event.target as HTMLInputElement;
-    if (checkBox.checked) {
-      this._addSelected(record);
-    } else {
-      this._removeSelected(record);
-    }
-  }
-
-  onSelectAll($event: Event): void {
-    if (this.selectedRecords.length === this.localization.length) {
-      this.selectedRecords = [];
-    } else {
-      this.selectedRecords = _deepClone(this.localization);
-    }
-  }
 
   constructor(public langService: LangService, private dialogService: DialogService, public toast: ToastService) {
 
@@ -108,7 +53,6 @@ export class LocalizationComponent implements OnInit, OnDestroy, PageComponentIn
     ).subscribe((locals) => {
       this.localization = locals;
       this.localizationsClone = locals.slice();
-      this.selectedRecords = [];
     });
   }
 
@@ -140,7 +84,6 @@ export class LocalizationComponent implements OnInit, OnDestroy, PageComponentIn
 
   delete(localization: Localization, $event: MouseEvent): void {
     $event.preventDefault();
-    // @ts-ignore
     const sub = this.dialogService.confirm(this.langService.map.msg_confirm_delete_x.change({x: localization.localizationKey}))
       .onAfterClose$
       .subscribe((click: UserClickOn) => {
@@ -152,43 +95,6 @@ export class LocalizationComponent implements OnInit, OnDestroy, PageComponentIn
           });
         }
       });
-  }
-
-  _mapBulkResponse(resultMap: any, key: string): void {
-    const failedRecords: Localization[] = [];
-    for (const item of this.selectedRecords) {
-      // @ts-ignore
-      if (resultMap.hasOwnProperty(item[key]) && !resultMap[item[key]]) {
-        failedRecords.push(item);
-      }
-    }
-    if (failedRecords.length === 0) {
-      this.toast.success(this.langService.map.msg_delete_success);
-    } else if (failedRecords.length === this.selectedRecords.length) {
-      this.toast.success(this.langService.map.msg_delete_fail);
-    } else {
-      const listHtml = generateHtmlList(this.langService.map.msg_delete_success_except, failedRecords.map((item) => item.getName()));
-      this.dialogService.info(listHtml.outerHTML);
-    }
-  }
-
-  deleteBulk($event: MouseEvent): void {
-    $event.preventDefault();
-    if (this.selectedRecords.length > 0) {
-      this.dialogService.confirm(this.langService.map.msg_confirm_delete_selected)
-        .onAfterClose$.subscribe((click: UserClickOn) => {
-        if (click === UserClickOn.YES) {
-          const ids = this.selectedRecords.map((item) => {
-            return item.id;
-          });
-          const sub = this.langService.deleteBulk(ids).subscribe((response) => {
-            this._mapBulkResponse(response, 'id');
-            this.reload$.next(null);
-            sub.unsubscribe();
-          });
-        }
-      });
-    }
   }
 
   search(searchText: string): void {
