713de10e9e5749b8c39ad5f8852b307f263accf5
Navjot Singh <navjotsandha@gmail.com>
2021-02-24 20:08:21 +0300

    Bug 39555:Search - show AIDs button - AIDs type doesn't show data

--- src/app/model-interceptors/subvention-aid-interceptor.ts
+++ src/app/model-interceptors/subvention-aid-interceptor.ts
@@ -1,11 +1,13 @@
 import {IModelInterceptor} from '../interfaces/i-model-interceptor';
 import {SubventionAid} from '../models/subvention-aid';
 import {formatDate} from '@angular/common';
+import {AdminResult} from '../models/admin-result';
 
 export class SubventionAidInterceptor implements IModelInterceptor<SubventionAid> {
   receive(model: SubventionAid): SubventionAid {
     model.approvalDate = model.approvalDate ? formatDate(new Date(model.approvalDate), 'yyyy-MM-dd', 'en-US') : '';
     model.aidStartPayDate = model.aidStartPayDate ? formatDate(new Date(model.aidStartPayDate), 'yyyy-MM-dd', 'en-US') : '';
+    model.aidLookupInfo = AdminResult.createInstance(model.aidLookupInfo)
     return model;
   }
 
@@ -14,6 +16,7 @@ export class SubventionAidInterceptor implements IModelInterceptor<SubventionAid
     delete model.arName;
     delete model.enName;
     delete model.mainAidType;
+    delete model.aidLookupInfo;
     model.approvalDate = model.approvalDate ? (new Date(model.approvalDate)).toISOString() : '';
     model.aidStartPayDate = model.aidStartPayDate ? (new Date(model.aidStartPayDate)).toISOString() : '';
     return model;
