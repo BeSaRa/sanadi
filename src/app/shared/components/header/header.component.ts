32c01a1f43310d51794365f4f3f9c1d68eea1cf6
BeSaRa <besara@windowslive.com>
2021-03-27 13:10:01 +0300

    make logout functionality

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -4,6 +4,13 @@ import {LangService} from '../../../services/lang.service';
 import {UrlService} from '../../../services/url.service';
 import {SidebarComponent} from '../sidebar/sidebar.component';
 import {EmployeeService} from '../../../services/employee.service';
+import {AuthService} from '../../../services/auth.service';
+import {Subject} from 'rxjs';
+import {takeUntil} from 'rxjs/operators';
+import {ToastService} from '../../../services/toast.service';
+import {DialogService} from '../../../services/dialog.service';
+import {UserClickOn} from '../../../enums/user-click-on.enum';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -14,10 +21,15 @@ export class HeaderComponent implements OnInit {
   @Input()
   sidebar!: SidebarComponent;
   menuState: string = 'mdi-menu';
+  destroy$: Subject<any> = new Subject<any>();
 
   constructor(private scrollService: AppRootScrollService,
               public langService: LangService,
+              private dialogService: DialogService,
+              private authService: AuthService,
               public employee: EmployeeService,
+              private toastService: ToastService,
+              private router: Router,
               public urlService: UrlService) {
   }
 
@@ -42,5 +54,23 @@ export class HeaderComponent implements OnInit {
   toggleLang($event: MouseEvent) {
     $event.preventDefault();
     this.langService.toggleLanguage();
+  }
+
+  logout() {
+    this.dialogService
+      .confirm(this.langService.map.msg_are_you_sure_you_want_logout)
+      .onAfterClose$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((click: UserClickOn) => {
+        return click === UserClickOn.YES ? this._logout() : null;
+      });
+  }
+
+  private _logout() {
+    this.authService.logout().subscribe(() => {
+      this.router.navigate(['/login']).then(() => {
+        this.toastService.success(this.langService.map.msg_logout_success);
+      });
+    });
   }
 }
