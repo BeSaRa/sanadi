5809d4a1995f10f39146d8383a75a0842788ceda
BeSaRa <besara@windowslive.com>
2021-05-09 15:30:21 +0300

    add inbox structure

--- src/app/e-services/e-services.module.ts
+++ src/app/e-services/e-services.module.ts
@@ -5,10 +5,11 @@ import {EServicesRoutingModule} from './e-services-routing.module';
 import {EServicesComponent} from './e-services.component';
 import {SharedModule} from '../shared/shared.module';
 import { InquiriesComponent } from './pages/inquiries/inquiries.component';
+import { UserInboxComponent } from './pages/user-inbox/user-inbox.component';
 
 
 @NgModule({
-  declarations: [EServicesComponent, InquiriesComponent],
+  declarations: [EServicesComponent, InquiriesComponent, UserInboxComponent],
   imports: [
     CommonModule,
     SharedModule,
