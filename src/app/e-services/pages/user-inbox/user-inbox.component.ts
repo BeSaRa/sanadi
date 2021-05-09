80e8d3a165854fb84d5922c2af8a7349b578563d
BeSaRa <besara@windowslive.com>
2021-05-09 15:30:21 +0300

    add inbox structure
    (cherry picked from commit 46a510948f54ec65b6472faa05f2241d8ff886f0)

---
+++ src/app/e-services/pages/user-inbox/user-inbox.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-user-inbox',
+  templateUrl: './user-inbox.component.html',
+  styleUrls: ['./user-inbox.component.scss']
+})
+export class UserInboxComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
