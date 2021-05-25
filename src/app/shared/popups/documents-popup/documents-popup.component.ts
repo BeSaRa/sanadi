6c963d9cb696a3c53e1cae532cb1978cff65ad03
BeSaRa <besara@windowslive.com>
2021-05-25 14:51:17 +0300

    finish : manage attachment dialog.

---
+++ src/app/shared/popups/documents-popup/documents-popup.component.ts
@@ -0,0 +1,21 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {LangService} from '../../../services/lang.service';
+import {DIALOG_DATA_TOKEN} from '../../tokens/tokens';
+import {EServiceGenericService} from '../../../generics/e-service-generic-service';
+
+@Component({
+  selector: 'documents-popup',
+  templateUrl: './documents-popup.component.html',
+  styleUrls: ['./documents-popup.component.scss']
+})
+export class DocumentsPopupComponent implements OnInit {
+
+  constructor(public lang: LangService, @Inject(DIALOG_DATA_TOKEN) public data: { service: EServiceGenericService<any, any>, caseId: string }) {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+}
