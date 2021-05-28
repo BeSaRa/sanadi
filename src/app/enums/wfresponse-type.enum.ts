b86e993b44e7eb5431336d81e0e44d25f448bb13
BeSaRa <besara@windowslive.com>
2021-05-29 01:45:42 +0300

    add close / return response

--- src/app/enums/wfresponse-type.enum.ts
+++ src/app/enums/wfresponse-type.enum.ts
@@ -2,5 +2,7 @@ export enum WFResponseType {
   TO_USER = 'toUser',
   APPROVE = 'approve',
   REJECT = 'reject',
+  CLOSE = 'close',
+  RETURN = 'return',
   TO_COMPETENT_DEPARTMENT = 'toCompetentDept'
 }
