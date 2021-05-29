cdea7d7ba27306d84580770f0b90b4979c90be61
BeSaRa <besara@windowslive.com>
2021-05-29 03:20:47 +0300

    add type property to the action  give us the ability  to add divider in context menu item.
    (cherry picked from commit c903870b67c8a09c03ce574afc1c4d5d1b3915a7)

--- src/app/modules/context-menu/interfaces/i-menu-item.ts
+++ src/app/modules/context-menu/interfaces/i-menu-item.ts
@@ -1,8 +1,9 @@
 import {ILanguageKeys} from '../../../interfaces/i-language-keys';
 
 export interface IMenuItem {
-  onClick: (item?: any) => void;
-  label: keyof ILanguageKeys | ((item?: any) => string);
+  type: 'action' | 'divider';
+  onClick?: (item?: any) => void;
+  label?: keyof ILanguageKeys | ((item?: any) => string);
   icon?: string;
   show?: (item?: any) => boolean;
   children?: IMenuItem[]
