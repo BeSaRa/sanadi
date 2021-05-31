af4557231538052118fbef4e89b0ce894ef6e875
BeSaRa <besara@windowslive.com>
2021-05-31 10:04:45 +0300

    add more filed to IESComponent and user it in the viewer
    (cherry picked from commit ad10865279ab3d982fa718daee89e87b3d749d54)

--- src/app/interfaces/iescomponent.ts
+++ src/app/interfaces/iescomponent.ts
@@ -1,7 +1,9 @@
 import {Subject} from 'rxjs';
 import {SaveTypes} from '../enums/save-types';
+import {FormGroup} from '@angular/forms';
 
 export interface IESComponent {
   outModel: any;
   save: Subject<SaveTypes>;
+  form: FormGroup
 }
