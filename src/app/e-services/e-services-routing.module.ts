468c337c05c0310dede9375b60afde5c2b99be9d
Mahmoud Soliman <mahmoud.mohsen.soliman@gmail.com>
2021-06-03 03:51:12 +0200

    add international cooperation container

--- src/app/e-services/e-services-routing.module.ts
+++ src/app/e-services/e-services-routing.module.ts
@@ -4,11 +4,13 @@ import {RouterModule, Routes} from '@angular/router';
 import {EServicesComponent} from './e-services.component';
 import {InquiryContainerComponent} from './pages/inquiry-container/inquiry-container.component';
 import {ConsultationContainerComponent} from './pages/consultation-container/consultation-container.component';
+import {InternationalCooperationContainerComponent} from './pages/international-cooperation-container/international-cooperation-container.component';
 
 const routes: Routes = [
   {path: '', component: EServicesComponent},
   {path: 'inquiries', component: InquiryContainerComponent},
-  {path: 'consultations', component: ConsultationContainerComponent}
+  {path: 'consultations', component: ConsultationContainerComponent},
+  {path: 'international-coop', component: InternationalCooperationContainerComponent}
 ];
 
 @NgModule({
