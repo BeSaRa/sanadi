5bb1b5803fbef1b654330e98c7617b1c9a964cc9
BeSaRa <besara@windowslive.com>
2021-06-15 11:04:47 +0300

    add useSearchToFilter input the table component to let automatic call search function inside the model while filter process
    (cherry picked from commit 263a05457558012f06038e8a70514b1a6f777638)

--- src/app/models/case-model.ts
+++ src/app/models/case-model.ts
@@ -12,8 +12,10 @@ import {DynamicComponentService} from '../services/dynamic-component.service';
 import {delay, map, take, tap} from 'rxjs/operators';
 import {CaseViewerPopupComponent} from '../shared/popups/case-viewer-popup/case-viewer-popup.component';
 import {IESComponent} from '../interfaces/iescomponent';
+import {ISearchFields} from '../interfaces/i-search-fields';
+import {searchFunctionType} from '../types/types';
 
-export abstract class CaseModel<S extends EServiceGenericService<T>, T extends FileNetModel<T>> extends FileNetModel<T> {
+export abstract class CaseModel<S extends EServiceGenericService<T>, T extends FileNetModel<T>> extends FileNetModel<T> implements ISearchFields {
   serial!: number;
   fullSerial!: string;
   caseState!: number;
@@ -29,6 +31,27 @@ export abstract class CaseModel<S extends EServiceGenericService<T>, T extends F
   assignDate!: string;
   className!: string;
   service!: S;
+
+  searchFields: { [key: string]: string | searchFunctionType } = {};
+
+  search(searchText: string): boolean {
+    const self = this as unknown as ISearchFields;
+    const fields = (this as unknown as ISearchFields).searchFields;
+    const keys = Object.keys(fields);
+    if (!searchText) {
+      return true;
+    }
+    return keys.some((key) => {
+      if (typeof self.searchFields[key] === 'function') {
+        const func = self.searchFields[key] as searchFunctionType;
+        return func(searchText.trim().toLowerCase());
+      } else {
+        const field = self.searchFields[key] as string;
+        const value = (this as unknown as any)[field] ? ((this as unknown as any)[field] as string) + '' : '';
+        return value.toLowerCase().indexOf(searchText.trim().toLowerCase()) !== -1;
+      }
+    });
+  }
 
   create(): Observable<T> {
     return this.service.create(this as unknown as T);
