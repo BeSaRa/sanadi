27fa1c25d82c789197b7b55ca42bb1a902290eca
BeSaRa <besara@windowslive.com>
2021-07-27 18:23:03 +0300

    reformat the Language Model

--- src/app/models/language.ts
+++ src/app/models/language.ts
@@ -2,6 +2,11 @@ import {Styles} from '../enums/styles.enum';
 import {Direction} from '@angular/cdk/bidi';
 
 export class Language {
-  constructor(public  id: number, public  name: string, public code: string, public direction: Direction, public style: Styles, public toggleTo: string) {
+  constructor(public id: number,
+              public name: string,
+              public code: string,
+              public direction: Direction,
+              public style: Styles,
+              public toggleTo: string) {
   }
 }
