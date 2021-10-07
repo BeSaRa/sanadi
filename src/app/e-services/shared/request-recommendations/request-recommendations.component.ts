d7dc6ab59b8e06b1bc177090489463b048160240
Khaled Yabroudi <khaledyabroudi@gmail.com>
2021-10-07 12:08:09 +0300

    43960:Enhancement - Recommendations tab (general manager role) should not appear for partner and final license requests

--- src/app/e-services/shared/request-recommendations/request-recommendations.component.ts
+++ src/app/e-services/shared/request-recommendations/request-recommendations.component.ts
@@ -3,6 +3,8 @@ import {ITableOptions} from '@app/interfaces/i-table-options';
 import {FilterEventTypes} from '@app/types/types';
 import {LangService} from '@app/services/lang.service';
 import {AdminResult} from '@app/models/admin-result';
+import {CaseStatus} from "@app/enums/case-status.enum";
+import {CaseTypes} from "@app/enums/case-types.enum";
 
 @Component({
   selector: 'request-recommendations',
@@ -43,7 +45,8 @@ export class RequestRecommendationsComponent implements OnInit {
           enName: this.langService.getEnglishLocalByKey('role_specialist')
         }),
         comment: !this.record ? '' : this.record.specialistJustification,
-        actionInfo: !this.record ? null : this.record.specialistDecisionInfo
+        actionInfo: !this.record ? null : this.record.specialistDecisionInfo,
+        show: true
       },
       {
         roleInfo: AdminResult.createInstance({
@@ -51,7 +54,8 @@ export class RequestRecommendationsComponent implements OnInit {
           enName: this.langService.getEnglishLocalByKey('role_reviewer_department')
         }),
         comment: !this.record ? '' : this.record.reviewerDepartmentJustification,
-        actionInfo: !this.record ? null : this.record.reviewerDepartmentDecisionInfo
+        actionInfo: !this.record ? null : this.record.reviewerDepartmentDecisionInfo,
+        show: true
       },
       {
         roleInfo: AdminResult.createInstance({
@@ -59,7 +63,8 @@ export class RequestRecommendationsComponent implements OnInit {
           enName: this.langService.getEnglishLocalByKey('role_manager')
         }),
         comment: !this.record ? '' : this.record.managerJustification,
-        actionInfo: !this.record ? null : this.record.managerDecisionInfo
+        actionInfo: !this.record ? null : this.record.managerDecisionInfo,
+        show: true
       },
       {
         roleInfo: AdminResult.createInstance({
@@ -67,7 +72,8 @@ export class RequestRecommendationsComponent implements OnInit {
           enName: this.langService.getEnglishLocalByKey('role_general_manager')
         }),
         comment: !this.record ? '' : this.record.generalManagerJustification,
-        actionInfo: !this.record ? null : this.record.generalManagerDecisionInfo
+        actionInfo: !this.record ? null : this.record.generalManagerDecisionInfo,
+        show: this.record.caseType === CaseTypes.INITIAL_EXTERNAL_OFFICE_APPROVAL
       },
       {
         roleInfo: AdminResult.createInstance({
@@ -75,8 +81,11 @@ export class RequestRecommendationsComponent implements OnInit {
           enName: this.langService.getEnglishLocalByKey('role_chief')
         }),
         comment: !this.record ? '' : this.record.chiefJustification,
-        actionInfo: !this.record ? null : this.record.chiefDecisionInfo
+        actionInfo: !this.record ? null : this.record.chiefDecisionInfo,
+        show: true
       }
     ];
+
+    this.data = this.data.filter(item => item.show)
   }
 }
