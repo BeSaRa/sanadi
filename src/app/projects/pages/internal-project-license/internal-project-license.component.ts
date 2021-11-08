fe26be82e46433a7b31aca67742691833a1daadd
Navjot Singh <navjotsandha@gmail.com>
2021-11-08 21:30:13 +0300

    Bug 44817:Internal Projects: Attachments: after user add attachments from inside reqeust , he can't delete it "delete is disabled"

--- src/app/projects/pages/internal-project-license/internal-project-license.component.ts
+++ src/app/projects/pages/internal-project-license/internal-project-license.component.ts
@@ -380,6 +380,26 @@ export class InternalProjectLicenseComponent extends EServicesGenericComponent<I
     return isAllowed;
   }
 
+  isAttachmentReadonly(): boolean {
+    if (!this.model?.id) {
+      return false;
+    }
+    let isAllowed = true;
+    if (this.openFrom === OpenFrom.TEAM_INBOX) {
+      isAllowed = this.model.taskDetails.isClaimed();
+    }
+    if (isAllowed) {
+      let caseStatus = this.model.getCaseStatus(),
+        caseStatusEnum = this.service.caseStatusEnumMap[this.model.getCaseType()];
+
+      if (caseStatusEnum) {
+        isAllowed = (caseStatus !== caseStatusEnum.CANCELLED && caseStatus !== caseStatusEnum.FINAL_APPROVE && caseStatus !== caseStatusEnum.FINAL_REJECTION);
+      }
+    }
+
+    return !isAllowed;
+  }
+
   get basicInfoGroup(): FormGroup {
     return (this.form.get('basicInfo')) as FormGroup;
   }
