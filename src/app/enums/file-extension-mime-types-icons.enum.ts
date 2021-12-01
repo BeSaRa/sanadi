816f0d2e5c482b762992178348ffecd2f490617c
Navjot Singh <navjotsandha@gmail.com>
2021-12-01 15:43:41 +0300

    enum > file extension, file mime types, file icons

---
+++ src/app/enums/file-extension-mime-types-icons.enum.ts
@@ -0,0 +1,37 @@
+export enum FileExtensionsEnum {
+  PDF = '.pdf',
+  PNG = '.png',
+  JPG = '.jpg',
+  JPEG = '.jpeg',
+  MP4 = '.mp4',
+  MKV = '.mkv',
+  PPT = '.ppt',
+  PPTX = '.pptx'
+}
+
+export enum FileMimeTypesEnum {
+  PDF = 'application/pdf',
+  PNG = 'image/png',
+  JPG = 'image/jpeg',
+  JPEG = 'image/jpeg',
+  MP4 = 'video/mp4',
+  MKV = 'video/x-matroska',
+  PPT = 'application/vnd.ms-powerpoint',
+  PPTX = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+}
+
+export enum FileIconsEnum {
+  FILE = 'mdi-file-outline',
+  IMAGE = 'mdi-file-image-outline',
+  VIDEO = 'mdi-file-video-outline',
+  HIDDEN = 'mdi-file-hidden',
+  SEARCH = 'mdi-file-search',
+  PDF = 'mdi-file-pdf-box',
+  PNG = 'mdi-file-png-box',
+  JPG = 'mdi-file-jpg-box',
+  JPEG = 'mdi-file-jpg-box',
+  MP4 = 'mdi-file-video-outline',
+  MKV = 'mdi-file-video-outline',
+  PPT = 'mdi-file-powerpoint-outline',
+  PPTX = 'mdi-file-powerpoint-outline'
+}
