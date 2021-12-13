7d571d47740a5691790b679a1a3909f4c387bb4c
Mahmoud Soliman <mahmoud.mohsen.soliman@gmail.com>
2021-12-13 18:17:30 +0200

    Bug 45396:Training - download certification: Raca user cannot download certificate for raca trainees

--- src/app/training-services/popups/trainee-list/trainee-list.component.ts
+++ src/app/training-services/popups/trainee-list/trainee-list.component.ts
@@ -1,17 +1,22 @@
-import {Component, Input, OnChanges, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnDestroy, OnInit} from '@angular/core';
 import {TrainingProgram} from '@app/models/training-program';
 import {LangService} from '@app/services/lang.service';
+import {TraineeData} from '@app/models/trainee-data';
+import {takeUntil} from 'rxjs/operators';
+import {CertificateService} from '@app/services/certificate.service';
+import {Subject} from 'rxjs';
 
 @Component({
   selector: 'trainee-list',
   templateUrl: './trainee-list.component.html',
   styleUrls: ['./trainee-list.component.scss']
 })
-export class TraineeListComponent implements OnInit, OnChanges {
+export class TraineeListComponent implements OnInit, OnChanges, OnDestroy {
+  destroy$: Subject<void> = new Subject<void>();
   @Input() trainingProgram!: TrainingProgram;
   model!: TrainingProgram;
-  displayedColumns: string[] = ['arName', 'enName', 'department', 'status', 'nationality'];
-  constructor(public lang: LangService) { }
+  displayedColumns: string[] = ['arName', 'enName', 'department', 'status', 'nationality', 'actions'];
+  constructor(public lang: LangService, private certificateService: CertificateService) { }
 
   ngOnChanges() {
     this.model = (new TrainingProgram()).clone(this.trainingProgram);
@@ -21,4 +26,18 @@ export class TraineeListComponent implements OnInit, OnChanges {
   ngOnInit(): void {
   }
 
+  downloadCertificate(event: MouseEvent, model: TraineeData) {
+    event.preventDefault();
+    return this.certificateService.downloadCertificate(this.model.id, model.trainee.id)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((data) => {
+        let downloadURL = window.URL.createObjectURL(data.blob);
+        window.open(downloadURL);
+      });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
