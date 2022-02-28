23bf98562e52fe383f6109c5b055da89bf56e4a0
BeSaRa <besara@windowslive.com>
2022-02-28 15:05:30 +0300

    remove bootstrap animation and add angular animation for good performance

--- src/app/shared/components/tab/tab.component.ts
+++ src/app/shared/components/tab/tab.component.ts
@@ -2,11 +2,30 @@ import {Component, Host, Input, OnDestroy, OnInit, TemplateRef} from '@angular/c
 import {TabListService} from '../tabs/tab-list-service';
 import {delay, takeUntil} from 'rxjs/operators';
 import {Subject} from 'rxjs';
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'tab , [tab]',
   templateUrl: './tab.component.html',
-  styleUrls: ['./tab.component.scss']
+  styleUrls: ['./tab.component.scss'],
+  animations: [
+    trigger('toggleExpand', [
+      state('open', style({
+        height: '!',
+        opacity: 1,
+        padding: '*'
+      })),
+      state('close', style({
+        height: 0,
+        opacity: 0,
+        padding: 0,
+        overflow: 'hidden'
+      })),
+      transition('open <=> close', [
+        animate('.250s ease-in-out')
+      ])
+    ])
+  ]
 })
 export class TabComponent implements OnInit, OnDestroy {
   @Input() title!: string;
@@ -21,6 +40,7 @@ export class TabComponent implements OnInit, OnDestroy {
   containerId: number = 0;
   tabId: string = '';
   tabIdRef: string = '';
+  expanded: string = 'open';
 
   constructor(@Host() private tabListService: TabListService) {
   }
@@ -50,5 +70,9 @@ export class TabComponent implements OnInit, OnDestroy {
       .subscribe((tab) => {
         this.active = this === tab;
       });
+  }
+
+  toggleAccordion() {
+    this.expanded = this.expanded === 'open' ? 'close' : 'open';
   }
 }
