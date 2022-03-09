a171a47ef92d7f117df618f029377484b801963a
Navjot Singh <navjotsandha@gmail.com>
2022-03-09 10:12:41 +0300

    action icons enum

--- src/app/enums/action-icons-enum.ts
+++ src/app/enums/action-icons-enum.ts
@@ -1,7 +1,10 @@
 export enum ActionIconsEnum {
   EDIT = 'mdi-pen',
   EDIT_USER = 'mdi-account-edit',
+  VIEW = 'mdi-eye',
   DELETE = 'mdi-close-box',
+  DELETE_TRASH = 'mdi-delete',
   LOGS = 'mdi-history',
-
+  CHILD_ITEMS = 'mdi-circle-multiple',
+  STATUS = 'mdi-list-status'
 }
