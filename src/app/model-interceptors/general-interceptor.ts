0499c11ec4b991be18080a9e6edaf5a84b06a723
Rajat <rajat.chugh2431@gmail.com>
2022-03-17 22:00:03 +0530

    taskDetails changes added in interceptor

--- src/app/model-interceptors/general-interceptor.ts
+++ src/app/model-interceptors/general-interceptor.ts
@@ -1,8 +1,10 @@
 import {AdminResult} from "@app/models/admin-result";
+import { TaskDetails } from "@app/models/task-details";
 
 export class GeneralInterceptor {
   static receive(model: any): any {
     model.setItemRoute && model.setItemRoute();
+    model.taskDetails && (model.taskDetails = new TaskDetails().clone(model.taskDetails));
     model.taskDetails && model.taskDetails.fromUserInfo && (model.taskDetails.fromUserInfo = AdminResult.createInstance(model.taskDetails.fromUserInfo));
     model.creatorInfo && (model.creatorInfo = AdminResult.createInstance(model.creatorInfo))
     model.caseStatusInfo && (model.caseStatusInfo = AdminResult.createInstance(model.caseStatusInfo))
@@ -21,6 +23,7 @@ export class GeneralInterceptor {
     delete model.inboxService;
     delete model.caseStatusInfo;
     delete model.creatorInfo;
+    delete model.taskDetails;
     return model;
   }
 }
