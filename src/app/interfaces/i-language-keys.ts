59f3f0a27cd48074c225cac9aac3e9f15f78a549
Navjot Singh <navjotsandha@gmail.com>
2022-03-27 16:20:00 +0300

    user request > request info > disclosure/non-disclosure

--- src/app/interfaces/i-language-keys.ts
+++ src/app/interfaces/i-language-keys.ts
@@ -909,4 +909,5 @@ export interface ILanguageKeys {
   msg_please_select_service_to_add: string;
   menu_urgent_intervention_license: string;
   msg_completion_offer_to_other_ou_if_not_fully_paid: string;
+  disclosure_non_disclosure: string;
 }
