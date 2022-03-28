3c2ec4c14f4c48d43db05205da5a22793d8f3832
Navjot Singh <navjotsandha@gmail.com>
2022-03-28 17:24:04 +0300

    fix: collection services permissions on menu and routing

--- src/app/modules/collection/collection-routing.module.ts
+++ src/app/modules/collection/collection-routing.module.ts
@@ -6,6 +6,7 @@ import {
 } from "@app/shared/components/e-service-component-wrapper/e-service-component-wrapper.component";
 import {ServicesGuard} from "@app/guards/services.guard";
 import {ServiceItemResolver} from "@app/resolvers/service-item.resolver";
+import {Permissions} from '@app/enums/Permissions';
 
 const routes: Routes = [
   {path: '', component: CollectionComponent},
@@ -15,6 +16,8 @@ const routes: Routes = [
     canActivate: [ServicesGuard],
     resolve: {info: ServiceItemResolver},
     data: {
+      permissionKey: Permissions.COLLECTION_APPROVAL,
+      configPermissionGroup: null, checkAnyPermission: false,
       render: 'CollectionApprovalComponent'
     }
   },
@@ -24,6 +27,8 @@ const routes: Routes = [
     canActivate: [ServicesGuard],
     resolve: {info: ServiceItemResolver},
     data: {
+      permissionKey: Permissions.COLLECTION_APPROVAL,
+      configPermissionGroup: null, checkAnyPermission: false,
       render: 'CollectorApprovalComponent'
     }
   }
