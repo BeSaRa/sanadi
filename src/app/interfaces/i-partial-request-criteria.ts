b6fbf2dd0dd1dda21541bd0276fa354c7e3b42d5
Navjot Singh <navjotsandha@gmail.com>
2022-04-14 02:25:13 +0300

    sanadi > fix: removed requestType from IPartialRequestCriteria

--- src/app/interfaces/i-partial-request-criteria.ts
+++ src/app/interfaces/i-partial-request-criteria.ts
@@ -2,7 +2,6 @@ export interface IPartialRequestCriteria {
   orgId: number,
   requestYear: number,
   benCategory: number,
-  requestType: string,
   gender: number
   aidLookupParentId: number
   aidLookupId: number
