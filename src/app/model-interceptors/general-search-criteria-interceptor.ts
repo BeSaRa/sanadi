2a747117fa729a151bf4ec9185cfdbda366030b0
Navjot Singh <navjotsandha@gmail.com>
2022-04-14 10:50:47 +0300

    e-services > fix: handle createdOnFrom, createdOnTo in general-search-criteria-interceptor

--- src/app/model-interceptors/general-search-criteria-interceptor.ts
+++ src/app/model-interceptors/general-search-criteria-interceptor.ts
@@ -5,14 +5,21 @@ import {IMyDateModel} from 'angular-mydatepicker';
 import {identity} from 'rxjs';
 import {ICaseSearchCriteria} from '../interfaces/icase-search-criteria';
 import {InquirySearchCriteriaInterceptor} from '../search-criteria-interceptors/inquiry-search-criteria-interceptor';
-import {ConsultationSearchCriteriaInterceptor} from '../search-criteria-interceptors/consultation-search-criteria-interceptor';
-import {InternationalCooperationSearchCriteriaInterceptor} from '../search-criteria-interceptors/international-cooperation-search-criteria-interceptor';
+import {
+  ConsultationSearchCriteriaInterceptor
+} from '../search-criteria-interceptors/consultation-search-criteria-interceptor';
+import {
+  InternationalCooperationSearchCriteriaInterceptor
+} from '../search-criteria-interceptors/international-cooperation-search-criteria-interceptor';
 import {CaseTypes} from '../enums/case-types.enum';
-import * as dayjs from 'dayjs';
 import {FactoryService} from '../services/factory.service';
 import {ConfigurationService} from '../services/configuration.service';
-import {FinalExternalOfficeApprovalSearchCriteriaInterceptor} from '@app/search-criteria-interceptors/final-external-office-approval-search-criteria-interceptor';
-import {PartnerApprovalSearchCriteriaInterceptor} from "@app/search-criteria-interceptors/partner-approval-search-criteria-interceptor";
+import {
+  FinalExternalOfficeApprovalSearchCriteriaInterceptor
+} from '@app/search-criteria-interceptors/final-external-office-approval-search-criteria-interceptor';
+import {
+  PartnerApprovalSearchCriteriaInterceptor
+} from "@app/search-criteria-interceptors/partner-approval-search-criteria-interceptor";
 
 const interceptors: Map<number, IModelInterceptor<any>> = new Map<number, IModelInterceptor<any>>();
 
@@ -34,10 +41,9 @@ export class GeneralSearchCriteriaInterceptor implements IModelInterceptor<ICase
     delete model.service;
     delete model.employeeService;
 
-    model.createdOnFrom = dayjs(DateUtils.changeDateFromDatepicker(model.createdOnFrom as unknown as IMyDateModel))
-      .startOf('day').startOf('day').format(configurationService.CONFIG.TIMESTAMP).split(' ').join('T') + 'Z';
-    model.createdOnTo = dayjs(DateUtils.changeDateFromDatepicker(model.createdOnTo as unknown as IMyDateModel))
-      .endOf('day').format(configurationService.CONFIG.TIMESTAMP).split(' ').join('T') + 'Z';
+    model.createdOnFrom = DateUtils.setStartOfDay(model.createdOnFrom as unknown as IMyDateModel).format(configurationService.CONFIG.TIMESTAMP).split(' ').join('T') + 'Z';
+    model.createdOnTo = DateUtils.setEndOfDay(model.createdOnTo  as unknown as IMyDateModel).format(configurationService.CONFIG.TIMESTAMP).split(' ').join('T') + 'Z';
+
     return interceptors.get(model.caseType!)?.send(model) || identity(model);
   }
 
