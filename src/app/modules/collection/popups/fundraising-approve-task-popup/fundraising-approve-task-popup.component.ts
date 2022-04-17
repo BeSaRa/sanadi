e1876ee7825934120299a5fb27393c372c9cb593
Navjot Singh <navjotsandha@gmail.com>
2022-04-17 11:50:13 +0300

    Bug 48975:fundraising - the approve popup shouldnot appear in case the request type is cancel

--- src/app/modules/collection/popups/fundraising-approve-task-popup/fundraising-approve-task-popup.component.ts
+++ src/app/modules/collection/popups/fundraising-approve-task-popup/fundraising-approve-task-popup.component.ts
@@ -1,21 +1,22 @@
-import { Component, Inject, OnInit, ViewChild } from "@angular/core";
-import { FormControl } from "@angular/forms";
-import { WFResponseType } from "@app/enums/wfresponse-type.enum";
-import { CommonUtils } from "@app/helpers/common-utils";
-import { HasLicenseApproval } from "@app/interfaces/has-license-approval";
-import { ILanguageKeys } from "@app/interfaces/i-language-keys";
-import { IWFResponse } from "@app/interfaces/i-w-f-response";
-import { CollectionItem } from "@app/models/collection-item";
-import { Fundraising } from "@app/models/fundraising";
-import { DialogService } from "@app/services/dialog.service";
-import { InboxService } from "@app/services/inbox.service";
-import { LangService } from "@app/services/lang.service";
-import { ToastService } from "@app/services/toast.service";
-import { DialogRef } from "@app/shared/models/dialog-ref";
-import { DIALOG_DATA_TOKEN } from "@app/shared/tokens/tokens";
-import { Subject } from "rxjs";
-import { takeUntil, exhaustMap, switchMap, tap, map, filter } from "rxjs/operators";
-import { ApprovalFormComponent } from "../../shared/approval-form/approval-form.component";
+import {Component, Inject, OnInit, ViewChild} from "@angular/core";
+import {FormControl} from "@angular/forms";
+import {WFResponseType} from "@app/enums/wfresponse-type.enum";
+import {CommonUtils} from "@app/helpers/common-utils";
+import {HasLicenseApproval} from "@app/interfaces/has-license-approval";
+import {ILanguageKeys} from "@app/interfaces/i-language-keys";
+import {IWFResponse} from "@app/interfaces/i-w-f-response";
+import {Fundraising} from "@app/models/fundraising";
+import {DialogService} from "@app/services/dialog.service";
+import {InboxService} from "@app/services/inbox.service";
+import {LangService} from "@app/services/lang.service";
+import {ToastService} from "@app/services/toast.service";
+import {DialogRef} from "@app/shared/models/dialog-ref";
+import {DIALOG_DATA_TOKEN} from "@app/shared/tokens/tokens";
+import {Subject} from "rxjs";
+import {takeUntil, exhaustMap, switchMap, tap, map, filter} from "rxjs/operators";
+import {ApprovalFormComponent} from "../../shared/approval-form/approval-form.component";
+import {ServiceRequestTypes} from '@app/enums/service-request-types';
+import {CustomValidators} from '@app/validators/custom-validators';
 
 @Component({
   selector: "fundraising-approve-task-popup",
@@ -30,25 +31,19 @@ export class FundraisingApproveTaskPopupComponent implements OnInit {
   response: WFResponseType = WFResponseType.APPROVE;
 
   model: Fundraising;
-  comment: FormControl = new FormControl();
+  comment: FormControl = new FormControl('', [CustomValidators.maxLength(CustomValidators.defaultLengths.EXPLANATIONS)]);
 
-  @ViewChild(ApprovalFormComponent, { static: true })
+  @ViewChild(ApprovalFormComponent, {static: true})
   approvalForm!: ApprovalFormComponent;
 
-  constructor(
-    private dialog: DialogService,
-    private dialogRef: DialogRef,
-    private toast: ToastService,
-    private inboxService: InboxService,
-    @Inject(DIALOG_DATA_TOKEN)
-    public data: {
-      model: Fundraising;
-      action: WFResponseType;
-    },
-    public lang: LangService
-  ) {
-    this.label = (CommonUtils.changeCamelToSnakeCase(this.data.action) +
-      "_task") as unknown as keyof ILanguageKeys;
+  constructor(private dialog: DialogService,
+              private dialogRef: DialogRef,
+              private toast: ToastService,
+              private inboxService: InboxService,
+              @Inject(DIALOG_DATA_TOKEN)
+              public data: { model: Fundraising; action: WFResponseType; },
+              public lang: LangService) {
+    this.label = (CommonUtils.changeCamelToSnakeCase(this.data.action) + "_task") as unknown as keyof ILanguageKeys;
     this.response = this.data.action;
     this.model = this.data.model;
   }
@@ -62,10 +57,13 @@ export class FundraisingApproveTaskPopupComponent implements OnInit {
   ngOnInit(): void {
     this.listenToAction();
     this.listenToSave();
+    if (this.isCommentRequired()) {
+      this.comment.setValidators([CustomValidators.required, CustomValidators.maxLength(CustomValidators.defaultLengths.EXPLANATIONS)]);
+    }
   }
 
   listenToSave(): void {
-    this.approvalForm.saveInfo
+    this.approvalForm?.saveInfo
       .pipe(takeUntil(this.destroy$))
       .subscribe((model: HasLicenseApproval) => {
         this.model = model as unknown as Fundraising;
@@ -75,10 +73,13 @@ export class FundraisingApproveTaskPopupComponent implements OnInit {
   private listenToAction() {
     this.action$
       .pipe(takeUntil(this.destroy$))
-      .pipe(tap((_) => this.approvalForm.saveApprovalInfo()))
-      .pipe(map(_ => this.model.hasValidApprovalInfo()))
-      .pipe(tap(valid => !valid && this.displayInvalidItemMessages()))
+      .pipe(tap((_) => this.isCancelRequestType() ? null : this.approvalForm.saveApprovalInfo()))
+      .pipe(map(_ => this.isCancelRequestType() ? true : this.model.hasValidApprovalInfo()))
+      .pipe(tap(valid => !valid && this.displayInvalidFormMessage()))
       .pipe(filter(valid => valid))
+      .pipe(map(_ => this.isCommentRequired() ? this.comment.invalid : false))
+      .pipe(tap(invalid => invalid && this.dialog.error(this.lang.map.msg_all_required_fields_are_filled)))
+      .pipe(filter(invalid => !invalid))
       .pipe(exhaustMap((_) => this.model.save()))
       .pipe(
         switchMap((_) =>
@@ -95,19 +96,24 @@ export class FundraisingApproveTaskPopupComponent implements OnInit {
       });
   }
 
-  private displayInvalidItemMessages() {
-    this.dialog.error(
-      this.lang.map
-        .please_make_sure_that_you_filled_out_all_required_data_for_all_license
-    );
+  private displayInvalidFormMessage() {
+    this.dialog.error(this.lang.map.please_make_sure_that_you_filled_out_all_required_data_for_all_license);
   }
 
   private getResponse(): Partial<IWFResponse> {
     return this.comment.value
       ? {
-          selectedResponse: this.response,
-          comment: this.comment.value,
-        }
-      : { selectedResponse: this.response };
+        selectedResponse: this.response,
+        comment: this.comment.value,
+      }
+      : {selectedResponse: this.response};
+  }
+
+  isCancelRequestType(): boolean {
+    return this.data.model.requestType === ServiceRequestTypes.CANCEL;
+  }
+
+  private isCommentRequired(): boolean {
+    return this.isCancelRequestType();
   }
 }
