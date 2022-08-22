fe96af10350003dd0a6330c21743c1d1d0062d37
BeSaRa <besara@windowslive.com>
2022-08-23 01:30:10 +0300

    fix: external user statusInfo

--- src/app/model-interceptors/org-user-interceptor.ts
+++ src/app/model-interceptors/org-user-interceptor.ts
@@ -1,18 +1,17 @@
-import {OrgUser} from '../models/org-user';
-import {generateModelAndCast, isValidAdminResult} from '../helpers/utils';
-import {AdminResult} from '../models/admin-result';
-import {DateUtils} from '../helpers/date-utils';
-import {CommonUtils} from '@app/helpers/common-utils';
-import {IModelInterceptor} from '@app/interfaces/i-model-interceptor';
+import { OrgUser } from '../models/org-user';
+import { AdminResult } from '../models/admin-result';
+import { DateUtils } from '@helpers/date-utils';
+import { CommonUtils } from '@app/helpers/common-utils';
+import { IModelInterceptor } from '@app/interfaces/i-model-interceptor';
 
 export class OrgUserInterceptor implements IModelInterceptor<OrgUser> {
   receive(model: OrgUser | any): (OrgUser | any) {
-    model.customRoleInfo = isValidAdminResult(model.customRoleInfo) ? generateModelAndCast(AdminResult, model.customRoleInfo) : model.customRoleInfo;
-    model.jobTitleInfo = isValidAdminResult(model.jobTitleInfo) ? generateModelAndCast(AdminResult, model.jobTitleInfo) : model.jobTitleInfo;
-    model.orgBranchInfo = isValidAdminResult(model.orgBranchInfo) ? generateModelAndCast(AdminResult, model.orgBranchInfo) : model.orgBranchInfo;
-    model.orgUnitInfo = isValidAdminResult(model.orgUnitInfo) ? generateModelAndCast(AdminResult, model.orgUnitInfo) : model.orgUnitInfo;
-    model.statusInfo = isValidAdminResult(model.statusInfo) ? generateModelAndCast(AdminResult, model.statusInfo) : model.statusInfo;
-    model.userTypeInfo = isValidAdminResult(model.userTypeInfo) ? generateModelAndCast(AdminResult, model.userTypeInfo) : model.userTypeInfo;
+    model.customRoleInfo = AdminResult.createInstance(model.customRoleInfo)
+    model.jobTitleInfo = AdminResult.createInstance(model.jobTitleInfo)
+    model.orgBranchInfo = AdminResult.createInstance(model.orgBranchInfo)
+    model.orgUnitInfo = AdminResult.createInstance(model.orgUnitInfo)
+    model.statusInfo = AdminResult.createInstance(model.statusInfo)
+    model.userTypeInfo = AdminResult.createInstance(model.userTypeInfo)
     model.statusDateModifiedString = model.statusDateModified ? DateUtils.getDateStringFromDate(model.statusDateModified, 'DEFAULT_DATE_FORMAT') : '';
     return model;
   }
