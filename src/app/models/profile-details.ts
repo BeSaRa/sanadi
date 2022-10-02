fdfe559a6bb3fa4da62f0dd1c426329c953d5009
AbdulQader_Q <AbdulQader_Q@hotmail.com>
2022-10-02 19:55:10 +0300

    Bug 52238

--- src/app/models/profile-details.ts
+++ src/app/models/profile-details.ts
@@ -3,6 +3,6 @@ import { Cloneable } from './cloneable';
 export class ProfielDetails extends Cloneable<ProfielDetails> {
   arName!: string;
   enName!: string;
-  profileId!: string;
-  entityId!: string;
+  profileId!: number;
+  entityId!: number;
 }
