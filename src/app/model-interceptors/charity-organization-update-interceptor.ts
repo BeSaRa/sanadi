87ab9d11b7a42fb64ab13b25fa024debfca2dec8
AbdulQader_Q <AbdulQader_Q@hotmail.com>
2022-10-04 11:28:15 +0300

    more fixes

--- src/app/model-interceptors/charity-organization-update-interceptor.ts
+++ src/app/model-interceptors/charity-organization-update-interceptor.ts
@@ -1,12 +1,15 @@
 import { DateUtils } from '@app/helpers/date-utils';
 import { IModelInterceptor } from '@app/interfaces/i-model-interceptor';
 import { AdminResult } from '@app/models/admin-result';
+import { Bylaw } from '@app/models/bylaw';
 import { CharityDecision } from '@app/models/charity-decision';
 import { CharityOrganizationUpdate } from '@app/models/charity-organization-update';
 import { CharityReport } from '@app/models/charity-report';
+import { ForeignAidClassification } from '@app/models/foreign-aid-classification';
 import { OrgMember } from '@app/models/org-member';
 import { OrganizationOfficer } from '@app/models/organization-officer';
 import { RealBeneficiary } from '@app/models/real-beneficiary';
+import { ByLawInterceptor } from './bylaw-interceptor';
 import { CharityDecisionInterceptor } from './charity-Decision-interceptor';
 import { CharityReportInterceptor } from './charity-report-interceptor';
 import { ForeignAidClassificationInterceptor } from './foreign-aid-classification-interceptor';
@@ -17,12 +20,15 @@ import { RealBeneficiaryInterceptor } from './real-beneficiary-interceptors';
 export class CharityOrganizationUpdateInterceptor implements IModelInterceptor<CharityOrganizationUpdate> {
   caseInterceptor?: IModelInterceptor<CharityOrganizationUpdate> | undefined;
   send(model: Partial<CharityOrganizationUpdate>): Partial<CharityOrganizationUpdate> {
+
     delete model.service;
     delete model.followUpService;
+    const foreignAidClassificationInterceptor = new ForeignAidClassificationInterceptor();
     const charityReportInterceptor = new CharityReportInterceptor();
     const charityDecisionInterceptor = new CharityDecisionInterceptor();
     const realBeneficiaryInterceptor = new RealBeneficiaryInterceptor();
     const membersInterceptor = new OrgMemberInterceptor();
+    const bylawInterceptor = new ByLawInterceptor()
     const organizationOfficer = new OrganizationOfficerInterceptor();
     model.realBeneficiaryList = model.realBeneficiaryList?.map(e => realBeneficiaryInterceptor.send(e) as RealBeneficiary);
     model.boardMemberList = model.boardMemberList?.map(e => membersInterceptor.send(e) as OrgMember);
@@ -36,12 +42,17 @@ export class CharityOrganizationUpdateInterceptor implements IModelInterceptor<C
     model.incomingDecisionList = model.incomingDecisionList?.map(e => charityDecisionInterceptor.send(e) as CharityDecision);
     model.outgoingDecisionList = model.outgoingDecisionList?.map(e => charityDecisionInterceptor.send(e) as CharityDecision);
     model.complianceOfficerList = model.complianceOfficerList?.map(e => organizationOfficer.send(e) as OrganizationOfficer);
+
+    model.wFClassificationList = model.wFClassificationList?.map(e => foreignAidClassificationInterceptor.send(e) as ForeignAidClassification);
     model.charityContactOfficerList = model.charityContactOfficerList?.map(e => organizationOfficer.send(e) as OrganizationOfficer);
+    model.byLawList = model.byLawList?.map(e => bylawInterceptor.send(e) as Bylaw);
     (model.lastUpdateDate && (model.lastUpdateDate = DateUtils.getDateStringFromDate(model.lastUpdateDate)));
+
     (model.firstReleaseDate && (model.firstReleaseDate = DateUtils.getDateStringFromDate(model.firstReleaseDate)))
     return model;
   }
   receive(model: CharityOrganizationUpdate): CharityOrganizationUpdate {
+    const bylawInterceptor = new ByLawInterceptor()
     const charityReportInterceptor = new CharityReportInterceptor();
     const charityDecisionInterceptor = new CharityDecisionInterceptor();
     const realBeneficiaryInterceptor = new RealBeneficiaryInterceptor();
@@ -63,7 +74,7 @@ export class CharityOrganizationUpdateInterceptor implements IModelInterceptor<C
     model.founderMemberList = model.founderMemberList?.map(e => membersInterceptor.receive(e) as OrgMember);
     model.generalAssemblyMemberList = model.generalAssemblyMemberList?.map(e => membersInterceptor.receive(e) as OrgMember);
     model.currentExecutiveManagementList = model.currentExecutiveManagementList?.map(e => membersInterceptor.receive(e) as OrgMember);
-
+    model.byLawList = model.byLawList?.map(e => bylawInterceptor.receive(e));
     model.riskReportList = model.riskReportList?.map(e => charityReportInterceptor.receive(e) as CharityReport);
     model.coordinationSupportReport = model.coordinationSupportReport?.map(e => charityReportInterceptor.receive(e) as CharityReport);
     model.incomingReportList = model.incomingReportList?.map(e => charityReportInterceptor.receive(e) as CharityReport);
