8febd0357aa6112c35aaf11f86ed2d9897ad0deb
Mahmoud Soliman <mahmoud.mohsen.soliman@gmail.com>
2022-10-05 13:15:53 +0200

    fix: admin > audit log bug

--- src/app/models/audit-log.ts
+++ src/app/models/audit-log.ts
@@ -1,6 +1,10 @@
 import {AdminResult} from './admin-result';
 import {isValidValue} from '@helpers/utils';
+import {InterceptModel} from '@decorators/intercept-model';
+import {AuditLogInterceptor} from '@app/model-interceptors/audit-log-interceptor';
+const {send, receive} = new AuditLogInterceptor();
 
+@InterceptModel({send, receive})
 export class AuditLog {
   auditId!: number;
   updatedOn!: string;
