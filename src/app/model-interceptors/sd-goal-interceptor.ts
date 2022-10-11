dd9e90f2e734b97f96bf3dfad1b968fd2453ada1
Navjot Singh <navjotsandha@gmail.com>
2022-10-11 12:39:01 +0300

    Bug 52377:Admin: SD Goals: Deactivated record appeared in DDL

--- src/app/model-interceptors/sd-goal-interceptor.ts
+++ src/app/model-interceptors/sd-goal-interceptor.ts
@@ -2,16 +2,24 @@ import {IModelInterceptor} from '@app/interfaces/i-model-interceptor';
 import {SDGoal} from '@app/models/sdgoal';
 import {FactoryService} from '@app/services/factory.service';
 import {LookupService} from '@app/services/lookup.service';
+import {CommonStatusEnum} from '@app/enums/common-status.enum';
+import {CommonUtils} from '@helpers/common-utils';
 
-export class SdGoalInterceptor implements IModelInterceptor<SDGoal>{
+export class SdGoalInterceptor implements IModelInterceptor<SDGoal> {
   receive(model: SDGoal): SDGoal {
     const lookupService = FactoryService.getService('LookupService') as LookupService;
+    if (!CommonUtils.isValidValue(model.status)) {
+      model.status = CommonStatusEnum.DEACTIVATED;
+    }
     model.statusInfo = (lookupService.listByCategory.CommonStatus.find(s => s.lookupKey == model.status)!);
     return model;
   }
 
   send(model: Partial<SDGoal>): Partial<SDGoal> {
-    model.status = model.status ? 1 : 0;
+    if (!CommonUtils.isValidValue(model.status)) {
+      model.status = CommonStatusEnum.DEACTIVATED;
+    }
+    model.status = model.status ? CommonStatusEnum.ACTIVATED : CommonStatusEnum.DEACTIVATED;
     delete model.langService;
     delete model.service;
     delete model.statusInfo;
