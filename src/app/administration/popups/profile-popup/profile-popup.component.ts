47422c52801e76aad58949a329545e47e51c62ff
AbdulQader_Q <AbdulQader_Q@hotmail.com>
2022-10-17 18:53:35 +0300

    Complete namings

--- src/app/administration/popups/profile-popup/profile-popup.component.ts
+++ src/app/administration/popups/profile-popup/profile-popup.component.ts
@@ -54,6 +54,9 @@ export class ProfilePopupComponent extends AdminGenericDialog<Profile> {
       this.services = x;
     })
   }
+  get OperationTypes() {
+    return OperationTypes;
+  }
   get basicInfoForm(): AbstractControl | null {
     return this.form.get('basicInfo');
   }
