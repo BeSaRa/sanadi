d828049dc8897e44df66a88caa2d39a26956bad0
Navjot Singh <navjotsandha@gmail.com>
2022-10-18 12:39:57 +0300

    refactor: custom menu screen

--- src/app/models/custom-menu.ts
+++ src/app/models/custom-menu.ts
@@ -1,16 +1,16 @@
-import { InterceptModel } from '@app/decorators/decorators/intercept-model';
-import { CommonStatusEnum } from '@app/enums/common-status.enum';
-import { infoSearchFields } from '@app/helpers/info-search-fields';
-import { normalSearchFields } from '@app/helpers/normal-search-fields';
-import { INames } from '@app/interfaces/i-names';
-import { CustomMenuInterceptor } from '@app/model-interceptors/custom-menu-interceptor';
-import { FactoryService } from '@app/services/factory.service';
-import { LangService } from '@app/services/lang.service';
-import { ISearchFieldsMap } from '@app/types/types';
-import { CustomValidators } from '@app/validators/custom-validators';
-import { CustomMenuService } from '@services/custom-menu.service';
-import { BaseModel } from './base-model';
-import { Lookup } from './lookup';
+import {InterceptModel} from '@app/decorators/decorators/intercept-model';
+import {CommonStatusEnum} from '@app/enums/common-status.enum';
+import {infoSearchFields} from '@app/helpers/info-search-fields';
+import {normalSearchFields} from '@app/helpers/normal-search-fields';
+import {INames} from '@app/interfaces/i-names';
+import {CustomMenuInterceptor} from '@app/model-interceptors/custom-menu-interceptor';
+import {FactoryService} from '@app/services/factory.service';
+import {LangService} from '@app/services/lang.service';
+import {ISearchFieldsMap} from '@app/types/types';
+import {CustomValidators} from '@app/validators/custom-validators';
+import {CustomMenuService} from '@services/custom-menu.service';
+import {BaseModel} from './base-model';
+import {Lookup} from './lookup';
 
 const interceptor = new CustomMenuInterceptor();
 
@@ -26,20 +26,18 @@ export class CustomMenu extends BaseModel<CustomMenu, CustomMenuService> {
   menuView!: number;
   userType!: number;
   menuURL!: string;
-  parentMenuItemId: number|null =null;
-  langService: LangService;
+  parentMenuItemId?: number;
   statusInfo!: Lookup;
 
+  // extra properties
   service!: CustomMenuService;
+  langService: LangService;
 
   searchFields: ISearchFieldsMap<CustomMenu> = {
-    ...normalSearchFields(['arName', 'enName', 'menuURL']),
+    ...normalSearchFields(['arName', 'enName']),
     ...infoSearchFields(['statusInfo']),
   };
 
-
-
-  parentId: any;
   constructor() {
     super();
     this.langService = FactoryService.getService('LangService');
@@ -49,9 +47,11 @@ export class CustomMenu extends BaseModel<CustomMenu, CustomMenuService> {
   getName(): string {
     return this[(this.langService.map.lang + 'Name') as keyof INames];
   }
+
   updateStatus(newStatus: CommonStatusEnum): any {
     return this.service.updateStatus(this.id, newStatus);
   }
+
   buildForm(controls?: boolean): any {
     const {
       arName,
@@ -67,48 +67,48 @@ export class CustomMenu extends BaseModel<CustomMenu, CustomMenuService> {
     return {
       arName: controls
         ? [
-            arName,
-            [
-              CustomValidators.required,
-              CustomValidators.maxLength(
-                CustomValidators.defaultLengths.ARABIC_NAME_MAX
-              ),
-              CustomValidators.minLength(
-                CustomValidators.defaultLengths.MIN_LENGTH
-              ),
-              CustomValidators.pattern('AR_NUM_ONE_AR'),
-            ],
-          ]
+          arName,
+          [
+            CustomValidators.required,
+            CustomValidators.maxLength(
+              CustomValidators.defaultLengths.ARABIC_NAME_MAX
+            ),
+            CustomValidators.minLength(
+              CustomValidators.defaultLengths.MIN_LENGTH
+            ),
+            CustomValidators.pattern('AR_NUM_ONE_AR'),
+          ],
+        ]
         : arName,
       enName: controls
         ? [
-            enName,
-            [
-              CustomValidators.required,
-              CustomValidators.maxLength(
-                CustomValidators.defaultLengths.ENGLISH_NAME_MAX
-              ),
-              CustomValidators.minLength(
-                CustomValidators.defaultLengths.MIN_LENGTH
-              ),
-              CustomValidators.pattern('ENG_NUM_ONE_ENG'),
-            ],
-          ]
+          enName,
+          [
+            CustomValidators.required,
+            CustomValidators.maxLength(
+              CustomValidators.defaultLengths.ENGLISH_NAME_MAX
+            ),
+            CustomValidators.minLength(
+              CustomValidators.defaultLengths.MIN_LENGTH
+            ),
+            CustomValidators.pattern('ENG_NUM_ONE_ENG'),
+          ],
+        ]
         : enName,
       status: controls ? [status, []] : status,
       menuOrder: controls
-        ? [menuOrder, [CustomValidators.required,CustomValidators.number]]
+        ? [menuOrder, [CustomValidators.required, CustomValidators.number]]
         : menuOrder,
       menuType: controls ? [menuType, [CustomValidators.required]] : menuType,
       menuView: controls ? [menuView, []] : menuView,
       userType: controls ? [userType, [CustomValidators.required]] : userType,
       menuURL: controls
         ? [
-            menuURL,
-            [
-              CustomValidators.maxLength(350),
-            ],
-          ]
+          menuURL,
+          [
+            CustomValidators.maxLength(350),
+          ],
+        ]
         : menuURL,
       parentMenuItemId: controls
         ? [parentMenuItemId, []]
