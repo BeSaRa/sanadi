3bc9343a5757273ac22bdcd5a96a09c959c93fc8
Navjot Singh <navjotsandha@gmail.com>
2022-10-30 22:18:36 +0300

    fix: urgent intervention financial notification > fix afterSave error

--- src/app/model-interceptors/urgent-intervention-financial-notification-interceptor.ts
+++ src/app/model-interceptors/urgent-intervention-financial-notification-interceptor.ts
@@ -1,11 +1,16 @@
-import { UrgentInterventionFinancialNotificationService } from '@services/urgent-intervention-financial-notification.service';
-import { AdminResult } from '@app/models/admin-result';
-import { InterventionField } from '@app/models/intervention-field';
-import { InterventionRegion } from '@app/models/intervention-region';
-import { ImplementingAgency } from '@app/models/implementing-agency';
-import { FactoryService } from '@app/services/factory.service';
-import { UrgentInterventionFinancialNotification } from '@app/models/urgent-intervention-financial-notification';
-import { IModelInterceptor } from '@contracts/i-model-interceptor';
+import {AdminResult} from '@app/models/admin-result';
+import {InterventionField} from '@app/models/intervention-field';
+import {InterventionRegion} from '@app/models/intervention-region';
+import {ImplementingAgency} from '@app/models/implementing-agency';
+import {UrgentInterventionFinancialNotification} from '@app/models/urgent-intervention-financial-notification';
+import {IModelInterceptor} from '@contracts/i-model-interceptor';
+import {ImplementingAgencyInterceptor} from '@app/model-interceptors/implementing-agency-interceptor';
+import {InterventionRegionInterceptor} from '@app/model-interceptors/intervention-region-interceptor';
+import {InterventionFieldInterceptor} from '@app/model-interceptors/intervention-field-interceptor';
+
+const implementingAgencyInterceptor: IModelInterceptor<ImplementingAgency> = new ImplementingAgencyInterceptor();
+const interventionRegionInterceptor: IModelInterceptor<InterventionRegion> = new InterventionRegionInterceptor();
+const interventionFieldInterceptor: IModelInterceptor<InterventionField> = new InterventionFieldInterceptor();
 
 export class UrgentInterventionFinancialNotificationInterceptor implements IModelInterceptor<UrgentInterventionFinancialNotification> {
   receive(model: UrgentInterventionFinancialNotification): UrgentInterventionFinancialNotification {
@@ -13,29 +18,27 @@ export class UrgentInterventionFinancialNotificationInterceptor implements IMode
     model.beneficiaryCountryInfo && (model.beneficiaryCountryInfo = AdminResult.createInstance(model.beneficiaryCountryInfo));
     model.executionCountryInfo && (model.executionCountryInfo = AdminResult.createInstance(model.executionCountryInfo));
 
-    let service: UrgentInterventionFinancialNotificationService = FactoryService.getService('UrgentInterventionFinancialNotificationService');
     if (model.implementingAgencyList && model.implementingAgencyList.length > 0) {
-      model.implementingAgencyList = model.implementingAgencyList.map(x => service.implementingAgencyInterceptor.receive(new ImplementingAgency().clone(x)));
+      model.implementingAgencyList = model.implementingAgencyList.map(x => implementingAgencyInterceptor.receive(new ImplementingAgency().clone(x)));
     }
     if (model.interventionRegionList && model.interventionRegionList.length > 0) {
-      model.interventionRegionList = model.interventionRegionList.map(x => service.interventionRegionInterceptor.receive(new InterventionRegion().clone(x)));
+      model.interventionRegionList = model.interventionRegionList.map(x => interventionRegionInterceptor.receive(new InterventionRegion().clone(x)));
     }
     if (model.interventionFieldList && model.interventionFieldList.length > 0) {
-      model.interventionFieldList = model.interventionFieldList.map(x => service.interventionFieldInterceptor.receive(new InterventionField().clone(x)));
+      model.interventionFieldList = model.interventionFieldList.map(x => interventionFieldInterceptor.receive(new InterventionField().clone(x)));
     }
     return model;
   }
 
   send(model: Partial<UrgentInterventionFinancialNotification>): Partial<UrgentInterventionFinancialNotification> {
-    let service: UrgentInterventionFinancialNotificationService = FactoryService.getService('UrgentInterventionFinancialNotificationService');
     if (model.implementingAgencyList && model.implementingAgencyList.length > 0) {
-      model.implementingAgencyList = model.implementingAgencyList.map(x => service.implementingAgencyInterceptor.send(x) as ImplementingAgency);
+      model.implementingAgencyList = model.implementingAgencyList.map(x => implementingAgencyInterceptor.send(new ImplementingAgency().clone(x)) as ImplementingAgency);
     }
     if (model.interventionRegionList && model.interventionRegionList.length > 0) {
-      model.interventionRegionList = model.interventionRegionList.map(x => service.interventionRegionInterceptor.send(x) as InterventionRegion);
+      model.interventionRegionList = model.interventionRegionList.map(x => interventionRegionInterceptor.send(new InterventionRegion().clone(x)) as InterventionRegion);
     }
     if (model.interventionFieldList && model.interventionFieldList.length > 0) {
-      model.interventionFieldList = model.interventionFieldList.map(x => service.interventionFieldInterceptor.send(x) as InterventionField);
+      model.interventionFieldList = model.interventionFieldList.map(x => interventionFieldInterceptor.send(new InterventionField().clone(x)) as InterventionField);
     }
     model.amount && (model.amount = +model.amount);
     delete model.requestTypeInfo;
--- src/app/services/urgent-intervention-financial-notification.service.ts
+++ src/app/services/urgent-intervention-financial-notification.service.ts
@@ -4,28 +4,22 @@ import {
 import {
   UrgentInterventionFinancialNotificationInterceptor
 } from './../model-interceptors/urgent-intervention-financial-notification-interceptor';
-import { InterventionRegion } from '@app/models/intervention-region';
-import { InterventionField } from '@app/models/intervention-field';
-import { InterventionFieldInterceptor } from '@app/model-interceptors/intervention-field-interceptor';
-import { InterventionRegionInterceptor } from '@app/model-interceptors/intervention-region-interceptor';
-import { ImplementingAgencyInterceptor } from '@app/model-interceptors/implementing-agency-interceptor';
-import { ImplementingAgency } from '@app/models/implementing-agency';
-import { Observable } from 'rxjs';
-import { UrgentInterventionAnnouncementResult } from '../models/urgent-intervention-announcement-result';
-import { UrgentInterventionAnnouncementSearchCriteria } from '../models/urgent-intervention-announcement-search-criteria';
-import { LicenseService } from './license.service';
-import { FactoryService } from './factory.service';
-import { Injectable } from '@angular/core';
-import { UrgentInterventionFinancialNotification } from '@app/models/urgent-intervention-financial-notification';
-import { HttpClient } from '@angular/common/http';
-import { DomSanitizer } from '@angular/platform-browser';
-import { ILanguageKeys } from '@app/interfaces/i-language-keys';
-import { DialogService } from './dialog.service';
-import { DynamicOptionsService } from './dynamic-options.service';
-import { UrlService } from './url.service';
-import { IModelInterceptor } from '@app/interfaces/i-model-interceptor';
-import { BaseGenericEService } from "@app/generics/base-generic-e-service";
-import { CastResponseContainer } from "@decorators/cast-response";
+import {Observable} from 'rxjs';
+import {UrgentInterventionAnnouncementResult} from '../models/urgent-intervention-announcement-result';
+import {UrgentInterventionAnnouncementSearchCriteria} from '../models/urgent-intervention-announcement-search-criteria';
+import {LicenseService} from './license.service';
+import {FactoryService} from './factory.service';
+import {Injectable} from '@angular/core';
+import {UrgentInterventionFinancialNotification} from '@app/models/urgent-intervention-financial-notification';
+import {HttpClient} from '@angular/common/http';
+import {DomSanitizer} from '@angular/platform-browser';
+import {ILanguageKeys} from '@app/interfaces/i-language-keys';
+import {DialogService} from './dialog.service';
+import {DynamicOptionsService} from './dynamic-options.service';
+import {UrlService} from './url.service';
+import {IModelInterceptor} from '@app/interfaces/i-model-interceptor';
+import {BaseGenericEService} from '@app/generics/base-generic-e-service';
+import {CastResponseContainer} from '@decorators/cast-response';
 
 @CastResponseContainer({
   $default: {
@@ -43,35 +37,36 @@ export class UrgentInterventionFinancialNotificationService extends BaseGenericE
   searchColumns: string[] = ['fullSerial', 'requestTypeInfo', 'createdOn', 'caseStatus', 'ouInfo', 'subject'];
   selectLicenseDisplayColumns = ['beneficiaryCountry', 'executionCountry', 'subject', 'licenseNumber', 'actions'];
 
-  implementingAgencyInterceptor: IModelInterceptor<ImplementingAgency> = new ImplementingAgencyInterceptor();
-  interventionRegionInterceptor: IModelInterceptor<InterventionRegion> = new InterventionRegionInterceptor();
-  interventionFieldInterceptor: IModelInterceptor<InterventionField> = new InterventionFieldInterceptor();
-  constructor(
-    private urlService: UrlService,
-    public http: HttpClient,
-    public dialog: DialogService,
-    private licenseService: LicenseService,
-    public domSanitizer: DomSanitizer,
-    public dynamicService: DynamicOptionsService
-  ) {
-    super()
+  constructor(private urlService: UrlService,
+              public http: HttpClient,
+              public dialog: DialogService,
+              private licenseService: LicenseService,
+              public domSanitizer: DomSanitizer,
+              public dynamicService: DynamicOptionsService) {
+    super();
     FactoryService.registerService('UrgentInterventionFinancialNotificationService', this);
   }
+
   _getUrlService(): UrlService {
     return this.urlService;
   }
+
   _getURLSegment(): string {
-    return this._getUrlService().URLS.URGENT_INTERVENTION_FINANCIAL_NOTIFICATION
+    return this._getUrlService().URLS.URGENT_INTERVENTION_FINANCIAL_NOTIFICATION;
   }
+
   _getModel() {
     return UrgentInterventionFinancialNotification;
   }
+
   _getInterceptor(): Partial<IModelInterceptor<UrgentInterventionFinancialNotification>> {
-    return this.interceptor
+    return this.interceptor;
   }
+
   getSearchCriteriaModel<S extends UrgentInterventionFinancialNotification>(): UrgentInterventionFinancialNotification {
     return new UrgentInterventionFinancialNotificationSearchCriteria();
   }
+
   getCaseComponentName(): string {
     return 'UrgentInterventionFinancialNotificationComponent';
   }
