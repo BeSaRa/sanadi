730d838903eae21f3e679815b8dc246246c45ce3
Abdulaziz-eblacorp <ashahrour@eblacorp.com>
2022-11-01 12:57:03 +0300

    fix -- 53425:NPO Update: Update request: data should be allowed to edit for license specialist user "Creator" in case returned

--- src/app/modules/general-services/pages/npo-management/npo-management.component.ts
+++ src/app/modules/general-services/pages/npo-management/npo-management.component.ts
@@ -152,9 +152,9 @@ NpoManagementService
       return;
     }
     if (this.openFrom === OpenFrom.USER_INBOX) {
-      if (this.employeeService.isCharityManager() || this.employeeService.isLicensingUser()) {
+      if (this.employeeService.isCharityManager()) {
         this.readonly = false;
-      } else if (this.employeeService.isCharityUser()) {
+      } else if (this.employeeService.isCharityUser() || this.employeeService.isLicensingUser()) {
         this.readonly = !this.model.isReturned();
       }
     } else if (this.openFrom === OpenFrom.TEAM_INBOX) {
