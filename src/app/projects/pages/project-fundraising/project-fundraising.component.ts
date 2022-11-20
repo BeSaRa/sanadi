3d9c997ba2e43e22bcf943c2dc5032145f30894b
BeSaRa <besara@windowslive.com>
2022-11-20 12:09:48 +0300

    prepare structure of Project fundraising

---
+++ src/app/projects/pages/project-fundraising/project-fundraising.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'project-fundraising',
+  templateUrl: './project-fundraising.component.html',
+  styleUrls: ['./project-fundraising.component.scss']
+})
+export class ProjectFundraisingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
