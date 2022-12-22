7168595fd80fd6a8565e665594f97185d996b300
Mahmoud Soliman <mahmoud.mohsen.soliman@gmail.com>
2022-12-22 14:27:24 +0200

    Bug 54132:Admin: Profile: Profile ID should be unique and not allowed to duplicate

--- src/app/interfaces/i-language-keys.ts
+++ src/app/interfaces/i-language-keys.ts
@@ -1,4 +1,5 @@
 export interface ILanguageKeys {
+  profile_code_already_exists: string;
   make_sure_that_x_sum_equal_to_target_amount: string;
   take_the_remaining: string;
   btn_add_all: string;
