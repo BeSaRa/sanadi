937cc8328b00854e8e56e846d4be722836788686
Abdulaziz-eblacorp <ashahrour@eblacorp.com>
2022-12-27 20:30:41 +0300

    fix -- load org for external users on FCP, hide send to depr and custom approve form for creator on FCP, set validation for permanint

--- src/app/interfaces/i-language-keys.ts
+++ src/app/interfaces/i-language-keys.ts
@@ -1699,4 +1699,5 @@ export interface ILanguageKeys {
   removed: string;
   msg_field_order_is_already_exist: string;
   msg_field_identifier_is_already_exist: string;
+  approval_foreign_countries_projects: string;
 }
