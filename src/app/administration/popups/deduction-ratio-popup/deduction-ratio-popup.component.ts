1102650afb80b06c650ee6c676f8a8889143e99f
BeSaRa <besara@windowslive.com>
2023-01-03 06:42:30 +0300

    fix: 54581:deduction ratio: when create new request with "زكاة" "تلقى غير مشروط" deduction ratio added by admin didn't display

--- src/app/administration/popups/deduction-ratio-popup/deduction-ratio-popup.component.ts
+++ src/app/administration/popups/deduction-ratio-popup/deduction-ratio-popup.component.ts
@@ -1,20 +1,23 @@
-import { Component, Inject } from '@angular/core';
-import { AbstractControl, UntypedFormBuilder, UntypedFormGroup, ValidationErrors } from '@angular/forms';
-import { OperationTypes } from '@app/enums/operation-types.enum';
-import { UserTypes } from '@app/enums/user-types.enum';
-import { AdminGenericDialog } from '@app/generics/admin-generic-dialog';
-import { IDialogData } from '@app/interfaces/i-dialog-data';
-import { DeductionRatioItem } from '@app/models/deduction-ratio-item';
-import { Lookup } from '@app/models/lookup';
-import { Profile } from '@app/models/profile';
-import { LangService } from '@app/services/lang.service';
-import { LookupService } from '@app/services/lookup.service';
-import { ProfileService } from '@app/services/profile.service';
-import { ToastService } from '@app/services/toast.service';
-import { DialogRef } from '@app/shared/models/dialog-ref';
-import { DIALOG_DATA_TOKEN } from '@app/shared/tokens/tokens';
-import { Observable, of } from 'rxjs';
-import {catchError, filter, map, switchMap, take, takeUntil} from 'rxjs/operators';
+import {Component, Inject} from '@angular/core';
+import {AbstractControl, UntypedFormBuilder, UntypedFormGroup, ValidationErrors} from '@angular/forms';
+import {OperationTypes} from '@app/enums/operation-types.enum';
+import {UserTypes} from '@app/enums/user-types.enum';
+import {AdminGenericDialog} from '@app/generics/admin-generic-dialog';
+import {IDialogData} from '@app/interfaces/i-dialog-data';
+import {DeductionRatioItem} from '@app/models/deduction-ratio-item';
+import {Lookup} from '@app/models/lookup';
+import {LangService} from '@app/services/lang.service';
+import {LookupService} from '@app/services/lookup.service';
+import {ToastService} from '@app/services/toast.service';
+import {DialogRef} from '@app/shared/models/dialog-ref';
+import {DIALOG_DATA_TOKEN} from '@app/shared/tokens/tokens';
+import {Observable, of, Subject} from 'rxjs';
+import {distinctUntilChanged, filter, map, pairwise, startWith, switchMap, takeUntil, tap} from 'rxjs/operators';
+import {ProjectPermitTypes} from "@app/enums/project-permit-types";
+import {ProjectWorkArea} from "@app/enums/project-work-area";
+import {DialogService} from "@services/dialog.service";
+import {UserClickOn} from "@app/enums/user-click-on.enum";
+import {CustomValidators} from "@app/validators/custom-validators";
 
 @Component({
   selector: 'deduction-ratio-popup',
@@ -27,10 +30,15 @@ export class DeductionRatioPopupComponent extends AdminGenericDialog<DeductionRa
   model!: DeductionRatioItem;
   operation: OperationTypes;
   saveVisible = true;
-  userTypes: Lookup[] = this.lookupService.listByCategory.UserType.filter((x:any) => x.lookupKey !== UserTypes.INTEGRATION_USER);
-  workAreas: Lookup[] = this.lookupService.listByCategory.ProjectWorkArea
-  permitTypes: Lookup[] = this.lookupService.listByCategory.ProjectPermitType
-  profileTypes: Lookup[] = this.lookupService.listByCategory.ProfileType
+  userTypes: Lookup[] = this.lookupService.listByCategory.UserType.filter((x: any) => x.lookupKey !== UserTypes.INTEGRATION_USER);
+  workAreas: Lookup[] = this.lookupService.listByCategory.ProjectWorkArea.slice().sort((a, b) => a.lookupKey - b.lookupKey)
+  permitTypes: Lookup[] = this.lookupService.listByCategory.ProjectPermitType.slice().sort((a, b) => a.lookupKey - b.lookupKey)
+  profileTypes: Lookup[] = this.lookupService.listByCategory.ProfileType.slice().sort((a, b) => a.lookupKey - b.lookupKey)
+
+
+  permitTypeChangeWarn: Subject<{ warn: boolean, oldValue: ProjectPermitTypes }> = new Subject()
+  workAreaChangeWarn: Subject<{ warn: boolean, oldValue: ProjectWorkArea }> = new Subject()
+
   // profileList: Profile[] = [];
 
   constructor(public dialogRef: DialogRef,
@@ -38,9 +46,10 @@ export class DeductionRatioPopupComponent extends AdminGenericDialog<DeductionRa
               public lang: LangService,
               @Inject(DIALOG_DATA_TOKEN) data: IDialogData<DeductionRatioItem>,
               private toast: ToastService,
+              private dialog: DialogService,
               private lookupService: LookupService,
               // private profileService:ProfileService,
-              ) {
+  ) {
     super();
     this.model = data.model;
     this.operation = data.operation;
@@ -51,25 +60,29 @@ export class DeductionRatioPopupComponent extends AdminGenericDialog<DeductionRa
   }
 
   buildForm(): void {
-    this.form = this.fb.group(this.model.buildForm(true), { validator: this.validateMinMax });
+    this.form = this.fb.group(this.model.buildForm(true), {validators: [this.validateMinMax]});
     if (this.operation === OperationTypes.VIEW) {
       this.form.disable();
       this.saveVisible = false;
       this.validateFieldsVisible = false;
     }
+
+    this.listenToPermitTypeChanges()
+    this.listenToPermitTypeWarn()
+    this.listenToWorkAreaChanges()
+    this.listenToWorkAreaWarn()
+    this.checkWorkAreaValidator()
   }
+
   validateMinMax(control: AbstractControl): ValidationErrors | null {
     const minValue = control.get("minLimit")?.value;
     const maxValue = control.get("maxLimit")?.value;
-    if (minValue&&maxValue&& +minValue > +maxValue) { return { 'minBiggerThanMax': true } }
+    if (minValue && maxValue && +minValue > +maxValue) {
+      return {'minBiggerThanMax': true}
+    }
     return null
   }
 
-  workAreaReadOnly(){
-    const permitTypeValue = this.form.get('permitType')?.value
-    return permitTypeValue == 3 || permitTypeValue == 4 
-  }
-  
   beforeSave(model: DeductionRatioItem, form: UntypedFormGroup): Observable<boolean> | boolean {
     return form.valid;
   }
@@ -104,19 +117,101 @@ export class DeductionRatioPopupComponent extends AdminGenericDialog<DeductionRa
 
   destroyPopup(): void {
   }
-  // private _loadProfiles(): void {
-  //   this.profileService.loadAsLookups()
-  //     .pipe(
-  //       takeUntil(this.destroy$),
-  //       catchError(() => {
-  //         return of([]);
-  //       })
-  //     )
-  //     .subscribe((result) => this.profileList = result);
-  // }
-  NameNotValid(){
-    const arName = this.form.get('arName')
-    const enName = this.form.get('enName')
-    return (arName?.invalid && (arName?.dirty || arName?.touched))|| (enName?.invalid && (enName?.dirty || enName?.touched))
+
+  get workArea(): AbstractControl {
+    return this.form.get('workArea')! as AbstractControl
+  }
+
+  get permitType(): AbstractControl {
+    return this.form.get('permitType') as AbstractControl
+  }
+
+  listenToWorkAreaChanges(): void {
+    this.workArea.valueChanges
+      .pipe(takeUntil(this.destroy$))
+      .pipe(distinctUntilChanged())
+      .pipe(startWith<number, number>(this.workArea.value))
+      .pipe(pairwise())
+      .pipe(map(([oldValue, newValue]: [ProjectWorkArea, ProjectWorkArea]) => {
+        return {
+          warn: newValue && [ProjectPermitTypes.CHARITY, ProjectPermitTypes.UNCONDITIONAL_RECEIVE].includes(this.permitType.value),
+          oldValue
+        }
+      }))
+      .subscribe(this.workAreaChangeWarn)
+  }
+
+  listenToPermitTypeChanges(): void {
+    this.permitType.valueChanges
+      .pipe(takeUntil(this.destroy$))
+      .pipe(distinctUntilChanged())
+      .pipe(startWith<number, number>(this.permitType.value))
+      .pipe(pairwise())
+      .pipe(map(([oldValue, newValue]: [ProjectPermitTypes, ProjectPermitTypes]) => {
+        return {
+          warn: [ProjectPermitTypes.UNCONDITIONAL_RECEIVE, ProjectPermitTypes.CHARITY].includes(newValue) && !!this.workArea.value,
+          oldValue
+        }
+      }))
+      .subscribe(this.permitTypeChangeWarn)
+  }
+
+  listenToPermitTypeWarn(): void {
+    this.permitTypeChangeWarn
+      .pipe(takeUntil(this.destroy$))
+      .pipe(
+        switchMap(({warn, oldValue}) => {
+          return warn ? this.dialog.confirm(this.lang.map.the_selected_x_not_match_with_selected_y_and_y_will_be_erased.change({
+              x: this.lang.map.permit_type,
+              y: this.lang.map.work_area
+            }))
+              .onAfterClose$.pipe(map(v => ({clearValue: v === UserClickOn.YES, oldValue})))
+            : of(false).pipe(tap(() => this.checkWorkAreaValidator(true)), filter((v) => v), map(v => ({
+              clearValue: v,
+              oldValue
+            })))
+        })
+      )
+      .subscribe(({clearValue, oldValue}) => {
+        if (clearValue) {
+          this.workArea.setValue(null, {emitEvent: false})
+        } else {
+          this.permitType.setValue(oldValue, {emitEvent: false})
+        }
+        this.checkWorkAreaValidator(true)
+      })
+  }
+
+  listenToWorkAreaWarn(): void {
+    this.workAreaChangeWarn
+      .pipe(takeUntil(this.destroy$))
+      .pipe(
+        switchMap(({warn, oldValue}) => {
+          return warn ? this.dialog.confirm(this.lang.map.the_selected_x_not_match_with_selected_y_and_y_will_be_erased.change({
+              x: this.lang.map.work_area,
+              y: this.lang.map.permit_type
+            }))
+              .onAfterClose$.pipe(map(v => ({clearValue: v === UserClickOn.YES, oldValue})))
+            : of(false).pipe(tap(() => this.checkWorkAreaValidator(true)), filter((v) => v), map(v => ({
+              clearValue: v,
+              oldValue
+            })))
+        })
+      )
+      .subscribe(({clearValue, oldValue}) => {
+        if (clearValue) {
+          this.permitType.setValue(null, {emitEvent: false})
+        } else {
+          this.workArea.setValue(oldValue, {emitEvent: false})
+        }
+        this.checkWorkAreaValidator()
+      })
+  }
+
+  private checkWorkAreaValidator(emitEvent: boolean = false): void {
+    const value: ProjectPermitTypes = this.permitType.value;
+    const required = [ProjectPermitTypes.SINGLE_TYPE_PROJECT, ProjectPermitTypes.SECTIONAL_BASKET].includes(value) || !value;
+    this.workArea.setValidators(required ? CustomValidators.required : null)
+    this.workArea.updateValueAndValidity({emitEvent})
   }
 }
