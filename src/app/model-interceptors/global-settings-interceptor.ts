eeea82d75176a935794a85c336fddd214caf409e
Mahmoud Soliman <mahmoud.mohsen.soliman@gmail.com>
2023-01-17 21:16:31 +0200

    finish global settings

--- src/app/model-interceptors/global-settings-interceptor.ts
+++ src/app/model-interceptors/global-settings-interceptor.ts
@@ -3,10 +3,20 @@ import {GlobalSettings} from '@models/global-settings';
 
 export class GlobalSettingsInterceptor implements IModelInterceptor<GlobalSettings> {
   send(model: Partial<GlobalSettings>): Partial<GlobalSettings> {
+    model.fileType = JSON.stringify(model.fileTypeArr);
     return model;
   }
 
   receive(model: GlobalSettings): GlobalSettings {
+    let fileTypeArr: number[];
+    try {
+      fileTypeArr = JSON.parse(model.fileType);
+    }
+    catch (err) {
+      fileTypeArr = [];
+    }
+    model.fileTypeArr = fileTypeArr;
+
     return model;
   }
 }
