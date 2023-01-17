7f4b8931733c27d1337b8a5eed3817642015684f
Mamoun-Hamed <108342857+Mamoun-Hamed@users.noreply.github.com>
2023-01-17 12:30:37 +0200

    temp remove project implementation form inbox service

--- src/app/services/inbox.service.ts
+++ src/app/services/inbox.service.ts
@@ -114,7 +114,6 @@ export class InboxService {
     private generalAssociationMeetingAttendanceService: GeneralAssociationMeetingAttendanceService,
     private organizationsEntitiesSupportService:OrganizationsEntitiesSupportService,
     private financialTransferLicensingService:FinancialTransferLicensingService,
-    private projectImplementationService: ProjectImplementationService
     ) {
     FactoryService.registerService('InboxService', this);
     // register all e-services that we need.
@@ -150,7 +149,6 @@ export class InboxService {
     this.services.set(CaseTypes.PROJECT_FUNDRAISING, this.projectFundraisingService);
     this.services.set(CaseTypes.ORGANIZATION_ENTITIES_SUPPORT, this.organizationsEntitiesSupportService);
     this.services.set(CaseTypes.FINANCIAL_TRANSFERS_LICENSING, this.financialTransferLicensingService);
-    this.services.set(CaseTypes.PROJECT_IMPLEMENTATION, this.projectImplementationService);
   }
 
   @CastResponse(() => QueryResultSet)
