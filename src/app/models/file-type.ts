e341d81fec8a39b70b42ec28ec0ffdbe536333e3
Mahmoud Soliman <mahmoud.mohsen.soliman@gmail.com>
2023-01-17 21:16:31 +0200

    finish global settings

---
+++ src/app/models/file-type.ts
@@ -0,0 +1,24 @@
+import {SearchableCloneable} from '@models/searchable-cloneable';
+import {INames} from '@contracts/i-names';
+import {FactoryService} from '@services/factory.service';
+import {LangService} from '@services/lang.service';
+
+export class FileType extends SearchableCloneable<FileType> {
+  id!: number;
+  extension!: string;
+  description!: string;
+  mimeType!: string;
+  arName!: string;
+  enName!: string;
+  size!: number;
+  langService: LangService;
+
+  constructor() {
+    super();
+    this.langService = FactoryService.getService('LangService');
+  }
+
+  getName() {
+    return this[(this.langService?.map.lang + 'Name') as keyof INames] || '';
+  }
+}
