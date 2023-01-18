9e8f41145a52f883d65ecb0e2aa7756ea5317947
BeSaRa <besara@windowslive.com>
2023-01-18 10:30:03 +0300

    encode the menu item urls

--- src/app/modules/dynamic-menus/dynamic-menu-details/dynamic-menu-details.component.ts
+++ src/app/modules/dynamic-menus/dynamic-menu-details/dynamic-menu-details.component.ts
@@ -76,7 +76,7 @@ export class DynamicMenuDetailsComponent implements OnInit {
     } else if (CommonUtils.isValidValue(parentId)) {
       url = this._finalizeUrl(Number(parentId));
     }
-    this.safeUrl = this.domSanitizer.bypassSecurityTrustResourceUrl(url);
+    this.safeUrl = this.domSanitizer.bypassSecurityTrustResourceUrl(encodeURIComponent(url));
   }
 
   private listenToLangChange(): void {
