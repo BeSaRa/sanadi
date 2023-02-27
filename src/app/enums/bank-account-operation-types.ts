926367ffa4bc9a239920f0d2cc5eb5ed7804362e
Abdulaziz-eblacorp <ashahrour@eblacorp.com>
2023-02-27 13:19:52 +0300

    Bug 54551:82 CR - internal banking account - change the transaction type name based on the selected request type

--- src/app/enums/bank-account-operation-types.ts
+++ src/app/enums/bank-account-operation-types.ts
@@ -1,5 +1,10 @@
 export enum BankAccountOperationTypes {
+  Account = 1,
   NEW_ACCOUNT = 1,
   MERGE = 2,
   INACTIVE = 3
+}
+export enum BankAccountEnNameKeys {
+  Account = 'Account',
+  NEW_ACCOUNT = 'NEW_ACCOUNT'
 }
