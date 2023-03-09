c6e066094796ce6306e86609c74b6037ec89c0d8
Navjot Singh <navjotsandha@gmail.com>
2023-03-09 21:58:35 +0300

    mouseEnterLeaveDirective > add targetElementRef (HTMLElement)

--- src/app/shared/directives/mouse-enter-leave.directive.ts
+++ src/app/shared/directives/mouse-enter-leave.directive.ts
@@ -5,17 +5,29 @@ import {Directive, ElementRef, HostListener, Input} from '@angular/core';
 })
 export class MouseEnterLeaveDirective {
   @Input('mouseEnterLeave') elementClass = '';
+  /**
+   * @description Reference to the target element to be affected by mouseEnterLeave
+   */
+  @Input() targetElementRef?: HTMLElement;
 
   constructor(private element: ElementRef) {
   }
 
   @HostListener('mouseenter')
   mouseEnter(): void {
+    if (this.targetElementRef) {
+      this.targetElementRef.classList.add(this.elementClass);
+      return;
+    }
     this.element.nativeElement.classList.add(this.elementClass);
   }
 
   @HostListener('mouseleave')
   mouseLeave(): void {
+    if (this.targetElementRef) {
+      this.targetElementRef.classList.remove(this.elementClass);
+      return;
+    }
     this.element.nativeElement.classList.remove(this.elementClass);
   }
 }
