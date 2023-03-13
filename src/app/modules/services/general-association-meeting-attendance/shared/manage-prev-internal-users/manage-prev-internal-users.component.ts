399d8e94b034ba540eb27cca7c64c5edc0a4924b
Abdulaziz-eblacorp <ashahrour@eblacorp.com>
2023-03-13 13:46:39 +0300

    update -- add old internal members for meneral mettings

---
+++ src/app/modules/services/general-association-meeting-attendance/shared/manage-prev-internal-users/manage-prev-internal-users.component.ts
@@ -0,0 +1,38 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { LangService } from '@services/lang.service';
+import { GeneralAssociationMeetingAttendanceService } from '@services/general-association-meeting-attendance.service';
+import { GeneralAssociationInternalMember } from '@app/models/general-association-internal-member';
+import { GeneralAssociationInternalMemberTypeEnum } from '@app/enums/general-association-internal-member-type-enum';
+import { GeneralAssociationMeetingAttendance } from '@app/models/general-association-meeting-attendance';
+import { MeetingAttendanceReport } from '@app/models/meeting-attendance-report';
+import { GeneralMeetingAttendanceNote } from '@app/models/general-meeting-attendance-note';
+
+@Component({
+  selector: 'manage-prev-internal-users',
+  templateUrl: './manage-prev-internal-users.component.html',
+  styleUrls: ['./manage-prev-internal-users.component.scss']
+})
+export class ManagePrevInternalUsersComponent implements OnInit {
+  @Input() model!: GeneralAssociationMeetingAttendance;
+  @Input() selectedInternalUsers: GeneralAssociationInternalMember[] = [];
+  @Input() meetingReport!: MeetingAttendanceReport;
+  @Input() generalNotes: GeneralMeetingAttendanceNote[] = [];
+
+  selectedInternalUser!: GeneralAssociationInternalMember | null;
+
+  membersDisplayedColumns: string[] = ['index', 'arabicName', 'englishName', 'isDecisionMaker', 'actions'];
+  internalUserType = GeneralAssociationInternalMemberTypeEnum;
+
+  constructor(
+    public lang: LangService,
+    private generalAssociationMeetingService: GeneralAssociationMeetingAttendanceService) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  viewMemberCommentsAndNotes(event: MouseEvent, model: GeneralAssociationInternalMember) {
+    event.preventDefault();
+    this.generalAssociationMeetingService.openViewMemberCommentsAndNotesDialog(model, this.meetingReport, this.generalNotes, model.userId, this.model?.id);
+  }
+}
