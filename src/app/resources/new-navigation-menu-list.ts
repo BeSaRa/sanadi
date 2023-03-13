59bfffe5ef1341bc15ae3538b1418532f90e1851
Navjot Singh <navjotsandha@gmail.com>
2023-03-13 11:11:50 +0300

    fix: training partner > missing menu

--- src/app/resources/new-navigation-menu-list.ts
+++ src/app/resources/new-navigation-menu-list.ts
@@ -2614,6 +2614,18 @@ export const newNavigationMenuList = [
     group: 'administration',
     itemOrder: 26,
     svg: null
-  }
-
+  },
+  {
+    id: 88,
+    langKey: 'menu_training_program_partner',
+    path: '/home/administration/training-program-partner',
+    icon: 'mdi-badge-account-horizontal-outline',
+    isSvg: false,
+    permission: PermissionsEnum.TRAINING_PROGRAM_PARTNER,
+    permissionGroup: null,
+    parent: 6,
+    group: 'administration',
+    itemOrder: 22,
+    svg: null
+  },
 ];
