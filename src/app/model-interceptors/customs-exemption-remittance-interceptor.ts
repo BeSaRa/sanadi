b6609288b9161dddaf6b84209bf3eb6aa6c0a40b
Navjot Singh <navjotsandha@gmail.com>
2023-04-12 10:58:40 +0300

    case audit history - customs exemption remittance

--- src/app/model-interceptors/customs-exemption-remittance-interceptor.ts
+++ src/app/model-interceptors/customs-exemption-remittance-interceptor.ts
@@ -1,28 +1,39 @@
-import { WFResponseType } from '@app/enums/wfresponse-type.enum';
-import { CommonCaseStatus } from '@app/enums/common-case-status.enum';
+import {WFResponseType} from '@app/enums/wfresponse-type.enum';
+import {CommonCaseStatus} from '@app/enums/common-case-status.enum';
 import {IModelInterceptor} from '@app/interfaces/i-model-interceptor';
 import {AdminResult} from '@app/models/admin-result';
 import {CustomsExemptionRemittance} from '@app/models/customs-exemption-remittance';
 
 export class CustomsExemptionRemittanceInterceptor implements IModelInterceptor<CustomsExemptionRemittance> {
   send(model: Partial<CustomsExemptionRemittance>): Partial<CustomsExemptionRemittance> {
-    delete model.requestTypeInfo;
-    delete model.shipmentSourceInfo;
-    delete model.shipmentCarrierInfo;
-    delete model.receiverNameInfo;
+    CustomsExemptionRemittanceInterceptor._deleteBeforeSend(model);
     return model;
   }
+
   receive(model: CustomsExemptionRemittance): CustomsExemptionRemittance {
     model.requestTypeInfo = AdminResult.createInstance(model.requestTypeInfo);
     model.shipmentSourceInfo = AdminResult.createInstance(model.shipmentSourceInfo);
     model.shipmentCarrierInfo = AdminResult.createInstance(model.shipmentCarrierInfo);
     model.receiverNameInfo = AdminResult.createInstance(model.receiverNameInfo);
+    model.receiverTypeInfo = AdminResult.createInstance(model.receiverTypeInfo);
+    model.countryInfo = AdminResult.createInstance(model.countryInfo);
+    model.linkedProjectInfo = AdminResult.createInstance(model.linkedProjectInfo);
 
-    if(model.getCaseStatus() ===  CommonCaseStatus.CANCELLED){
-      if(!!model.taskDetails?.responses){
+    if (model.getCaseStatus() === CommonCaseStatus.CANCELLED) {
+      if (!!model.taskDetails?.responses) {
         model.taskDetails.responses = [WFResponseType.CLOSE]
       }
     }
     return model;
+  }
+
+  private static _deleteBeforeSend(model: Partial<CustomsExemptionRemittance>): void {
+    delete model.requestTypeInfo;
+    delete model.shipmentSourceInfo;
+    delete model.shipmentCarrierInfo;
+    delete model.receiverNameInfo;
+    delete model.receiverTypeInfo;
+    delete model.countryInfo;
+    delete model.linkedProjectInfo;
   }
 }
--- src/app/models/customs-exemption-remittance.ts
+++ src/app/models/customs-exemption-remittance.ts
@@ -4,7 +4,7 @@ import {infoSearchFields} from '@app/helpers/info-search-fields';
 import {normalSearchFields} from '@app/helpers/normal-search-fields';
 import {FactoryService} from '@app/services/factory.service';
 import {CustomsExemptionRemittanceService} from '@services/customs-exemption-remittance.service';
-import {ISearchFieldsMap} from '@app/types/types';
+import {ControlValueLabelLangKey, ISearchFieldsMap} from '@app/types/types';
 import {CustomValidators} from '@app/validators/custom-validators';
 import {AdminResult} from './admin-result';
 import {CaseModel} from './case-model';
@@ -15,6 +15,7 @@ import {mixinRequestType} from '@app/mixins/mixin-request-type';
 import {HasRequestType} from '@app/interfaces/has-request-type';
 import {InterceptModel} from '@decorators/intercept-model';
 import {CustomsExemptionRemittanceInterceptor} from '@app/model-interceptors/customs-exemption-remittance-interceptor';
+import {ObjectUtils} from '@helpers/object-utils';
 
 const _ApprovalDocument = mixinRequestType(CaseModel);
 const {send, receive} = new CustomsExemptionRemittanceInterceptor();
@@ -100,57 +101,58 @@ export class CustomsExemptionRemittance extends _ApprovalDocument<CustomsExempti
     this.dialog = FactoryService.getService('DialogService');
   }
 
-  buildBasicInfo(controls: boolean = false): any {
-    const {
-      arName,
-      enName,
-      requestType,
-      description,
-      shipmentSource,
-      shipmentWeight,
-      waybill,
-      shipmentPort,
-      linkedProject,
-      // projectLicense,
-      projectName,
-      country,
-      zoneNumber,
-      receiverType,
-      receiverName,
-      otherReceiverName,
-      shipmentApproximateValue,
-      shipmentCarrier,
-      oldFullSerial, // order number
-      oldBookFullSerial // document number
-    } = this;
-
+  getBasicInfoValuesWithLabels(): { [key: string]: ControlValueLabelLangKey } {
     return {
-      arName: controls ? [arName, [CustomValidators.required,
+      requestType: {langKey: 'request_type', value: this.requestType},
+      oldFullSerial: {langKey: 'order_number', value: this.oldFullSerial}, // order number
+      oldBookFullSerial: {langKey: 'document_number', value: this.oldBookFullSerial}, // document number
+      arName: {langKey: 'arabic_name', value: this.arName},
+      enName: {langKey: 'english_name', value: this.enName},
+      shipmentCarrier: {langKey: 'shipping_method', value: this.shipmentCarrier},
+      shipmentSource: {langKey: 'shipment_source', value: this.shipmentSource},
+      shipmentWeight: {langKey: 'shipment_weight', value: this.shipmentWeight},
+      waybill: {langKey: 'bill_of_lading_number', value: this.waybill},
+      shipmentPort: {langKey: 'shipment_port', value: this.shipmentPort},
+      linkedProject: {langKey: 'subordinate_to_project', value: this.linkedProject},
+      projectName: {langKey: 'project_name', value: this.projectName},
+      country: {langKey: 'country', value: this.country},
+      zoneNumber: {langKey: 'region', value: this.zoneNumber},
+      receiverType: {langKey: 'recipient_type', value: this.receiverType},
+      receiverName: {langKey: 'recipient_name', value: this.receiverName},
+      otherReceiverName: {langKey: 'other_recipient_name', value: this.otherReceiverName},
+      shipmentApproximateValue: {langKey: 'shipment_approximate_value', value: this.shipmentApproximateValue},
+      description: {langKey: 'lbl_description', value: this.description}
+    };
+  }
+
+  buildBasicInfo(controls: boolean = false): any {
+    const values = ObjectUtils.getControlValues<CustomsExemptionRemittance>(this.getBasicInfoValuesWithLabels());
+    return {
+      arName: controls ? [values.arName, [CustomValidators.required,
         CustomValidators.maxLength(200),
         CustomValidators.minLength(CustomValidators.defaultLengths.MIN_LENGTH),
-        CustomValidators.pattern('AR_NUM')]] : arName,
-      enName: controls ? [enName, [CustomValidators.required,
+        CustomValidators.pattern('AR_NUM')]] : values.arName,
+      enName: controls ? [values.enName, [CustomValidators.required,
         CustomValidators.maxLength(200),
         CustomValidators.minLength(CustomValidators.defaultLengths.MIN_LENGTH),
-        CustomValidators.pattern('ENG_NUM')]] : enName,
-      requestType: controls ? [requestType, [CustomValidators.required]] : requestType,
-      description: controls ? [description, [CustomValidators.required, CustomValidators.maxLength(2000)]] : description,
-      shipmentSource: controls ? [shipmentSource, [CustomValidators.required]] : shipmentSource,
-      shipmentWeight: controls ? [shipmentWeight, [CustomValidators.required, CustomValidators.maxLength(50)]] : shipmentWeight,
-      waybill: controls ? [waybill, [CustomValidators.required, CustomValidators.maxLength(200)]] : waybill,
-      shipmentPort: controls ? [shipmentPort, [CustomValidators.required, CustomValidators.maxLength(200)]] : shipmentPort,
-      linkedProject: controls ? [linkedProject, [CustomValidators.required]] : linkedProject,
-      // projectLicense: controls ? [projectLicense, CustomValidators.maxLength(50)] : projectLicense,
-      projectName: controls ? [projectName, [CustomValidators.maxLength(50)]] : projectName,
-      country: controls ? [country, [CustomValidators.required]] : country,
-      zoneNumber: controls ? [zoneNumber, [CustomValidators.required, CustomValidators.maxLength(200)]] : zoneNumber,
-      receiverType: controls ? [receiverType, [CustomValidators.required]] : receiverType,
-      receiverName: controls ? [receiverName, [CustomValidators.required]] : receiverName,
-      otherReceiverName: controls ? [otherReceiverName, [CustomValidators.maxLength(200)]] : otherReceiverName,
-      shipmentApproximateValue: controls ? [shipmentApproximateValue, [CustomValidators.required, CustomValidators.maxLength(50), CustomValidators.decimal(2)]] : shipmentApproximateValue,
-      shipmentCarrier: controls ? [shipmentCarrier, [CustomValidators.required]] : shipmentCarrier,
-      oldFullSerial: controls ? [oldFullSerial, [CustomValidators.maxLength(250)]] : oldFullSerial,
-      oldBookFullSerial: controls ? [oldBookFullSerial, [CustomValidators.maxLength(250)]] : oldBookFullSerial,
+        CustomValidators.pattern('ENG_NUM')]] : values.enName,
+      requestType: controls ? [values.requestType, [CustomValidators.required]] : values.requestType,
+      description: controls ? [values.description, [CustomValidators.required, CustomValidators.maxLength(2000)]] : values.description,
+      shipmentSource: controls ? [values.shipmentSource, [CustomValidators.required]] : values.shipmentSource,
+      shipmentWeight: controls ? [values.shipmentWeight, [CustomValidators.required, CustomValidators.maxLength(50)]] : values.shipmentWeight,
+      waybill: controls ? [values.waybill, [CustomValidators.required, CustomValidators.maxLength(200)]] : values.waybill,
+      shipmentPort: controls ? [values.shipmentPort, [CustomValidators.required, CustomValidators.maxLength(200)]] : values.shipmentPort,
+      linkedProject: controls ? [values.linkedProject, [CustomValidators.required]] : values.linkedProject,
+      projectName: controls ? [values.projectName, [CustomValidators.maxLength(50)]] : values.projectName,
+      country: controls ? [values.country, [CustomValidators.required]] : values.country,
+      zoneNumber: controls ? [values.zoneNumber, [CustomValidators.required, CustomValidators.maxLength(200)]] : values.zoneNumber,
+      receiverType: controls ? [values.receiverType, [CustomValidators.required]] : values.receiverType,
+      receiverName: controls ? [values.receiverName, [CustomValidators.required]] : values.receiverName,
+      otherReceiverName: controls ? [values.otherReceiverName, [CustomValidators.maxLength(200)]] : values.otherReceiverName,
+      shipmentApproximateValue: controls ? [values.shipmentApproximateValue, [CustomValidators.required, CustomValidators.maxLength(50), CustomValidators.decimal(2)]] : values.shipmentApproximateValue,
+      shipmentCarrier: controls ? [values.shipmentCarrier, [CustomValidators.required]] : values.shipmentCarrier,
+      oldFullSerial: controls ? [values.oldFullSerial, [CustomValidators.maxLength(250)]] : values.oldFullSerial,
+      oldBookFullSerial: controls ? [values.oldBookFullSerial, [CustomValidators.maxLength(250)]] : values.oldBookFullSerial,
     };
   }
 
@@ -173,5 +175,39 @@ export class CustomsExemptionRemittance extends _ApprovalDocument<CustomsExempti
     return {
       followUpDate: controls ? [DateUtils.changeDateToDatepicker(followUpDate)] : DateUtils.changeDateToDatepicker(followUpDate),
     };
+  }
+
+  // don't delete (used in case audit history)
+  getAdminResultByProperty(property: keyof CustomsExemptionRemittance): AdminResult {
+    let adminResultValue: AdminResult;
+    switch (property) {
+      case 'requestType':
+        adminResultValue = this.requestTypeInfo;
+        break;
+      case 'receiverType':
+        adminResultValue = this.receiverTypeInfo;
+        break;
+      case 'receiverName':
+        adminResultValue = this.receiverNameInfo;
+        break;
+      case 'caseStatus':
+        adminResultValue = this.caseStatusInfo;
+        break;
+      case 'shipmentCarrier':
+        adminResultValue = this.shipmentCarrierInfo;
+        break;
+      case 'shipmentSource':
+        adminResultValue = this.shipmentSourceInfo;
+        break;
+      case 'country':
+        adminResultValue = this.countryInfo;
+        break;
+      case 'linkedProject':
+        adminResultValue = this.linkedProjectInfo;
+        break;
+      default:
+        adminResultValue = AdminResult.createInstance({arName: this[property] as string, enName: this[property] as string});
+    }
+    return adminResultValue ?? new AdminResult();
   }
 }
