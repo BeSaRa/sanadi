e164ba1a7b866873824503feb8a9d22b58334585
aalshahin <aalshahin@eblacorp.com>
2023-04-18 12:12:48 +0400

    npo management - npo bank ccount

--- src/app/models/npo-bank-account.ts
+++ src/app/models/npo-bank-account.ts
@@ -1,6 +1,9 @@
 import { AdminResult } from './admin-result';
 import { CustomValidators } from '@app/validators/custom-validators';
 import { SearchableCloneable } from '@app/models/searchable-cloneable';
+import { ISearchFieldsMap } from '@app/types/types';
+import { normalSearchFields } from '@app/helpers/normal-search-fields';
+import { infoSearchFields } from '@app/helpers/info-search-fields';
 import { AuditOperationTypes } from '@app/enums/audit-operation-types';
 import { ControlValueLabelLangKey } from '@app/types/types';
 import { CommonUtils } from '@app/helpers/common-utils';
@@ -57,4 +60,8 @@ export class NpoBankAccount extends SearchableCloneable<NpoBankAccount> implemen
     }
     return adminResultValue ?? new AdminResult();
   }
+  searchFields: ISearchFieldsMap<NpoBankAccount> = {
+    ...normalSearchFields(['currency','accountNumber','bankId','iban']),
+    ...infoSearchFields(['bankInfo','currencyInfo'])
+  };
 }
