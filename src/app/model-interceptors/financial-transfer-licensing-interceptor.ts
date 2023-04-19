1451faa294d6a578f8c779987f46d3c49d4c6b30
Mamoun-Hamed <108342857+Mamoun-Hamed@users.noreply.github.com>
2023-04-19 12:22:10 +0300

    Add FINANCIAL_TRANSFERS_LICENSING

--- src/app/model-interceptors/financial-transfer-licensing-interceptor.ts
+++ src/app/model-interceptors/financial-transfer-licensing-interceptor.ts
@@ -1,12 +1,15 @@
 import { DateUtils } from '@app/helpers/date-utils';
 import { AdminResult } from '@app/models/admin-result';
 import { FinancialTransferLicensing } from '@app/models/financial-transfer-licensing';
+import { FinancialTransfersProject } from '@app/models/financial-transfers-project';
 import { IModelInterceptor } from '@contracts/i-model-interceptor';
 import { IMyDateModel } from 'angular-mydatepicker';
-
+import { FinancialTransferProjectsInterceptor } from './financial-transfer-projects-interceptor';
+const financialTransferProjectInterceptor = new FinancialTransferProjectsInterceptor()
 export class FinancialTransferLicensingInterceptor
   implements IModelInterceptor<FinancialTransferLicensing>
 {
+
   receive(model: FinancialTransferLicensing): FinancialTransferLicensing {
      model.licenseStatusInfo && (model.licenseStatusInfo = AdminResult.createInstance(model.licenseStatusInfo));
     model.requestTypeInfo &&
@@ -29,8 +32,9 @@ export class FinancialTransferLicensingInterceptor
       ));
     model.countryInfo &&
       (model.countryInfo = AdminResult.createInstance(model.countryInfo));
-    model.currencyInfo &&
-      (model.currencyInfo = AdminResult.createInstance(model.countryInfo));
+    model.financialTransfersProjects = model.financialTransfersProjects.map(x=>{
+      return financialTransferProjectInterceptor.receive(new FinancialTransfersProject().clone(x))
+    })
 
     return model;
   }
@@ -48,6 +52,9 @@ export class FinancialTransferLicensingInterceptor
       : DateUtils.changeDateFromDatepicker(
           model.actualTransferDate as unknown as IMyDateModel
         )?.toISOString();
+    model.financialTransfersProjects =( model.financialTransfersProjects??[]).map(x=>{
+      return financialTransferProjectInterceptor.send(x) as FinancialTransfersProject
+    })
     FinancialTransferLicensingInterceptor._deleteBeforeSend(model);
     return model;
   }
