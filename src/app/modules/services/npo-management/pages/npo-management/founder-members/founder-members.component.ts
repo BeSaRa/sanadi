95e8844f3e4c64b85d9318d06f5470018b81d243
aalshahin <aalshahin@eblacorp.com>
2023-04-26 17:46:18 +0400

    founder members

--- src/app/modules/services/npo-management/pages/npo-management/founder-members/founder-members.component.ts
+++ src/app/modules/services/npo-management/pages/npo-management/founder-members/founder-members.component.ts
@@ -1,285 +1,71 @@
-import { DateUtils } from '@helpers/date-utils';
-import { DatepickerOptionsMap, ReadinessStatus } from '@app/types/types';
-import { LookupService } from '@services/lookup.service';
-import { Lookup } from '@models/lookup';
-import { JobTitleService } from '@services/job-title.service';
-import { JobTitle } from '@models/job-title';
-import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { LangService } from "@app/services/lang.service";
 import { ToastService } from "@app/services/toast.service";
 import { DialogService } from "@app/services/dialog.service";
-import { AbstractControl, UntypedFormArray, UntypedFormBuilder, UntypedFormControl, UntypedFormGroup } from "@angular/forms";
-import { BehaviorSubject, Subject } from "rxjs";
-import { filter, map, take, takeUntil } from "rxjs/operators";
-import { UserClickOn } from "@app/enums/user-click-on.enum";
 import { FounderMembers } from "@app/models/founder-members";
 import { FounderMembersPopupComponent } from '../../../popups/founder-members-popup/founder-members-popup.component';
 import { IMenuItem } from '@app/modules/context-menu/interfaces/i-menu-item';
 import { ActionIconsEnum } from '@app/enums/action-icons-enum';
+import { UiCrudListGenericComponent } from '@app/generics/ui-crud-list-generic-component';
+import { ComponentType } from '@angular/cdk/portal';
+import { IKeyValue } from '@app/interfaces/i-key-value';
 
 @Component({
   selector: 'founder-members',
   templateUrl: './founder-members.component.html',
   styleUrls: ['./founder-members.component.scss']
 })
-export class FounderMembersComponent implements OnInit, OnDestroy {
-  nationalityList: Lookup[] = this.lookupService.listByCategory.Nationality;
-  jobTitleAdminLookup: JobTitle[] = [];
-  constructor(public lang: LangService,
-    private toastService: ToastService,
-    private lookupService: LookupService,
-    private dialogService: DialogService,
-    private _jb: JobTitleService,
-    private fb: UntypedFormBuilder) {
-  }
+export class FounderMembersComponent extends UiCrudListGenericComponent<FounderMembers> {
+  
+  displayColumns: string[] = ['idNumber', 'fullName', 'email', 'phone', 'extraPhone', 'actions'];
 
-  private _list: FounderMembers[] = [];
-  @Input() set list(list: FounderMembers[]) {
-    this._list = list;
-    this.dataSource.next(this._list);
-  }
-
-  get list(): FounderMembers[] {
-    return this._list;
-  }
-
-  @Input() readonly: boolean = false;
-
-  @Output() readyEvent = new EventEmitter<ReadinessStatus>();
-
-  dataSource: BehaviorSubject<FounderMembers[]> = new BehaviorSubject<FounderMembers[]>([]);
-  columns = ['idNumber', 'fullName', 'email', 'phone', 'extraPhone', 'actions'];
-  filterControl: UntypedFormControl = new UntypedFormControl('');
   actions: IMenuItem<FounderMembers>[] = [
-    // edit
     {
       type: 'action',
       icon: ActionIconsEnum.EDIT,
       label: 'btn_edit',
-      onClick: (item: FounderMembers) => this.edit(item),
+      onClick: (item: FounderMembers) => this.edit$.next(item),
       show: (_item: FounderMembers) => !this.readonly
     },
-    // delete
     {
       type: 'action',
       icon: ActionIconsEnum.DELETE,
       label: 'btn_delete',
-      onClick: (item: FounderMembers) => this.delete(item),
+      onClick: (item: FounderMembers) => this.confirmDelete$.next(item),
       show: (_item: FounderMembers) => !this.readonly
     },
-    // view
     {
       type: 'action',
       icon: ActionIconsEnum.VIEW,
       label: 'view',
-      onClick: (item: FounderMembers) => this.view(item)
+      onClick: (item: FounderMembers) => this.view$.next(item),
     }
   ];
-  editItem? : FounderMembers
-  add$: Subject<any> = new Subject<any>();
-  private save$: Subject<any> = new Subject<any>();
 
-  private changed$: Subject<FounderMembers | null> = new Subject<FounderMembers | null>();
-  private current?: FounderMembers;
-  private destroy$: Subject<any> = new Subject<any>();
+  @Input() founderMemberList: FounderMembers[] = [];
 
-  form!: UntypedFormGroup;
-
-  datepickerOptionsMap: DatepickerOptionsMap = {
-    joinDate: DateUtils.getDatepickerOptions({
-      disablePeriod: "none",
-    }),
-  }
-  ngOnInit(): void {
-    this._jb.loadActive().subscribe((data) => {
-      this.jobTitleAdminLookup = data;
-    })
-    this.buildForm();
-    this.listenToAdd();
-    this.listenToChange();
-    this.listenToSave();
-    this._setComponentReadiness('READY');
+  constructor(
+    public lang: LangService,
+    public toast: ToastService,
+    public dialog: DialogService
+    ) {
+    super();
   }
 
-  ngOnDestroy(): void {
-    this.destroy$.next();
-    this.destroy$.complete();
-    this.destroy$.unsubscribe();
+  _getNewInstance(override?: Partial<FounderMembers> | undefined): FounderMembers {
+    return new FounderMembers().clone(override ?? {});
   }
 
-  private buildForm() {
-    this.form = this.fb.group({
-      founderMembers: this.fb.array([])
-    })
+  _getDialogComponent(): ComponentType<any> {
+    return FounderMembersPopupComponent
   }
 
-  get founderMembersFormArray(): UntypedFormArray {
-    return (this.form.get('founderMembers')) as UntypedFormArray;
+  _getDeleteConfirmMessage(record: FounderMembers): string {
+    return this.lang.map.msg_confirm_delete_x.change({x: record.fullName});
   }
 
-  private listenToAdd() {
-    this.add$.pipe(takeUntil(this.destroy$))
-      .subscribe(() => {
-        this.changed$.next(new FounderMembers())
-      })
+  getExtraDataForPopup(): IKeyValue {
+    return {};
   }
 
-  private listenToChange() {
-    this.changed$.pipe(takeUntil(this.destroy$))
-      .subscribe(member => {
-        this.current = member || undefined;
-        this.updateForm(this.current);
-      })
-  }
-
-  _getPopupComponent() {
-    return FounderMembersPopupComponent;
-  }
-
-  openFormPopup() {
-    this.dialogService.show(this._getPopupComponent(), {
-      form: this.form,
-      readonly: this.readonly,
-      editItem:this.editItem,
-      model: this.current,
-      founderMembersFormArray: this.founderMembersFormArray,
-      nationalityList:this.nationalityList,
-      jobTitleAdminLookup: this.jobTitleAdminLookup
-    }).onAfterClose$.subscribe((data) => {
-      if (data) {
-        this.save()
-      } else {
-        this.cancel();
-      }
-    })
-  }
-
-  private updateForm(record: FounderMembers | undefined) {
-    const founderMembersFormArray = this.founderMembersFormArray;
-    founderMembersFormArray.clear();
-
-    if (record) {
-      this._setComponentReadiness('NOT_READY');
-      founderMembersFormArray.push(this.fb.group((record.getFounderMembersFields(true))));
-      this.openFormPopup()
-    } else {
-      this._setComponentReadiness('READY');
-    }
-  }
-
-  save() {
-    if (this.readonly) {
-      return;
-    }
-    this.save$.next();
-  }
-
-  private listenToSave() {
-    this.save$.pipe(map(() => {
-      return this.form.get('founderMembers.0') as AbstractControl;
-    })).pipe(
-      takeUntil(this.destroy$),
-      filter((form) => {
-        const valid = this._list.findIndex(f => f.identificationNumber == form.value.identificationNumber) == -1;
-        !valid && this.editItem == undefined && this.dialogService
-          .error(this.lang.map.msg_user_identifier_is_already_exist)
-          .onAfterClose$
-          .pipe(take(1))
-          .subscribe(() => {
-            this.form.get('founderMembers')?.markAllAsTouched();
-            this.openFormPopup();
-          });
-        return valid || !!this.editItem
-      }),
-      map(() => {
-        return (this.form.get('founderMembers.0')) as UntypedFormArray;
-      }),
-      map((form) => {
-        return (new FounderMembers()).clone({
-          ...this.current, ...form.getRawValue()
-        });
-      })
-    ).subscribe((member: FounderMembers) => {
-      if (!member) {
-        return;
-      }
-
-      this._updateList(member, (!!this.editItem ? 'UPDATE' : 'ADD'));
-      this.toastService.success(this.lang.map.msg_save_success);
-      this.editItem == undefined;
-      this.changed$.next(null);
-    });
-  }
-
-  private _updateList(record: (FounderMembers | null), operation: 'ADD' | 'UPDATE' | 'DELETE' | 'NONE') {
-    if (record) {
-      if (operation === 'ADD') {
-        this.list.push(record);
-      } else {
-        let index = !this.editItem ? -1 : this.list.findIndex(x => x === this.editItem);
-        if (operation === 'UPDATE') {
-          this.list.splice(index, 1, record);
-        } else if (operation === 'DELETE') {
-          this.list.splice(index, 1);
-        }
-      }
-    }
-    this.list = this.list.slice();
-    this.dataSource.next(this.list);
-  }
-
-  edit(record: FounderMembers, $event?: MouseEvent) {
-    $event?.preventDefault();
-    if (this.readonly) {
-      return;
-    }
-    this.editItem = record
-    this.changed$.next(record);
-  }
-
-  delete(record: FounderMembers, $event?: MouseEvent): any {
-    $event?.preventDefault();
-    if (this.readonly) {
-      return;
-    }
-    this.dialogService.confirm(this.lang.map.msg_confirm_delete_selected)
-      .onAfterClose$
-      .pipe(take(1))
-      .subscribe((click: UserClickOn) => {
-        if (click === UserClickOn.YES) {
-          this._updateList(record, 'DELETE');
-          this.toastService.success(this.lang.map.msg_delete_success);
-        }
-      });
-  }
-
-  cancel() {
-    this.resetForm();
-    this._setComponentReadiness('READY');
-    this.editItem = undefined
-  }
-
-  private resetForm() {
-    this.founderMembersFormArray.clear();
-    this.founderMembersFormArray.markAsUntouched();
-    this.founderMembersFormArray.markAsPristine();
-  }
-
-  private _setComponentReadiness(readyStatus: ReadinessStatus) {
-    this.readyEvent.emit(readyStatus);
-  }
-  view(record: FounderMembers,$event?: MouseEvent) {
-    $event?.preventDefault();
-    this.editItem = record
-    this.changed$.next(record);
-  }
-  forceClearComponent() {
-    this.cancel();
-    this.list = [];
-    this._updateList(null, 'NONE');
-    this._setComponentReadiness('READY');
-  }
-  openDateMenu(ref: any) {
-    ref.toggleCalendar();
-  }
 }
--- src/app/modules/services/npo-management/popups/founder-members-popup/founder-members-popup.component.ts
+++ src/app/modules/services/npo-management/popups/founder-members-popup/founder-members-popup.component.ts
@@ -1,57 +1,99 @@
 import { Component, Inject } from '@angular/core';
-import { UntypedFormArray, UntypedFormGroup } from '@angular/forms';
+import { UntypedFormArray, UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
+import { UiCrudDialogComponentDataContract } from '@app/contracts/ui-crud-dialog-component-data-contract';
+import { OperationTypes } from '@app/enums/operation-types.enum';
+import { UiCrudDialogGenericComponent } from '@app/generics/ui-crud-dialog-generic-component.directive';
+import { ILanguageKeys } from '@app/interfaces/i-language-keys';
+import { AdminResult } from '@app/models/admin-result';
 import { FounderMembers } from '@app/models/founder-members';
 import { JobTitle } from '@app/models/job-title';
 import { Lookup } from '@app/models/lookup';
+import { DialogService } from '@app/services/dialog.service';
+import { JobTitleService } from '@app/services/job-title.service';
 import { LangService } from '@app/services/lang.service';
+import { LookupService } from '@app/services/lookup.service';
+import { ToastService } from '@app/services/toast.service';
 import { DialogRef } from '@app/shared/models/dialog-ref';
 import { DIALOG_DATA_TOKEN } from '@app/shared/tokens/tokens';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'founder-members-popup',
   templateUrl: './founder-members-popup.component.html',
   styleUrls: ['./founder-members-popup.component.scss']
 })
-export class FounderMembersPopupComponent {
-  form: UntypedFormGroup;
-  readonly: boolean;
-  editItem: FounderMembers;
+export class FounderMembersPopupComponent extends UiCrudDialogGenericComponent<FounderMembers>{
+  form!: UntypedFormGroup;
   model: FounderMembers;
-  founderMembersFormArray:UntypedFormArray;
-  nationalityList: Lookup[];
-  jobTitleAdminLookup: JobTitle[];
-  constructor(@Inject(DIALOG_DATA_TOKEN)
-   public data: {
-     form: UntypedFormGroup,
-     readonly: boolean,
-     editItem: FounderMembers,
-     model: FounderMembers,
-     founderMembersFormArray:UntypedFormArray,
-     nationalityList:Lookup[],
-     jobTitleAdminLookup: JobTitle[]
-   },
-     public lang: LangService,
-     private dialogRef: DialogRef) {
-     this.form = data.form;
-     this.readonly = data.readonly;
-     this.editItem = data.editItem;
-     this.model = data.model;
-     this.founderMembersFormArray = data.founderMembersFormArray
-     this.nationalityList = data.nationalityList
-     this.jobTitleAdminLookup = data.jobTitleAdminLookup
-   }
+  operation: OperationTypes;
+  popupTitleKey!: keyof ILanguageKeys;
+  nationalityList: Lookup[] = this.lookupService.listByCategory.Nationality;
+  jobTitleAdminLookup!: JobTitle[];
 
-   mapFormTo(form: any): FounderMembers {
-    const model: FounderMembers = new FounderMembers().clone(form);
+  _getNewInstance(override?: Partial<FounderMembers> | undefined): FounderMembers {
+    return new FounderMembers().clone(override ?? {});
+  }
+  initPopup(): void {
+    this.popupTitleKey = 'lbl_founder_members';
+    this.JobTitleService.loadActive().subscribe((data) => {
+      this.jobTitleAdminLookup = data;
+    })
+  }
+  destroyPopup(): void {
+  }
 
-    return model;
+  afterSave(savedModel: FounderMembers, originalModel: FounderMembers): void {
+    this.toast.success(this.operation === OperationTypes.CREATE
+      ? this.lang.map.msg_added_in_list_success : this.lang.map.msg_updated_in_list_success);
+    this.dialogRef.close(savedModel);
   }
-  cancel() {
-    this.dialogRef.close(null)
+  
+  beforeSave(model: FounderMembers, form: UntypedFormGroup): boolean | Observable<boolean> {
+    if (this.form.invalid) {
+      this.displayRequiredFieldsMessage();
+      return false;
+    }
+    const isDuplicate = this.list.some((x) => x === form.getRawValue());
+    if (isDuplicate) {
+      this.displayDuplicatedItemMessage();
+      return false;
+    }
+    return true;
   }
-  save() {
-    this.dialogRef.close(this.mapFormTo(this.form.getRawValue()))
+
+  prepareModel(model: FounderMembers, form: UntypedFormGroup): FounderMembers | Observable<FounderMembers> {
+    let formValue = form.getRawValue();
+     return this._getNewInstance({
+       ...this.model,
+       ...formValue,
+       jobTitleInfo: this.jobTitleAdminLookup.find((x) => x.id === formValue.JobTitle)?.createAdminResult() ?? new AdminResult(),
+       nationalityInfo: this.nationalityList.find((x) => x.id === formValue.Nationality)?.createAdminResult() ?? new AdminResult(),
+     });
   }
+
+  saveFail(error: Error): void {
+    throw new Error(error.message);
+  }
+
+  buildForm(): void {
+    this.form = this.fb.group(this.model.getFounderMembersFields(true));
+  }
+
+  constructor(@Inject(DIALOG_DATA_TOKEN) data: UiCrudDialogComponentDataContract<FounderMembers>,
+    public lang: LangService,
+    public dialogRef: DialogRef,
+    public dialogService: DialogService,
+    public fb: UntypedFormBuilder,
+    public toast: ToastService,
+    private lookupService:LookupService,
+    private JobTitleService: JobTitleService,
+  ) {
+    super();
+    this.model = data.model;
+    this.operation = data.operation;
+    this.list = data.list;
+  }
+
   searchNgSelect(term: string, item: any): boolean {
     return item.ngSelectSearch(term);
   }
