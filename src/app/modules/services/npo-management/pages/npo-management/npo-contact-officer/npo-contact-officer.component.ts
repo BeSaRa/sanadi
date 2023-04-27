843d1f153fdff5a90ec39dda38b382023d341163
aalshahin <aalshahin@eblacorp.com>
2023-04-27 11:06:29 +0400

    npo contact officer

--- src/app/modules/services/npo-management/pages/npo-management/npo-contact-officer/npo-contact-officer.component.ts
+++ src/app/modules/services/npo-management/pages/npo-management/npo-contact-officer/npo-contact-officer.component.ts
@@ -1,269 +1,65 @@
-import { JobTitle } from '@models/job-title';
-import { JobTitleService } from '@services/job-title.service';
-import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
-import { LangService } from "@app/services/lang.service";
-import { ToastService } from "@app/services/toast.service";
-import { DialogService } from "@app/services/dialog.service";
-import { AbstractControl, UntypedFormArray, UntypedFormBuilder, UntypedFormControl, UntypedFormGroup } from "@angular/forms";
-import { ReadinessStatus } from "@app/types/types";
-import { BehaviorSubject, Subject } from "rxjs";
-import { filter, map, take, takeUntil } from "rxjs/operators";
-import { UserClickOn } from "@app/enums/user-click-on.enum";
+import { Component, Input} from '@angular/core';
 import { NpoContactOfficer } from "@app/models/npo-contact-officer";
 import { NpoContactOfficerPopupComponent } from '../../../popups/npo-contact-officer-popup/npo-contact-officer-popup.component';
 import { IMenuItem } from '@app/modules/context-menu/interfaces/i-menu-item';
 import { ActionIconsEnum } from '@app/enums/action-icons-enum';
+import { UiCrudListGenericComponent } from '@app/generics/ui-crud-list-generic-component';
+import { ComponentType } from '@angular/cdk/portal';
+import { IKeyValue } from '@app/interfaces/i-key-value';
+import { DialogService } from '@app/services/dialog.service';
+import { LangService } from '@app/services/lang.service';
+import { ToastService } from '@app/services/toast.service';
 
 @Component({
   selector: 'npo-contact-officer',
   templateUrl: './npo-contact-officer.component.html',
   styleUrls: ['./npo-contact-officer.component.scss']
 })
-export class NpoContactOfficerComponent implements OnInit, OnDestroy {
+export class NpoContactOfficerComponent extends UiCrudListGenericComponent<NpoContactOfficer> {
   constructor(public lang: LangService,
-    private toastService: ToastService,
-    private _jb: JobTitleService,
-    private dialogService: DialogService,
-    private fb: UntypedFormBuilder) {
+    public toast: ToastService,
+    public dialog: DialogService) {
+  super();
   }
-
-  jobTitleAdminLookup: JobTitle[] = [];
-  private _list: NpoContactOfficer[] = [];
-  @Input() set list(list: NpoContactOfficer[]) {
-    this._list = list;
-    this.dataSource.next(this._list);
-  }
-
-  get list(): NpoContactOfficer[] {
-    return this._list;
-  }
-
-  @Input() readonly: boolean = false;
-
-  @Output() readyEvent = new EventEmitter<ReadinessStatus>();
-
-  dataSource: BehaviorSubject<NpoContactOfficer[]> = new BehaviorSubject<NpoContactOfficer[]>([]);
-  columns = ['idNumber', 'fullName', 'email', 'phone', 'extraPhone', 'actions'];
-
-  editItem?: NpoContactOfficer;
-
-  add$: Subject<any> = new Subject<any>();
-  private save$: Subject<any> = new Subject<any>();
-  filterControl: UntypedFormControl = new UntypedFormControl('');
-
-  private changed$: Subject<NpoContactOfficer | null> = new Subject<NpoContactOfficer | null>();
-  private current?: NpoContactOfficer;
-  private destroy$: Subject<any> = new Subject<any>();
-
+  
   actions: IMenuItem<NpoContactOfficer>[] = [
-    // edit
-    {
-      type: 'action',
-      icon: ActionIconsEnum.EDIT,
-      label: 'btn_edit',
-      onClick: (item: NpoContactOfficer) => this.edit(item),
-      show: (_item: NpoContactOfficer) => !this.readonly
-    },
-    // delete
-    {
-      type: 'action',
-      icon: ActionIconsEnum.DELETE,
-      label: 'btn_delete',
-      onClick: (item: NpoContactOfficer) => this.delete(item),
-      show: (_item: NpoContactOfficer) => !this.readonly
-    },
-    // view
-    {
-      type: 'action',
-      icon: ActionIconsEnum.VIEW,
-      label: 'view',
-      onClick: (item: NpoContactOfficer) => this.view(item)
-    }
-  ];
-  form!: UntypedFormGroup;
-
-  ngOnInit(): void {
-    this._jb.loadActive().subscribe((data) => {
-      this.jobTitleAdminLookup = data;
-    })
-    this.buildForm();
-    this.listenToAdd();
-    this.listenToChange();
-    this.listenToSave();
-    this._setComponentReadiness('READY');
-  }
-
-  ngOnDestroy(): void {
-    this.destroy$.next();
-    this.destroy$.complete();
-    this.destroy$.unsubscribe();
-  }
-
-  private buildForm() {
-    this.form = this.fb.group({
-      contactOfficers: this.fb.array([])
-    })
-  }
-
-  private listenToAdd() {
-    this.add$.pipe(takeUntil(this.destroy$))
-      .subscribe(() => {
-        this.changed$.next(new NpoContactOfficer())
-      })
-  }
-  _getPopupComponent() {
-    return NpoContactOfficerPopupComponent;
-  }
-  private listenToChange() {
-    this.changed$.pipe(takeUntil(this.destroy$))
-      .subscribe(contact => {
-        this.current = contact || undefined;
-        this.updateForm(this.current);
-      })
-  }
-  openFormPopup() {
-    this.dialogService.show(this._getPopupComponent(), {
-      form: this.form,
-      readonly: this.readonly,
-      editItem: this.editItem,
-      model: this.current,
-      contactOfficersFormArray: this.contactOfficersFormArray,
-      jobTitleAdminLookup: this.jobTitleAdminLookup
-    }).onAfterClose$.subscribe((data) => {
-      if (data) {
-        this.save()
-      } else {
-        this.cancel();
+      // edit
+      {
+        type: 'action',
+        icon: ActionIconsEnum.EDIT,
+        label: 'btn_edit',
+        onClick: (item: NpoContactOfficer) => this.edit$.next(item),
+        show: (_item: NpoContactOfficer) => !this.readonly
+      },
+      // delete
+      {
+        type: 'action',
+        icon: ActionIconsEnum.DELETE,
+        label: 'btn_delete',
+        onClick: (item: NpoContactOfficer) => this.confirmDelete$.next(item),
+        show: (_item: NpoContactOfficer) => !this.readonly
+      },
+      // view
+      {
+        type: 'action',
+        icon: ActionIconsEnum.VIEW,
+        label: 'view',
+        onClick: (item: NpoContactOfficer) => this.view$.next(item),
       }
-    })
+    ];
+  displayColumns: string[] = ['idNumber', 'fullName', 'email', 'phone', 'extraPhone', 'actions'];
+  
+  _getNewInstance(override?: Partial<NpoContactOfficer> | undefined): NpoContactOfficer {
+    return new NpoContactOfficer().clone(override ?? {});
   }
-  private updateForm(record: NpoContactOfficer | undefined) {
-    const contactOfficersFormArray = this.contactOfficersFormArray;
-    contactOfficersFormArray.clear();
-
-    if (record) {
-      this._setComponentReadiness('NOT_READY');
-      contactOfficersFormArray.push(this.fb.group((record.getContactOfficerFields(true))));
-      this.openFormPopup();
-    } else {
-      this._setComponentReadiness('READY');
-    }
+  _getDialogComponent(): ComponentType<any> {
+    return NpoContactOfficerPopupComponent
   }
-
-  save() {
-    if (this.readonly) {
-      return;
-    }
-    this.save$.next();
+  _getDeleteConfirmMessage(record: NpoContactOfficer): string {
+    return this.lang.map.msg_confirm_delete_x;
   }
-
-  private listenToSave() {
-    this.save$.pipe(map(() => {
-      return this.form.get('contactOfficers.0') as AbstractControl;
-    })).pipe(
-      takeUntil(this.destroy$),
-      filter((form) => {
-        const valid = this._list.findIndex(c => c.identificationNumber == form.value.identificationNumber) == -1;
-        !valid && this.editItem == undefined && this.dialogService
-          .error(this.lang.map.msg_user_identifier_is_already_exist)
-          .onAfterClose$
-          .pipe(take(1))
-          .subscribe(() => {
-            this.form.get('contactOfficers')?.markAllAsTouched();
-            this.openFormPopup();
-          });
-        return valid || !!this.editItem
-      }),
-      map(() => {
-        return (this.form.get('contactOfficers.0')) as UntypedFormArray;
-      }),
-      map((form) => {
-        return (new NpoContactOfficer()).clone({
-          ...this.current, ...form.getRawValue()
-        });
-      })
-    ).subscribe((contactOfficer: NpoContactOfficer) => {
-      if (!contactOfficer) {
-        return;
-      }
-      this._updateList(contactOfficer, (!!this.editItem ? 'UPDATE' : 'ADD'));
-      this.toastService.success(this.lang.map.msg_save_success);
-      this.editItem = undefined;
-      this.changed$.next(null);
-    });
+  getExtraDataForPopup(): IKeyValue {
+    return {};
   }
-
-  private _updateList(record: (NpoContactOfficer | null), operation: 'ADD' | 'UPDATE' | 'DELETE' | 'NONE') {
-    if (record) {
-      if (operation === 'ADD') {
-        this.list.push(record);
-      } else {
-        let index = !this.editItem ? -1 : this.list.findIndex(x => x === this.editItem);
-        if (operation === 'UPDATE') {
-          this.list.splice(index, 1, record);
-        } else if (operation === 'DELETE') {
-          this.list.splice(index, 1);
-        }
-      }
-    }
-    this.list = this.list.slice();
-    this.dataSource.next(this.list);
-  }
-
-  edit(record: NpoContactOfficer, $event?: MouseEvent) {
-    $event?.preventDefault();
-    if (this.readonly) {
-      return;
-    }
-    this.editItem = record
-    this.changed$.next(record);
-  }
-
-  delete(record: NpoContactOfficer,$event?: MouseEvent): any {
-    $event?.preventDefault();
-    if (this.readonly) {
-      return;
-    }
-    this.dialogService.confirm(this.lang.map.msg_confirm_delete_selected)
-      .onAfterClose$
-      .pipe(take(1))
-      .subscribe((click: UserClickOn) => {
-        if (click === UserClickOn.YES) {
-          this._updateList(record, 'DELETE');
-          this.toastService.success(this.lang.map.msg_delete_success);
-        }
-      });
-  }
-
-  cancel() {
-    this.resetForm();
-    this._setComponentReadiness('READY');
-    this.editItem = undefined
-  }
-
-  private resetForm() {
-    this.contactOfficersFormArray.clear();
-    this.contactOfficersFormArray.markAsUntouched();
-    this.contactOfficersFormArray.markAsPristine();
-  }
-
-  private _setComponentReadiness(readyStatus: ReadinessStatus) {
-    this.readyEvent.emit(readyStatus);
-  }
-
-  view(record: NpoContactOfficer, $event?: MouseEvent) {
-    $event?.preventDefault();
-    this.editItem = record
-    this.changed$.next(record);
-  }
-  forceClearComponent() {
-    this.cancel();
-    this.list = [];
-    this._updateList(null, 'NONE');
-    this._setComponentReadiness('READY');
-  }
-
-  get contactOfficersFormArray(): UntypedFormArray {
-    return (this.form.get('contactOfficers')) as UntypedFormArray;
-  }
+  @Input() contactOfficerList: NpoContactOfficer[] = [];
 }
