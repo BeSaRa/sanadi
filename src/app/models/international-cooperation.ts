4a406c500916098d1834800bf1df632a9c571eeb
Mamoun-Hamed <108342857+Mamoun-Hamed@users.noreply.github.com>
2023-04-27 13:25:20 +0300

    Add INTERNATIONAL_COOPERATION

--- src/app/models/international-cooperation.ts
+++ src/app/models/international-cooperation.ts
@@ -3,12 +3,16 @@ import {FactoryService} from '@services/factory.service';
 import {CustomValidators} from '../validators/custom-validators';
 import {InternationalCooperationService} from '@services/international-cooperation.service';
 import {CaseTypes} from '@app/enums/case-types.enum';
-import {ISearchFieldsMap} from '@app/types/types';
+import {ControlValueLabelLangKey, ISearchFieldsMap} from '@app/types/types';
 import {dateSearchFields} from '@app/helpers/date-search-fields';
 import {infoSearchFields} from '@app/helpers/info-search-fields';
 import {normalSearchFields} from '@app/helpers/normal-search-fields';
+import { IAuditModelProperties } from '@app/interfaces/i-audit-model-properties';
+import { AuditOperationTypes } from '@app/enums/audit-operation-types';
+import { CommonUtils } from '@app/helpers/common-utils';
+import { AdminResult } from './admin-result';
 
-export class InternationalCooperation extends CaseModel<InternationalCooperationService, InternationalCooperation> {
+export class InternationalCooperation extends CaseModel<InternationalCooperationService, InternationalCooperation> implements IAuditModelProperties<InternationalCooperation> {
   caseType: number = CaseTypes.INTERNATIONAL_COOPERATION;
   country!: string;
   email!: string;
@@ -17,6 +21,8 @@ export class InternationalCooperation extends CaseModel<InternationalCooperation
   organization!: string;
   requestBody!: string;
   service: InternationalCooperationService;
+
+  countryInfo!: AdminResult;
 
   searchFields: ISearchFieldsMap<InternationalCooperation> = {
     ...dateSearchFields(['createdOn']),
@@ -37,7 +43,42 @@ export class InternationalCooperation extends CaseModel<InternationalCooperation
       delete this.searchFields.organization;
     }
   }
+  getAdminResultByProperty(property: keyof InternationalCooperation): AdminResult {
+    let adminResultValue: AdminResult;
+    switch (property) {
+      case 'caseStatus':
+        adminResultValue = this.caseStatusInfo;
+        break;
+      case 'country':
+        adminResultValue = this.countryInfo;
+        break;
+      case 'competentDepartmentID':
+        adminResultValue =  this.service.departments.find(x=>x.id === this.competentDepartmentID)!.createAdminResult();;
+        break;
 
+      default:
+        let value: any = this[property];
+        if (!CommonUtils.isValidValue(value) || typeof value === 'object') {
+          value = '';
+        }
+        adminResultValue = AdminResult.createInstance({ arName: value as string, enName: value as string });
+    }
+    return adminResultValue ?? new AdminResult();
+  }
+  auditOperation: AuditOperationTypes = AuditOperationTypes.NO_CHANGE;
+
+  getBasicInfoValuesWithLabels(): { [key: string]: ControlValueLabelLangKey } {
+    return {
+      country: { langKey: 'country', value: this.country },
+      email: { langKey: 'lbl_email', value: this.email },
+      fullName: { langKey: 'full_name', value: this.fullName },
+      mobileNo: { langKey: 'mobile_number', value: this.mobileNo },
+      organization: { langKey: 'lbl_organization', value: this.organization },
+      requestBody: { langKey: 'request_body', value: this.requestBody },
+      competentDepartmentID: { langKey: 'competent_dep', value: this.competentDepartmentID },
+      competentDepartmentAuthName: { langKey: 'department', value: this.competentDepartmentAuthName }
+    }
+  }
   getFormFields(controls: boolean = false): any {
     const {
       country,
