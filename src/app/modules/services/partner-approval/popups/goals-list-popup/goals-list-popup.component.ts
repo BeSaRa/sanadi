1f9abdad17f80ae50fa09c993de16d3e0dc896d2
aalshahin <aalshahin@eblacorp.com>
2023-04-30 11:23:03 +0400

    goals list

--- src/app/modules/services/partner-approval/popups/goals-list-popup/goals-list-popup.component.ts
+++ src/app/modules/services/partner-approval/popups/goals-list-popup/goals-list-popup.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject, OnInit, OnDestroy } from '@angular/core';
-import { UntypedFormControl, UntypedFormGroup } from '@angular/forms';
+import { UntypedFormBuilder, UntypedFormControl, UntypedFormGroup } from '@angular/forms';
 import { CommonStatusEnum } from '@app/enums/common-status.enum';
 import { GoalList } from '@app/models/goal-list';
 import { LangService } from '@app/services/lang.service';
@@ -9,52 +9,116 @@ import { Lookup } from '@app/models/lookup';
 import { map, takeUntil } from 'rxjs/operators';
 import { DomainTypes } from '@app/enums/domain-types';
 import { CustomValidators } from '@app/validators/custom-validators';
-import { Subject } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { AdminResult } from '@app/models/admin-result';
 import { DacOchaService } from '@app/services/dac-ocha.service';
 import { LookupService } from '@app/services/lookup.service';
+import { UiCrudDialogGenericComponent } from '@app/generics/ui-crud-dialog-generic-component.directive';
+import { UiCrudDialogComponentDataContract } from '@app/contracts/ui-crud-dialog-component-data-contract';
+import { DialogService } from '@app/services/dialog.service';
+import { ToastService } from '@app/services/toast.service';
+import { OperationTypes } from '@app/enums/operation-types.enum';
+import { ILanguageKeys } from '@app/interfaces/i-language-keys';
 
 @Component({
   selector: 'app-goals-list-popup',
   templateUrl: './goals-list-popup.component.html',
   styleUrls: ['./goals-list-popup.component.scss']
 })
-export class GoalsListPopupComponent implements OnInit, OnDestroy {
-  commonStatusEnum = CommonStatusEnum;
-  form: UntypedFormGroup;
-  readonly: boolean;
-  editItem: number;
+export class GoalsListPopupComponent extends UiCrudDialogGenericComponent<GoalList> {
   model: GoalList;
-  viewOnly: boolean;
+  form!: UntypedFormGroup;
+  operation: OperationTypes;
+  popupTitleKey!: keyof ILanguageKeys;
+  commonStatusEnum = CommonStatusEnum;
   domainsList: Lookup[] = this.lookupService.listByCategory.Domain;
   mainDACCategoriesList: AdminResult[] = [];
   mainUNOCHACategoriesList: AdminResult[] = [];;
+  _getNewInstance(override?: Partial<GoalList> | undefined): GoalList {
+    return new GoalList().clone(override ?? {});
+  }
+
+  initPopup(): void {
+    this.popupTitleKey = 'domain';
+  }
+  
+  destroyPopup(): void {
+  }
+
+  afterSave(savedModel: GoalList, originalModel: GoalList): void {
+    this.toast.success(this.operation === OperationTypes.CREATE
+      ? this.lang.map.msg_added_in_list_success : this.lang.map.msg_updated_in_list_success);
+    this.dialogRef.close(savedModel);
+  }
+
+  beforeSave(model: GoalList, form: UntypedFormGroup): boolean | Observable<boolean> {
+    if (this.form.invalid) {
+      this.displayRequiredFieldsMessage();
+      return false;
+    }
+    const isDuplicate = this.list.some((x) => x === form.getRawValue());
+    if (isDuplicate) {
+      this.displayDuplicatedItemMessage();
+      return false;
+    }
+    return true;
+  }
+
+  prepareModel(model: GoalList, form: UntypedFormGroup): GoalList | Observable<GoalList> {
+    let formValue = form.getRawValue();
+    
+    let domainInfo: AdminResult =
+      this.domainsList
+        .find((x) => x.lookupKey === formValue.domain)
+        ?.convertToAdminResult() ?? new AdminResult();
+
+    let mainDACCategoryInfo: AdminResult =
+      this.mainDACCategoriesList.find(
+        (x) => x.id === formValue.mainDACCategory
+      ) ?? new AdminResult();
+
+    let mainUNOCHACategoryInfo: AdminResult =
+      this.mainUNOCHACategoriesList.find(
+        (x) => x.id === formValue.mainUNOCHACategory
+      ) ?? new AdminResult();
+
+     return this._getNewInstance({
+       ...this.model,
+       ...formValue,
+       domainInfo: domainInfo,
+       mainUNOCHACategoryInfo: mainUNOCHACategoryInfo,
+       mainDACCategoryInfo: mainDACCategoryInfo,
+      });
+  }
+  
+  saveFail(error: Error): void {
+    throw new Error(error.message);
+  }
+
+  buildForm(): void {
+    this.form = this.fb.group(this.model.buildForm(true));
+  }
+  
+  constructor(@Inject(DIALOG_DATA_TOKEN) data: UiCrudDialogComponentDataContract<GoalList>,
+               public lang: LangService,
+               public dialogRef: DialogRef,
+               public dialogService: DialogService,
+               public fb: UntypedFormBuilder,
+               public toast: ToastService,
+               private lookupService: LookupService,
+               private dacOchaService:DacOchaService) {
+     super();
+     this.model = data.model;
+     this.operation = data.operation;
+     this.list = data.list;
+  }
+  
 
   displayByDomain: 'DAC' | 'OCHA' | null = null;
-  private destroy$: Subject<any> = new Subject<any>();
 
-  constructor(@Inject(DIALOG_DATA_TOKEN)
-  public data: {
-    form: UntypedFormGroup,
-    readonly: boolean,
-    editItem: number,
-    model: GoalList,
-    viewOnly: boolean,
-  },
-    private dacOchaService: DacOchaService,
-    private lookupService: LookupService,
-    public lang: LangService,
-    private dialogRef: DialogRef) {
-    this.form = data.form;
-    this.readonly = data.readonly;
-    this.editItem = data.editItem;
-    this.model = data.model;
-    this.viewOnly = data.viewOnly;
-  }
   ngOnInit() {
     this.displayByDomain = null;
     this.listenToGoalListChange();
-    this.form.patchValue(this.model);
     this.loadOCHADACClassifications();
   }
 
@@ -93,29 +157,6 @@ export class GoalsListPopupComponent implements OnInit, OnDestroy {
       'mainUNOCHACategory'
     ) as UntypedFormControl;
   }
-  mapFormTo(form: any): GoalList {
-    let domainInfo: AdminResult =
-      this.domainsList
-        .find((x) => x.lookupKey === form.domain)
-        ?.convertToAdminResult() ?? new AdminResult();
-    let mainDACCategoryInfo: AdminResult =
-      this.mainDACCategoriesList.find(
-        (x) => x.id === form.mainDACCategory
-      ) ?? new AdminResult();
-
-    let mainUNOCHACategoryInfo: AdminResult =
-      this.mainUNOCHACategoriesList.find(
-        (x) => x.id === form.mainUNOCHACategory
-      ) ?? new AdminResult();
-
-    return new GoalList().clone({
-      ...this.model,
-      ...form,
-      domainInfo: domainInfo,
-      mainUNOCHACategoryInfo: mainUNOCHACategoryInfo,
-      mainDACCategoryInfo: mainDACCategoryInfo,
-    });
-  }
   private loadOCHADACClassifications() {
     return this.dacOchaService
       .loadAsLookups()
@@ -135,16 +176,5 @@ export class GoalsListPopupComponent implements OnInit, OnDestroy {
         })
       )
       .subscribe();
-  }
-  cancel() {
-    this.dialogRef.close(null)
-  }
-  save() {
-    this.dialogRef.close(this.mapFormTo(this.form.getRawValue()))
-  }
-  ngOnDestroy(): void {
-    this.destroy$.next();
-    this.destroy$.complete();
-    this.destroy$.unsubscribe();
   }
 }
--- src/app/modules/services/partner-approval/shared/goals-list/goals-list.component.ts
+++ src/app/modules/services/partner-approval/shared/goals-list/goals-list.component.ts
@@ -1,114 +1,68 @@
-import { OnDestroy } from '@angular/core';
 import { GoalList } from '@models/goal-list';
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { UntypedFormBuilder, UntypedFormControl, UntypedFormGroup } from '@angular/forms';
+import { Component, Input } from '@angular/core';
 import { ActionIconsEnum } from '@enums/action-icons-enum';
-import { UserClickOn } from '@enums/user-click-on.enum';
 import { CommonUtils } from '@helpers/common-utils';
 import { SortEvent } from '@contracts/sort-event';
-import { AdminLookup } from '@models/admin-lookup';
-import { AdminResult } from '@models/admin-result';
 import { IMenuItem } from '@modules/context-menu/interfaces/i-menu-item';
 import { DialogService } from '@services/dialog.service';
 import { LangService } from '@services/lang.service';
 import { ToastService } from '@services/toast.service';
-import { ReadinessStatus } from '@app/types/types';
-import { BehaviorSubject, Subject } from 'rxjs';
-import { filter, take, takeUntil, tap } from 'rxjs/operators';
 import { GoalsListPopupComponent } from '../../popups/goals-list-popup/goals-list-popup.component';
+import { UiCrudListGenericComponent } from '@app/generics/ui-crud-list-generic-component';
+import { ComponentType } from '@angular/cdk/portal';
+import { IKeyValue } from '@app/interfaces/i-key-value';
 
 @Component({
   selector: 'goals-list',
   templateUrl: './goals-list.component.html',
   styleUrls: ['./goals-list.component.css']
 })
-export class GoalsListComponent implements OnInit, OnDestroy {
-
-  constructor(
-    public lang: LangService,
-    private toastService: ToastService,
-    private dialogService: DialogService,
-    private fb: UntypedFormBuilder
-  ) { }
-
-  @Input() readonly: boolean = false;
-  private _list: GoalList[] = [];
-
-  @Input() set list(list: GoalList[]) {
-    this._list = list;
-    this.dataSource.next(this._list);
-  }
-  @Input() goal?: string = undefined;
-  get list(): GoalList[] {
-    return this._list;
+export class GoalsListComponent extends UiCrudListGenericComponent<GoalList> {
+  _getNewInstance(override?: Partial<GoalList> | undefined): GoalList {
+    return new GoalList().clone(override ?? {});
   }
 
-  filterControl: UntypedFormControl = new UntypedFormControl('');
-
-  @Output() readyEvent = new EventEmitter<ReadinessStatus>();
-
-  dataSource: BehaviorSubject<GoalList[]> = new BehaviorSubject<GoalList[]>([]);
-  columns = ['domain', 'mainDACCategory', 'mainUNOCHACategory', 'actions'];
-
-  editItem?: GoalList;
-  add$: Subject<any> = new Subject<any>();
-  viewOnly: boolean = false;
-
-  private save$: Subject<any> = new Subject<any>();
-
-  private recordChanged$: Subject<GoalList | null> = new Subject<GoalList | null>();
-  private current?: GoalList;
-  private destroy$: Subject<any> = new Subject<any>();
-
-  mainDACCategoryInfo: AdminLookup[] = [];
-  mainUNOCHACategoryInfo: AdminLookup[] = [];
-  domainInfo: AdminLookup[] = [];
-  form!: UntypedFormGroup;
-
-  ngOnInit(): void {
-    this.buildForm();
-    this.listenToAdd();
-    this.listenToChange();
-    this.listenToSave();
-    this._setComponentReadiness('READY');
-
+  _getDialogComponent(): ComponentType<any> {
+    return GoalsListPopupComponent
+  }
+  
+  _getDeleteConfirmMessage(record: GoalList): string {
+    return this.lang.map.msg_confirm_delete_selected
+  }
+  
+  getExtraDataForPopup(): IKeyValue {
+    return {};
   }
 
-  ngOnDestroy(): void {
-    this.destroy$.next();
-    this.destroy$.complete();
-    this.destroy$.unsubscribe();
-  }
-
-  private buildForm() {
-    const goalList = new GoalList();
-    this.form = this.fb.group(goalList.buildForm(true));
+  @Input() goalsList: GoalList[] = [];
+  constructor(public lang: LangService,
+    public toast: ToastService,
+    public dialog: DialogService) {
+  super();
   }
   actions: IMenuItem<GoalList>[] = [
-    // edit
     {
       type: 'action',
       icon: ActionIconsEnum.EDIT,
       label: 'btn_edit',
-      onClick: (item: GoalList) => this.edit(item),
+      onClick: (item: GoalList) => this.edit$.next(item),
       show: (_item: GoalList) => !this.readonly,
     },
-    // delete
     {
       type: 'action',
       icon: ActionIconsEnum.DELETE,
       label: 'btn_delete',
-      onClick: (item: GoalList) => this.delete(item),
+      onClick: (item: GoalList) => this.confirmDelete$.next(item),
       show: (_item: GoalList) => !this.readonly,
     },
-    // view
     {
       type: 'action',
       icon: ActionIconsEnum.VIEW,
       label: 'view',
-      onClick: (item: GoalList) => this.view(item),
+      onClick: (item: GoalList) => this.view$.next(item),
     },
   ];
+  displayColumns: string[] = ['domain', 'mainDACCategory', 'mainUNOCHACategory', 'actions'];
   sortingCallbacks = {
     domain: (a: GoalList, b: GoalList, dir: SortEvent): number => {
       let value1 = !CommonUtils.isValidValue(a)
@@ -138,191 +92,4 @@ export class GoalsListComponent implements OnInit, OnDestroy {
       return CommonUtils.getSortValue(value1, value2, dir.direction);
     },
   };
-  addAllowed(): boolean {
-    return !this.readonly;
-  }
-
-  _getPopupComponent() {
-    return GoalsListPopupComponent;
-  }
-  openFormDialog() {
-    this.dialogService.show(this._getPopupComponent(), {
-      viewOnly: this.viewOnly,
-      readonly: this.readonly,
-      form: this.form,
-      editItem: this.editItem,
-      model: this.current
-    }).onAfterClose$.subscribe((data) => {
-      if (data) {
-        this.save(data)
-      } else {
-        this.cancel()
-      }
-    })
-  }
-  private listenToAdd() {
-    this.add$.pipe(takeUntil(this.destroy$)).subscribe(() => {
-      this.viewOnly = false;
-      this.recordChanged$.next(new GoalList());
-    });
-  }
-
-  private listenToChange() {
-    this.recordChanged$.pipe(takeUntil(this.destroy$)).subscribe((goalList) => {
-      this.current = goalList || undefined;
-      this.updateForm(this.current);
-    });
-  }
-
-  private updateForm(record: GoalList | undefined) {
-    if (record) {
-      if (this.viewOnly) {
-        this._setComponentReadiness('READY');
-      } else {
-        this._setComponentReadiness('NOT_READY');
-      }
-      this.openFormDialog();
-      if (this.readonly || this.viewOnly) {
-        this.form.disable();
-      } else {
-        this.form.enable();
-      }
-    } else {
-      this._setComponentReadiness('READY');
-    }
-  }
-
-  save(model: GoalList) {
-    if (this.readonly || this.viewOnly) {
-      return;
-    }
-    this.save$.next(model);
-  }
-
-  private listenToSave() {
-    this.save$
-      .pipe(
-        takeUntil(this.destroy$),
-        tap((_) =>
-          this.form.invalid ? this.displayRequiredFieldsMessage() : true
-        ),
-        filter(() => this.form.valid),
-        filter(() => {
-          const formValue = this.form.getRawValue();
-          const isDuplicate = this.list.some(
-            (x) =>
-              (x.mainDACCategory === formValue.mainDACCategory) &&
-              (x.mainUNOCHACategory === formValue.mainUNOCHACategory)
-          );
-          if (isDuplicate) {
-            this.toastService.alert(this.lang.map.msg_duplicated_item);
-            this.openFormDialog();
-          }
-          return !isDuplicate;
-        })
-      )
-      .subscribe((goalList: GoalList) => {
-        if (!goalList) {
-          return;
-        }
-        this._updateList(goalList, !!this.editItem ? 'UPDATE' : 'ADD');
-        this.toastService.success(this.lang.map.msg_save_success);
-        this.recordChanged$.next(null);
-        this.cancel();
-      });
-
-  }
-  private displayRequiredFieldsMessage(): void {
-    this.dialogService
-      .error(this.lang.map.msg_all_required_fields_are_filled)
-      .onAfterClose$.pipe(take(1))
-      .subscribe(() => {
-        this.form.markAllAsTouched();
-      });
-  }
-  private _updateList(
-    record: GoalList | null,
-    operation: 'ADD' | 'UPDATE' | 'DELETE' | 'NONE'
-  ) {
-    if (record) {
-      if (operation === 'ADD') {
-        this.list.push(record);
-      } else {
-        let index = !this.editItem
-          ? -1
-          : this.list.findIndex((x) => x === this.editItem);
-        if (operation === 'UPDATE') {
-          this.list.splice(index, 1, record);
-        } else if (operation === 'DELETE') {
-          this.list.splice(index, 1);
-        }
-      }
-    }
-    this.list = this.list.slice();
-
-  }
-
-  edit(record: GoalList, $event?: MouseEvent) {
-    $event?.preventDefault();
-    if (this.readonly) {
-      return;
-    }
-    this.editItem = record;
-    this.viewOnly = false;
-    this.recordChanged$.next(record);
-  }
-
-  view(record: GoalList, $event?: MouseEvent) {
-    $event?.preventDefault();
-    this.editItem = record;
-    this.viewOnly = true;
-    this.recordChanged$.next(record);
-  }
-
-  delete(record: GoalList, $event?: MouseEvent): any {
-    $event?.preventDefault();
-    if (this.readonly) {
-      return;
-    }
-    this.dialogService
-      .confirm(this.lang.map.msg_confirm_delete_selected)
-      .onAfterClose$.pipe(take(1))
-      .subscribe((click: UserClickOn) => {
-        if (click === UserClickOn.YES) {
-          this.editItem = record;
-          this._updateList(record, 'DELETE');
-          this.toastService.success(this.lang.map.msg_delete_success);
-          this.cancel();
-        }
-      });
-  }
-
-  cancel() {
-    this.resetForm();
-    this.viewOnly = false;
-    this.editItem = undefined;
-    this._setComponentReadiness('READY');
-  }
-
-  private resetForm() {
-    this.form.reset();
-    this.form.markAsUntouched();
-    this.form.markAsPristine();
-  }
-
-  private _setComponentReadiness(readyStatus: ReadinessStatus) {
-    this.readyEvent.emit(readyStatus);
-  }
-
-  forceClearComponent() {
-    this.cancel();
-    this.list = [];
-    this._updateList(null, 'NONE');
-    this._setComponentReadiness('READY');
-  }
-
-  trackBy(item: AdminResult) {
-    return item.id;
-  }
-
 }
