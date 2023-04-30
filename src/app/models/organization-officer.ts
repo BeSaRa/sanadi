347552d8ed0b1058e56c3f932b013f71ed785880
aalshahin <aalshahin@eblacorp.com>
2023-04-30 17:21:52 +0400

    Organization Officers

--- src/app/models/organization-officer.ts
+++ src/app/models/organization-officer.ts
@@ -6,9 +6,9 @@ import {IAuditModelProperties} from '@app/interfaces/i-audit-model-properties';
 import {OrganizationOfficerInterceptor} from '@app/model-interceptors/organization-officer-interceptor';
 import {SearchableCloneable} from '@app/models/searchable-cloneable';
 import {ControlValueLabelLangKey, ISearchFieldsMap} from '@app/types/types';
-import {FactoryService} from '@services/factory.service';
-import {LangService} from '@services/lang.service';
-import {AdminResult} from './admin-result';
+import {CustomValidators } from '@app/validators/custom-validators';
+import { FactoryService } from '@services/factory.service';
+import { LangService } from '@services/lang.service';import {AdminResult} from './admin-result';
 
 const { receive, send } = new OrganizationOfficerInterceptor();
 
@@ -55,5 +55,20 @@ export class OrganizationOfficer extends SearchableCloneable<OrganizationOfficer
         adminResultValue = AdminResult.createInstance({ arName: value as string, enName: value as string });
     }
     return adminResultValue ?? new AdminResult();
+  }
+
+  buildForm(controls:boolean) {
+    return {
+      identificationNumber: controls?
+        [null, [CustomValidators.required].concat(CustomValidators.commonValidations.qId)]:this.identificationNumber,
+      fullName: controls?
+        [null, [CustomValidators.required, CustomValidators.maxLength(CustomValidators.defaultLengths.ENGLISH_NAME_MAX)]]:this.fullName,
+      email: controls?
+        [null, [CustomValidators.required, CustomValidators.maxLength(50), CustomValidators.pattern('EMAIL')]]:this.email,
+      phone: controls?
+        [null, [CustomValidators.required].concat(CustomValidators.commonValidations.phone)]:this.phone,
+      extraPhone: controls?
+        [null, CustomValidators.commonValidations.phone]:this.extraPhone
+    }
   }
 }
