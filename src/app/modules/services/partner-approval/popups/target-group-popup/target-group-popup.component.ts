f7950997a45f99572de1094c1ea34d010ea65645
aalshahin <aalshahin@eblacorp.com>
2023-04-30 11:40:37 +0400

    target group

--- src/app/modules/services/partner-approval/popups/target-group-popup/target-group-popup.component.ts
+++ src/app/modules/services/partner-approval/popups/target-group-popup/target-group-popup.component.ts
@@ -1,51 +1,76 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { UntypedFormGroup } from '@angular/forms';
+import { UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
+import { UiCrudDialogComponentDataContract } from '@app/contracts/ui-crud-dialog-component-data-contract';
+import { OperationTypes } from '@app/enums/operation-types.enum';
+import { UiCrudDialogGenericComponent } from '@app/generics/ui-crud-dialog-generic-component.directive';
+import { ILanguageKeys } from '@app/interfaces/i-language-keys';
 import { TargetGroup } from '@app/models/target-group';
+import { DialogService } from '@app/services/dialog.service';
 import { LangService } from '@app/services/lang.service';
+import { ToastService } from '@app/services/toast.service';
 import { DialogRef } from '@app/shared/models/dialog-ref';
 import { DIALOG_DATA_TOKEN } from '@app/shared/tokens/tokens';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-target-group-popup',
   templateUrl: './target-group-popup.component.html',
   styleUrls: ['./target-group-popup.component.scss']
 })
-export class TargetGroupPopupComponent implements OnInit {
-  form: UntypedFormGroup;
-  readonly: boolean;
-  editItem: number;
+export class TargetGroupPopupComponent extends UiCrudDialogGenericComponent<TargetGroup> {
   model: TargetGroup;
-  viewOnly: boolean;
+  form!: UntypedFormGroup;
+  operation: OperationTypes;
+  popupTitleKey!: keyof ILanguageKeys;
 
-  constructor(@Inject(DIALOG_DATA_TOKEN)
-  public data: {
-    form: UntypedFormGroup,
-    readonly: boolean,
-    editItem: number,
-    model: TargetGroup,
-    viewOnly: boolean,
-  },
-    public lang: LangService,
-    private dialogRef: DialogRef) {
-    this.form = data.form;
-    this.readonly = data.readonly;
-    this.editItem = data.editItem;
+  _getNewInstance(override?: Partial<TargetGroup> | undefined): TargetGroup {
+    return new TargetGroup().clone(override ?? {});
+  }
+  initPopup(): void {
+    this.popupTitleKey = 'target_groups';
+  }
+  destroyPopup(): void {
+  }
+  afterSave(savedModel: TargetGroup, originalModel: TargetGroup): void {
+    this.toast.success(this.operation === OperationTypes.CREATE
+      ? this.lang.map.msg_added_in_list_success : this.lang.map.msg_updated_in_list_success);
+    this.dialogRef.close(savedModel);
+  }
+  beforeSave(model: TargetGroup, form: UntypedFormGroup): boolean | Observable<boolean> {
+    if (this.form.invalid) {
+      this.displayRequiredFieldsMessage();
+      return false;
+    }
+    const isDuplicate = this.list.some((x) => x === form.getRawValue());
+    if (isDuplicate) {
+      this.displayDuplicatedItemMessage();
+      return false;
+    }
+    return true;
+  }
+  prepareModel(model: TargetGroup, form: UntypedFormGroup): TargetGroup | Observable<TargetGroup> {
+    let formValue = form.getRawValue();
+     return this._getNewInstance({
+       ...this.model,
+       ...formValue,
+     });
+  }
+  saveFail(error: Error): void {
+    throw new Error(error.message);
+  }
+  buildForm(): void {
+    this.form = this.fb.group(this.model.getTargetGroupFields(true));
+  }
+  
+  constructor(@Inject(DIALOG_DATA_TOKEN) data: UiCrudDialogComponentDataContract<TargetGroup>,
+  public lang: LangService,
+  public dialogRef: DialogRef,
+  public dialogService: DialogService,
+  public fb: UntypedFormBuilder,
+  public toast: ToastService) {
+    super();
     this.model = data.model;
-    this.viewOnly = data.viewOnly;
-  }
-  ngOnInit() {
-    this.form.patchValue(this.model);
-  }
-
-  mapFormTo(form: any): TargetGroup {
-    const model: TargetGroup = new TargetGroup().clone({...this.model, ...form});
-
-    return model;
-  }
-  cancel() {
-    this.dialogRef.close(null)
-  }
-  save() {
-    this.dialogRef.close(this.mapFormTo(this.form.getRawValue()))
+    this.operation = data.operation;
+    this.list = data.list;
   }
 }
