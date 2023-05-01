ffd502a7b6ecd936c0c6e761593a15e149787694
aalshahin <aalshahin@eblacorp.com>
2023-05-01 12:52:54 +0400

    best peactices

--- src/app/modules/services/urgent-intervention-closure/popups/best-practices-popup/best-practices-popup.component.ts
+++ src/app/modules/services/urgent-intervention-closure/popups/best-practices-popup/best-practices-popup.component.ts
@@ -1,55 +1,99 @@
 import { Component, Inject } from '@angular/core';
-import { UntypedFormGroup } from '@angular/forms';
+import { UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
+import { UiCrudDialogComponentDataContract } from '@app/contracts/ui-crud-dialog-component-data-contract';
+import { FieldAssessmentTypesEnum } from '@app/enums/field-assessment-types.enum';
+import { OperationTypes } from '@app/enums/operation-types.enum';
+import { UiCrudDialogGenericComponent } from '@app/generics/ui-crud-dialog-generic-component.directive';
+import { ILanguageKeys } from '@app/interfaces/i-language-keys';
 import { AdminResult } from '@app/models/admin-result';
 import { BestPractices } from '@app/models/best-practices';
+import { DialogService } from '@app/services/dialog.service';
+import { FieldAssessmentService } from '@app/services/field-assessment.service';
 import { LangService } from '@app/services/lang.service';
+import { ToastService } from '@app/services/toast.service';
 import { DialogRef } from '@app/shared/models/dialog-ref';
 import { DIALOG_DATA_TOKEN } from '@app/shared/tokens/tokens';
-import { CustomValidators } from '@app/validators/custom-validators';
+import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 @Component({
   selector: 'best-practices-popup',
   templateUrl: './best-practices-popup.component.html',
   styleUrls: ['./best-practices-popup.component.scss']
 })
-export class BestPracticesPopupComponent {
-  form: UntypedFormGroup;
-  readonly: boolean;
-  viewOnly: boolean;
-  editItem: boolean;
+export class BestPracticesPopupComponent extends UiCrudDialogGenericComponent<BestPractices>  {
   model: BestPractices;
-  bestPracticesList: AdminResult[];
-  customValidators = CustomValidators
-  constructor(@Inject(DIALOG_DATA_TOKEN)
-    public data: {
-      form: UntypedFormGroup,
-      readonly: boolean,
-      viewOnly: boolean,
-      editItem: boolean,
-      model: BestPractices,
-      bestPracticesList: AdminResult[];
-    },
-    public lang: LangService,
-    private dialogRef: DialogRef) {
-      this.form = data.form;
-      this.readonly = data.readonly;
-      this.viewOnly = data.viewOnly;
-      this.editItem = data.editItem;
-      this.model = data.model;
-      this.bestPracticesList = data.bestPracticesList;
-  }
-  mapFormTo(form: any): BestPractices {
-    const model: BestPractices = new BestPractices().clone(form);
+  form!: UntypedFormGroup;
+  operation: OperationTypes;
+  popupTitleKey!: keyof ILanguageKeys;
+  bestPracticesList: AdminResult[] = [];
 
-    return model;
+  constructor(@Inject(DIALOG_DATA_TOKEN) data: UiCrudDialogComponentDataContract<BestPractices>,
+  public lang: LangService,
+  public dialogRef: DialogRef,
+  public dialogService: DialogService,
+  public fb: UntypedFormBuilder,
+  public toast: ToastService,
+  private fieldAssessmentService: FieldAssessmentService) {
+    super();
+    this.model = data.model;
+    this.operation = data.operation;
+    this.list = data.list;
   }
-  cancel() {
-    this.dialogRef.close(null)
+
+  buildForm(): void {
+    this.form = this.fb.group(this.model.buildForm(true));
   }
-  save() {
-    this.dialogRef.close(this.mapFormTo(this.form.getRawValue()))
+
+  afterSave(savedModel: BestPractices, originalModel: BestPractices): void {
+    this.toast.success(this.operation === OperationTypes.CREATE
+      ? this.lang.map.msg_added_in_list_success : this.lang.map.msg_updated_in_list_success);
+    this.dialogRef.close(savedModel);
+  }
+
+  beforeSave(model: BestPractices, form: UntypedFormGroup): Observable<boolean> | boolean {
+    if (this.form.invalid) {
+      this.displayRequiredFieldsMessage();
+      return false;
+    }
+    const isDuplicate = this.list.some((x) => x === form.getRawValue());
+    if (isDuplicate) {
+      this.displayDuplicatedItemMessage();
+      return false;
+    }
+    return true;
+  }
+  prepareModel(model: BestPractices, form: UntypedFormGroup): Observable<BestPractices> | BestPractices {
+    let formValue = form.getRawValue();
+    let bestPracticesInfo = this.bestPracticesList.filter(x => formValue.bestPractices.includes(x.id));
+    return this._getNewInstance({
+      ...this.model,
+      ...formValue,
+      bestPracticesInfo
+    });
+  }
+
+  saveFail(error: Error): void {
+  }
+
+  destroyPopup(): void {
   }
   searchNgSelect(term: string, item: AdminResult): boolean {
     return item.ngSelectSearch(term);
+  }
+  _getNewInstance(override?: Partial<BestPractices> | undefined): BestPractices {
+    return new BestPractices().clone(override ?? {});
+  }
+  initPopup(): void {
+    this.popupTitleKey = 'best_practices';
+    this.fieldAssessmentService.loadByType(FieldAssessmentTypesEnum.BEST_PRACTICES)
+      .pipe(
+        catchError(() => of([])),
+        map(result => {
+          return result.map(x => x.convertToAdminResult());
+        })
+      ).subscribe((result) => {
+      this.bestPracticesList = result;
+    });
   }
 }
