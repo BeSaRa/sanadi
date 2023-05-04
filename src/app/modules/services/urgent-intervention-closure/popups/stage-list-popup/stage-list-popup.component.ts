1e888c1e17b7f7d8c2776d9f6650b0b5c6c55a7a
Abdulaziz-eblacorp <ashahrour@eblacorp.com>
2023-05-04 19:13:15 +0300

    update -- stage-list

--- src/app/modules/services/urgent-intervention-closure/popups/stage-list-popup/stage-list-popup.component.ts
+++ src/app/modules/services/urgent-intervention-closure/popups/stage-list-popup/stage-list-popup.component.ts
@@ -1,49 +1,77 @@
 import { Component, Inject } from '@angular/core';
-import { UntypedFormGroup } from '@angular/forms';
+import { UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
+import { UiCrudDialogComponentDataContract } from '@app/contracts/ui-crud-dialog-component-data-contract';
+import { OperationTypes } from '@app/enums/operation-types.enum';
+import { UiCrudDialogGenericComponent } from '@app/generics/ui-crud-dialog-generic-component.directive';
+import { ILanguageKeys } from '@app/interfaces/i-language-keys';
 import { Stage } from '@app/models/stage';
+import { DialogService } from '@app/services/dialog.service';
 import { LangService } from '@app/services/lang.service';
+import { ToastService } from '@app/services/toast.service';
 import { DialogRef } from '@app/shared/models/dialog-ref';
 import { DIALOG_DATA_TOKEN } from '@app/shared/tokens/tokens';
 import { CustomValidators } from '@app/validators/custom-validators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'stage-list-popup',
   templateUrl: './stage-list-popup.component.html',
   styleUrls: ['./stage-list-popup.component.scss']
 })
-export class StageListPopupComponent {
-  form: UntypedFormGroup;
-  readonly: boolean;
-  viewOnly: boolean;
-  editItem: boolean;
-  
+export class StageListPopupComponent extends UiCrudDialogGenericComponent<Stage>  {
+  operation: OperationTypes;
+  popupTitleKey!: keyof ILanguageKeys;
+  form!: UntypedFormGroup;
+  model!:  Stage;
   inputMaskPatterns = CustomValidators.inputMaskPatterns;
-  customValidators = CustomValidators
-  constructor(@Inject(DIALOG_DATA_TOKEN)
-    public data: {
-      form: UntypedFormGroup,
-      readonly: boolean,
-      viewOnly: boolean,
-      editItem: boolean,
 
-    },
+  constructor(@Inject(DIALOG_DATA_TOKEN) data: UiCrudDialogComponentDataContract<Stage>,
     public lang: LangService,
-    private dialogRef: DialogRef) {
-      this.form = data.form;
-      this.readonly = data.readonly;
-      this.viewOnly = data.viewOnly;
-      this.editItem = data.editItem;
+    public dialogRef: DialogRef,
+    public dialogService: DialogService,
+    public fb: UntypedFormBuilder,
+    public toast: ToastService) {
+    super();
+    this.model = data.model;
+    this.operation = data.operation;
+    this.list = data.list;
+  }
+  _getNewInstance(override?: Partial<Stage> | undefined): Stage {
+    return new Stage().clone(override ?? {});
+  }
+  initPopup(): void {
+    this.popupTitleKey = 'stage';
+  }
+  destroyPopup(): void {
+  }
+  afterSave(savedModel: Stage, originalModel: Stage): void {
+    this.toast.success(this.operation === OperationTypes.CREATE
+      ? this.lang.map.msg_added_in_list_success : this.lang.map.msg_updated_in_list_success);
+    this.dialogRef.close(savedModel);
+  }
+  beforeSave(model: Stage, form: UntypedFormGroup): boolean | Observable<boolean> {
+    if (this.form.invalid) {
+      this.displayRequiredFieldsMessage();
+      return false;
+    }
+    const isDuplicate = this.list.some((x) => x === form.getRawValue());
+    if (isDuplicate) {
+      this.displayDuplicatedItemMessage();
+      return false;
+    }
+    return true;
+  }
+  prepareModel(model: Stage, form: UntypedFormGroup): Stage | Observable<Stage> {
+    let formValue = form.getRawValue();
+    return this._getNewInstance({
+      ...this.model,
+      ...formValue,
+    });
+  }
+  saveFail(error: Error): void {
+  }
+  buildForm(): void {
+    this.form = this.fb.group(this.model.buildForm(true));
+  }
 
-  }
-  mapFormTo(form: any): Stage {
-    const model: Stage = new Stage().clone(form);
-
-    return model;
-  }
-  cancel() {
-    this.dialogRef.close(null)
-  }
-  save() {
-    this.dialogRef.close(this.mapFormTo(this.form.getRawValue()))
-  }
 }
