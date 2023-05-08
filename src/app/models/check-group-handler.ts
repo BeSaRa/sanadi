5f80f034d7a1fa6cb486d1f106bfbde9f0aea12b
Mamoun-Hamed <108342857+Mamoun-Hamed@users.noreply.github.com>
2023-05-08 15:10:15 +0300

    make new  function for handling  default permission clicking

--- src/app/models/check-group-handler.ts
+++ src/app/models/check-group-handler.ts
@@ -22,28 +22,28 @@ export class CheckGroupHandler<T extends { id: number }> {
     this.onGroupClickedCallback && this.onGroupClickedCallback();
   }
 
-  /*onCheckBoxClicked(item: T, {target}: Event, group: CheckGroup<T>): void {
+  onCheckBoxClicked(item: T, {target}: Event, group: CheckGroup<T>): void {
     let check = CheckGroupHandler.getCheckState(target);
     check ? this.addToSelection(item, group) : this.removeFromSelection(item, group);
     this.onCheckBoxClickedCallback && this.onCheckBoxClickedCallback(item, check, group);
-  }*/
+   }
 
-  onCheckBoxClicked(item: T, { target }: Event, group: CheckGroup<T>): void {
-    const menu = new CustomMenu().clone(item);
-    let check = CheckGroupHandler.getCheckState(target);
-    if (menu.isSystemParentItem()) {
-      if (check) {
-        this.checkAllChildren(menu, group);
-        return;
-      }
-      this.unCheckAllChildren(menu, group);
-      return;
-    }
-    check ? this.addToSelection(item, group) : this.removeFromSelection(item, group);
-    this.onCheckBoxClickedCallback && this.onCheckBoxClickedCallback(item, check, group);
-  }
+  // onCheckBoxClicked(item: T, { target }: Event, group: CheckGroup<T>): void {
+  //   const menu = new CustomMenu().clone(item);
+  //   let check = CheckGroupHandler.getCheckState(target);
+  //   if (menu.isSystemParentItem()) {
+  //     if (check) {
+  //       this.checkAllChildren(menu, group);
+  //       return;
+  //     }
+  //     this.unCheckAllChildren(menu, group);
+  //     return;
+  //   }
+  //   check ? this.addToSelection(item, group) : this.removeFromSelection(item, group);
+  //   this.onCheckBoxClickedCallback && this.onCheckBoxClickedCallback(item, check, group);
+  // }
 
-  private unCheckAllChildren(menu: CustomMenu, group: CheckGroup<T>) {
+   unCheckAllChildren(menu: CustomMenu, group: CheckGroup<T>) {
     menu.getChildrenIds().forEach(id => {
       if (!this.selection.includes(id))
         return;
@@ -52,7 +52,7 @@ export class CheckGroupHandler<T extends { id: number }> {
     });
   }
 
-  private checkAllChildren(menu: CustomMenu, group: CheckGroup<T>) {
+   checkAllChildren(menu: CustomMenu, group: CheckGroup<T>) {
     menu.getChildrenIds().forEach(id => {
       if (this.selection.includes(id))
         return;
@@ -69,7 +69,7 @@ export class CheckGroupHandler<T extends { id: number }> {
     this.selection = this.groups.reduce((acc, group) => acc.concat(group.getSelectedValue()), [] as number[]);
   }
 
-  private addToSelection(item: T, group: CheckGroup<T>): void {
+   addToSelection(item: T, group: CheckGroup<T>): void {
     if(!!item.id){
       group.addToSelection(item.id);
       this.selection = this.selection.concat(item.id);
@@ -80,7 +80,7 @@ export class CheckGroupHandler<T extends { id: number }> {
     this.addToSelection(item, group);
   }
 
-  private removeFromSelection(item: T, group: CheckGroup<T>): void {
+   removeFromSelection(item: T, group: CheckGroup<T>): void {
     if(!!item.id){
       group.removeFromSelection(item.id);
       this.selection.splice(this.selection.indexOf(item.id), 1);
