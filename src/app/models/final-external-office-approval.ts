ff19634c8a39c151f7d1b95aefd8560abb98b451
Navjot Singh <navjotsandha@gmail.com>
2023-05-25 11:19:50 +0300

    Bug 57183:UI: Final External Office Approval service: old value of "Branch Establishment Date" is displayed as an undefined number

--- src/app/models/final-external-office-approval.ts
+++ src/app/models/final-external-office-approval.ts
@@ -1,31 +1,32 @@
-import { FinalExternalOfficeApprovalService } from '@services/final-external-office-approval.service';
-import { FactoryService } from '@services/factory.service';
-import { BankAccount } from './bank-account';
-import { BankBranch } from './bank-branch';
-import { ExecutiveManagement } from './executive-management';
-import { CustomValidators } from '@app/validators/custom-validators';
-import { Validators } from '@angular/forms';
-import { LicenseApprovalModel } from '@app/models/license-approval-model';
-import { DateUtils } from '@app/helpers/date-utils';
-import { AdminResult } from '@app/models/admin-result';
-import { CaseTypes } from '@app/enums/case-types.enum';
-import { ControlValueLabelLangKey, ISearchFieldsMap } from "@app/types/types";
-import { dateSearchFields } from "@app/helpers/date-search-fields";
-import { infoSearchFields } from "@app/helpers/info-search-fields";
-import { normalSearchFields } from "@app/helpers/normal-search-fields";
+import {FinalExternalOfficeApprovalService} from '@services/final-external-office-approval.service';
+import {FactoryService} from '@services/factory.service';
+import {BankAccount} from './bank-account';
+import {BankBranch} from './bank-branch';
+import {ExecutiveManagement} from './executive-management';
+import {CustomValidators} from '@app/validators/custom-validators';
+import {Validators} from '@angular/forms';
+import {LicenseApprovalModel} from '@app/models/license-approval-model';
+import {DateUtils} from '@app/helpers/date-utils';
+import {AdminResult} from '@app/models/admin-result';
+import {CaseTypes} from '@app/enums/case-types.enum';
+import {ControlValueLabelLangKey, ISearchFieldsMap} from "@app/types/types";
+import {dateSearchFields} from "@app/helpers/date-search-fields";
+import {infoSearchFields} from "@app/helpers/info-search-fields";
+import {normalSearchFields} from "@app/helpers/normal-search-fields";
 import {
   FinalExternalOfficeApprovalInterceptor
 } from '@app/model-interceptors/final-external-office-approval-interceptor';
-import { InterceptModel } from "@decorators/intercept-model";
-import { AuditOperationTypes } from '@app/enums/audit-operation-types';
-import { IAuditModelProperties } from '@app/interfaces/i-audit-model-properties';
-import { CommonUtils } from '@app/helpers/common-utils';
-import { ObjectUtils } from '@app/helpers/object-utils';
+import {InterceptModel} from "@decorators/intercept-model";
+import {AuditOperationTypes} from '@app/enums/audit-operation-types';
+import {IAuditModelProperties} from '@app/interfaces/i-audit-model-properties';
+import {CommonUtils} from '@app/helpers/common-utils';
+import {ObjectUtils} from '@app/helpers/object-utils';
+import {ILanguageKeys} from "@contracts/i-language-keys";
 
 
-const { send, receive } = new FinalExternalOfficeApprovalInterceptor();
+const {send, receive} = new FinalExternalOfficeApprovalInterceptor();
 
-@InterceptModel({ send, receive })
+@InterceptModel({send, receive})
 export class FinalExternalOfficeApproval extends LicenseApprovalModel<FinalExternalOfficeApprovalService, FinalExternalOfficeApproval> implements IAuditModelProperties<FinalExternalOfficeApproval> {
   caseType: number = CaseTypes.FINAL_EXTERNAL_OFFICE_APPROVAL;
   requestType!: number;
@@ -44,7 +45,7 @@ export class FinalExternalOfficeApproval extends LicenseApprovalModel<FinalExter
   managerDecision?: number;
   reviewerDepartmentDecision?: number;
   establishmentDate: string = '';
-  establishmentDateTimeStamp!: number|null;
+  establishmentDateTimeStamp!: number | null;
   address: string = '';
   email: string = '';
   externalOfficeName: string = '';
@@ -94,30 +95,37 @@ export class FinalExternalOfficeApproval extends LicenseApprovalModel<FinalExter
       delete this.searchFields.organization;
     }
   }
+
   auditOperation: AuditOperationTypes = AuditOperationTypes.NO_CHANGE;
+
   getBasicInfoValuesWithLabels(): { [key: string]: ControlValueLabelLangKey } {
     return {
       requestType: {langKey: 'request_type', value: this.requestType},
-      oldLicenseFullSerial:{langKey: 'license_number', value: this.oldLicenseFullSerial},
-      oldLicenseId:{langKey: 'license_number', value: this.oldLicenseId},
-      oldLicenseSerial:{langKey: 'serial_number', value: this.oldLicenseSerial},
-      country:{langKey: 'country', value: this.country},
-      region:{langKey: 'region', value: this.region},
-      externalOfficeName:{langKey: 'external_office_name', value: this.externalOfficeName},
-      establishmentDate:{langKey: 'establishment_date', value: this.establishmentDate,comparisonValue: this.establishmentDateTimeStamp},
-      recordNo:{langKey: 'record_number', value: this.recordNo},
-      address:{langKey: 'lbl_address', value: this.address},
-      phone:{langKey: 'lbl_phone', value: this.phone},
-      fax:{langKey: 'fax_number', value: this.fax},
-      postalCode:{langKey: 'postal_code', value: this.postalCode},
-      email:{langKey: 'lbl_email', value: this.email},
-      description:{langKey: 'special_explanations', value: this.description},
-      headQuarterType:{langKey: 'office_type', value: this.headQuarterType},
-      licenseDuration:{langKey: 'license_duration', value: this.licenseDuration},
-      licenseDurationType:{langKey: 'license_duration_type', value: this.licenseDurationType},
-      countries:{langKey: 'states', value: this.countries}
+      oldLicenseFullSerial: {langKey: 'license_number', value: this.oldLicenseFullSerial},
+      oldLicenseId: {langKey: {} as keyof ILanguageKeys, value: this.oldLicenseId, skipAuditComparison: true},
+      oldLicenseSerial: {langKey: 'serial_number', value: this.oldLicenseSerial, skipAuditComparison: true},
+      country: {langKey: 'country', value: this.country},
+      region: {langKey: 'region', value: this.region},
+      externalOfficeName: {langKey: 'external_office_name', value: this.externalOfficeName},
+      establishmentDate: {
+        langKey: 'establishment_date',
+        value: this.establishmentDate,
+        comparisonValue: this.establishmentDateTimeStamp
+      },
+      recordNo: {langKey: 'record_number', value: this.recordNo},
+      address: {langKey: 'lbl_address', value: this.address},
+      phone: {langKey: 'lbl_phone', value: this.phone},
+      fax: {langKey: 'fax_number', value: this.fax},
+      postalCode: {langKey: 'postal_code', value: this.postalCode},
+      email: {langKey: 'lbl_email', value: this.email},
+      description: {langKey: 'special_explanations', value: this.description},
+      headQuarterType: {langKey: 'office_type', value: this.headQuarterType},
+      licenseDuration: {langKey: 'license_duration', value: this.licenseDuration},
+      licenseDurationType: {langKey: 'license_duration_type', value: this.licenseDurationType},
+      countries: {langKey: 'states', value: this.countries, comparisonValue: (this.countries ?? []).sort().join(',')}
     };
   }
+
   getFormFields(control: boolean = false): any {
     const values = ObjectUtils.getControlValues<FinalExternalOfficeApproval>(this.getBasicInfoValuesWithLabels());
 
@@ -143,6 +151,7 @@ export class FinalExternalOfficeApproval extends LicenseApprovalModel<FinalExter
       countries: control ? [values.countries] : values.countries
     }
   }
+
   getAdminResultByProperty(property: keyof FinalExternalOfficeApproval): AdminResult {
     let adminResultValue: AdminResult;
     switch (property) {
@@ -166,7 +175,7 @@ export class FinalExternalOfficeApproval extends LicenseApprovalModel<FinalExter
         adminResultValue = AdminResult.createInstance({arName: dateValue, enName: dateValue});
         break;
       case 'countries':
-        const countriesValue = this.countriesInfo.filter(x=>this.countries.includes(x.id!)).map(x=>x.getName()).toString();
+        const countriesValue = this.countriesInfo.filter(x => this.countries.includes(x.id!)).map(x => x.getName()).join(', ');
         adminResultValue = AdminResult.createInstance({arName: countriesValue, enName: countriesValue});
         break;
       default:
