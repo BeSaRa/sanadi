f7f29c574ee51a795e854898781d4fc9d0fff028
Mamoun-Hamed <108342857+Mamoun-Hamed@users.noreply.github.com>
2023-06-15 14:31:21 +0300

    add  admin permission page

---
+++ src/app/administration/popups/admin-permission-popup/admin-permission-popup.component.ts
@@ -0,0 +1,98 @@
+import { AdminLookup } from './../../../models/admin-lookup';
+import { Component, Inject } from '@angular/core';
+import { UntypedFormGroup, UntypedFormBuilder } from '@angular/forms';
+import { AdminLookupTypeEnum } from '@app/enums/admin-lookup-type-enum';
+import { OperationTypes } from '@app/enums/operation-types.enum';
+import { AdminGenericDialog } from '@app/generics/admin-generic-dialog';
+import { IDialogData } from '@app/interfaces/i-dialog-data';
+import { Lookup } from '@app/models/lookup';
+import { Permission } from '@app/models/permission';
+import { AdminLookupService } from '@app/services/admin-lookup.service';
+import { DialogService } from '@app/services/dialog.service';
+import { LangService } from '@app/services/lang.service';
+import { LookupService } from '@app/services/lookup.service';
+import { ToastService } from '@app/services/toast.service';
+import { DialogRef } from '@app/shared/models/dialog-ref';
+import { DIALOG_DATA_TOKEN } from '@app/shared/tokens/tokens';
+import { Observable } from 'rxjs';
+
+@Component({
+    selector: 'admin-permission-popup',
+    templateUrl: 'admin-permission-popup.component.html',
+    styleUrls: ['admin-permission-popup.component.scss']
+})
+export class AdminPermissionPopupComponent extends AdminGenericDialog<Permission> {
+
+  categoryList: Lookup[] = this.lookupService.listByCategory.PermissionCategory;
+  groupList: AdminLookup[] = [];
+  form!: UntypedFormGroup;
+  model!: Permission;
+  operation: OperationTypes;
+  saveVisible = true;
+  isViewForm:boolean = false;
+
+  constructor(public dialogRef: DialogRef,
+              public fb: UntypedFormBuilder,
+              public lang: LangService,
+              @Inject(DIALOG_DATA_TOKEN) data: IDialogData<Permission>,
+              private toast: ToastService,
+              private lookupService: LookupService,
+              private adminLookupService:AdminLookupService) {
+    super();
+    this.model = data.model;
+    this.operation = data.operation;
+  }
+
+  initPopup(): void {
+    this.adminLookupService.loadAsLookups(AdminLookupTypeEnum.PERMISSION_GROUP)
+    .subscribe(lookups=>{
+      this.groupList = lookups!
+
+    })
+  }
+
+  buildForm(): void {
+    this.form = this.fb.group(this.model.buildForm(true));
+    if (this.operation === OperationTypes.VIEW) {
+      this.form.disable();
+      this.saveVisible = false;
+      this.validateFieldsVisible = false;
+      this.isViewForm = true;
+    }
+  }
+
+  beforeSave(model: Permission, form: UntypedFormGroup): Observable<boolean> | boolean {
+    return form.valid;
+  }
+
+  prepareModel(model: Permission, form: UntypedFormGroup): Observable<Permission> | Permission {
+    return (new Permission()).clone({...model, ...form.value});
+  }
+
+  afterSave(model: Permission, dialogRef: DialogRef): void {
+    const message = this.operation === OperationTypes.CREATE ? this.lang.map.msg_create_x_success : this.lang.map.msg_update_x_success;
+    this.toast.success(message.change({x: model.getName()}));
+    this.model = model;
+    this.operation = OperationTypes.UPDATE;
+    dialogRef.close(model);
+  }
+
+  saveFail(error: Error): void {
+  }
+
+  get popupTitle(): string {
+    if (this.operation === OperationTypes.CREATE){
+      return this.lang.map.lbl_add_permission;
+    } else if (this.operation === OperationTypes.UPDATE) {
+      return this.lang.map.lbl_edit_permission;
+    } else if (this.operation === OperationTypes.VIEW) {
+      return this.lang.map.view;
+    }
+    return '';
+  };
+
+  destroyPopup(): void {
+  }
+
+
+}
--- src/app/services/permission.service.ts
+++ src/app/services/permission.service.ts
@@ -1,3 +1,4 @@
+import { AdminPermissionPopupComponent } from './../administration/popups/admin-permission-popup/admin-permission-popup.component';
 import { Injectable } from '@angular/core';
 import { Permission } from '../models/permission';
 import { HttpClient } from '@angular/common/http';
@@ -6,6 +7,15 @@ import { FactoryService } from './factory.service';
 import { CrudGenericService } from "@app/generics/crud-generic-service";
 import { Pagination } from '@app/models/pagination';
 import { CastResponseContainer } from "@decorators/cast-response";
+import { CrudWithDialogGenericService } from '@app/generics/crud-with-dialog-generic-service';
+import { ComponentType } from '@angular/cdk/portal';
+import { DialogService } from './dialog.service';
+import { JobTitlePopupComponent } from '@app/administration/popups/job-title-popup/job-title-popup.component';
+import { Observable, of } from 'rxjs';
+import { IDialogData } from '@app/interfaces/i-dialog-data';
+import { switchMap } from 'rxjs/operators';
+import { OperationTypes } from '@app/enums/operation-types.enum';
+import { DialogRef } from '@app/shared/models/dialog-ref';
 
 @CastResponseContainer({
   $default: {
@@ -19,10 +29,13 @@ import { CastResponseContainer } from "@decorators/cast-response";
 @Injectable({
   providedIn: 'root'
 })
-export class PermissionService extends CrudGenericService<Permission> {
+export class PermissionService extends  CrudWithDialogGenericService<Permission> {
+  _getDialogComponent(): ComponentType<any> {
+    return AdminPermissionPopupComponent;
+  }
   list!: Permission[];
 
-  constructor(public http: HttpClient, private urlService: UrlService) {
+  constructor(public http: HttpClient, private urlService: UrlService, public dialog: DialogService) {
     super();
     FactoryService.registerService('PermissionService', this);
   }
@@ -33,5 +46,15 @@ export class PermissionService extends CrudGenericService<Permission> {
 
   _getServiceURL(): string {
     return this.urlService.URLS.PERMISSIONS;
+  }
+  openViewDialog(modelId: number): Observable<DialogRef> {
+    return this.getByIdComposite(modelId).pipe(
+      switchMap((permission: Permission) => {
+        return of(this.dialog.show<IDialogData<Permission>>(AdminPermissionPopupComponent, {
+          model: permission,
+          operation: OperationTypes.VIEW
+        }));
+      })
+    );
   }
 }
