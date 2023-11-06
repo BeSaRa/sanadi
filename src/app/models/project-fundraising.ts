31fdc021ada2bc9f8b78d1b199c9d7ffdcbc4fd7
Mamoun-Hamed <108342857+Mamoun-Hamed@users.noreply.github.com>
2023-11-06 11:09:17 +0200

    Bug 60254:Project fundraising - the value saved in the deduction list is not saved

--- src/app/models/project-fundraising.ts
+++ src/app/models/project-fundraising.ts
@@ -382,7 +382,7 @@ export class ProjectFundraising extends _ApprovalLicenseWithMonthly<ProjectFundr
 
   hasInvalidTargetAmount(ignoreCountries: boolean = false): boolean {
     return !this.deductedPercentagesItemList.length
-      || this.deductedPercentagesItemList.some(item => item.deductionPercent <= 0)
+      || this.deductedPercentagesItemList.some(item => item.deductionPercent < 0)
       || this.calculateAllYearsAmount() !== this.targetAmount
       || (ignoreCountries ? false : this.calculateAllCountriesAmount() !== this.targetAmount)
       || (ignoreCountries ? false : this.amountOverCountriesList.some(item => item.targetAmount <= 0))
