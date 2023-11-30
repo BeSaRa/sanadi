d3e939b490c10c1863ff185defe37959a7fdf0c0
Mamoun-Hamed <108342857+Mamoun-Hamed@users.noreply.github.com>
2023-11-30 12:33:30 +0300

    BUG 60923 : 320-NPO Support: When request sent to Support Specialist: action allowed by user "accept / reject" can't be done as system ask for adding approve/reject template, and option to upload template not appeared

--- src/app/shared/components/e-service-component-wrapper/e-service-component-wrapper.component.ts
+++ src/app/shared/components/e-service-component-wrapper/e-service-component-wrapper.component.ts
@@ -1836,17 +1836,24 @@ export class EServiceComponentWrapperComponent implements OnInit, AfterViewInit,
             });
           })
       } else if (item.getCaseType() === CaseTypes.ORGANIZATION_ENTITIES_SUPPORT) {
-        const model = item as unknown as OrganizationsEntitiesSupport;
-        const component = this.component as unknown as OrganizationsEntitiesSupportComponent;
-        component.validateApproveTimplate()
-          .pipe(
-            tap(valid => !valid && this.service.dialog.error(this.lang.map.invalid_template)),
-            filter(valid => valid)
-          ).subscribe(() => {
-            model.approve().onAfterClose$.subscribe(actionTaken => {
-              actionTaken && this.navigateToSamePageThatUserCameFrom();
-            });
-          })
+        if (!this.employeeService.isLicensingUser() || !this.employeeService.isSupervisionAndControlUser()) {
+          item.approve().onAfterClose$.subscribe(actionTaken => {
+            actionTaken && this.navigateToSamePageThatUserCameFrom();
+          });
+        } else {
+          const model = item as unknown as OrganizationsEntitiesSupport;
+          const component = this.component as unknown as OrganizationsEntitiesSupportComponent;
+          component.validateApproveTimplate()
+            .pipe(
+              tap(valid => !valid && this.service.dialog.error(this.lang.map.invalid_template)),
+              filter(valid => valid)
+            ).subscribe(() => {
+              model.approve().onAfterClose$.subscribe(actionTaken => {
+                actionTaken && this.navigateToSamePageThatUserCameFrom();
+              });
+            })
+        }
+
       }
     } else {
       item.approve().onAfterClose$.subscribe(actionTaken => {
