e7ad15688291a611bdd27bfd726a1d14777900ad
BeSaRa <ahmedbesara@gmailcom>
2024-02-22 11:13:05 +0300

    2.5.11

--- src/app/resources/default-configuration.ts
+++ src/app/resources/default-configuration.ts
@@ -4,7 +4,7 @@ import {ConfigurationMergingScope} from '@app/types/types';
 export const configurationMergingLevel: ConfigurationMergingScope = 'limited';
 
 export const defaultConfiguration: Partial<IAppConfig> = {
-  VERSION: 'v2.5.10',
+  VERSION: 'v2.5.11',
   PRIVATE_VERSION: '',
   API_VERSION: 'v1',
   BASE_ENVIRONMENT: 'BAW_UI',
