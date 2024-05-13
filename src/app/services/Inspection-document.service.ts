5fb9f9758ba5becac22a85dba5ef3235886ec296
Mamoun-Hamed <en15hamed1992@gmail.com>
2024-05-13 07:25:44 +0300

    fix attachments endpoints

--- src/app/services/Inspection-document.service.ts
+++ src/app/services/Inspection-document.service.ts
@@ -26,67 +26,7 @@ export class InspectionDocumentService extends DocumentService {
     super({ ...obj, _getURLSegment: () => obj._getServiceURL() });
 
   }
-  @CastResponse(() => FileNetDocument, {
-    fallback: '$default',
-    unwrap: 'rs'
-  })
-  loadDocuments(caseId: string): Observable<FileNetDocument[]> {
-    return this.obj.http.get<FileNetDocument[]>(this.obj._getServiceURL() + '/folder/contained-documents/' + caseId);
-  }
-
-  addSingleDocument(caseId: string,
-    document: FileNetDocument,
-    progressCallback?: (percentage: number) => void): Observable<FileNetDocument> {
-
-    return this._save(OperationTypes.CREATE, caseId, document, progressCallback);
-  }
-  @CastResponse(() => FileNetDocument, {
-    fallback: '$default',
-    unwrap: 'rs'
-  })
-  private _save(saveType: OperationTypes, caseId: string, document: FileNetDocument, progressCallback?: (percentage: number) => void): Observable<FileNetDocument> {
-    const clonedDocument = document.clone() as Partial<FileNetDocument>;
-    const content = clonedDocument.files?.item(0);
-    const formData = new FormData();
-
-    delete clonedDocument.files;
-    delete clonedDocument.dialog;
-    delete clonedDocument.searchFields;
-    delete clonedDocument.employeeService;
-    delete clonedDocument.attachmentTypeInfo;
-    delete clonedDocument.createdOn;
-    delete clonedDocument.creatorInfo;
-    delete clonedDocument.ouInfo;
-    delete clonedDocument.required;
-    delete clonedDocument.langService;
-
-    clonedDocument.denormalizeItemId && clonedDocument.denormalizeItemId();
-
-    delete clonedDocument.gridName
-    if (!clonedDocument.description) {
-      delete clonedDocument.description;
-    }
-    content ? formData.append('content', content) : null;
-    let requestUrl = this.obj._getServiceURL() + '/document';
-    if (saveType === OperationTypes.UPDATE) {
-      requestUrl = this.obj._getServiceURL() + '/document';
-    }
-    return this.obj.http.post<FileNetDocument>(requestUrl, formData, {
-      params: new HttpParams({ fromObject: { ...clonedDocument as any, taskId: caseId } }),
-      reportProgress: true,
-      observe: 'events'
-    }).pipe(
-      filter(event => {
-        if (event.type === HttpEventType.UploadProgress && progressCallback && typeof event.total !== 'undefined') {
-          progressCallback(Math.floor(event.loaded * 100 / event.total));
-        }
-        return event.type === HttpEventType.Response;
-      }),
-      map<any, FileNetDocument>((response: any) => {
-        return document.clone(response.body.rs) as FileNetDocument;
-      })
-    );
-  }
+  
   deleteDocument(docId: string): Observable<boolean> {
     return this.obj.http.delete<boolean>(this.obj._getServiceURL() + '/document/' + docId, {
       params: new HttpParams({ fromObject: { taskId: `${this._model?.id}` } })
