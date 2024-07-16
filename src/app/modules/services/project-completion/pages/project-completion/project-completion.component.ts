9ab1e2a974a237b3b055a3ae3397d15b49e27683
Mamoun-Hamed <en15hamed1992@gmail.com>
2024-07-16 08:07:46 +0300

    Bug 63080: Project Complete: error appeared when open request by charity manager , prevent user form saving request

--- src/app/modules/services/project-completion/pages/project-completion/project-completion.component.ts
+++ src/app/modules/services/project-completion/pages/project-completion/project-completion.component.ts
@@ -302,14 +302,14 @@ export class ProjectCompletionComponent extends EServicesGenericComponent<Projec
     }
 
     if (this.openFrom === OpenFrom.USER_INBOX) {
-      if (this.employeeService.isExternalUser() && this.model.isReturned()) {
+      if (this.employeeService.isExternalUser()) {
         this.readonly = false;
       }
 
     } else if (this.openFrom === OpenFrom.TEAM_INBOX) {
       // after claim, consider it same as user inbox and use same condition
       if (this.model.taskDetails.isClaimed()) {
-        if (this.employeeService.isExternalUser() && this.model.isReturned()) {
+        if (this.employeeService.isExternalUser()) {
           this.readonly = false;
         }
 
