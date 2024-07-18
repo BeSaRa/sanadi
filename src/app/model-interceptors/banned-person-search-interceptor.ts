98e2b61dc8bd5dc5a122477039e5188f86dc6dfd
Mamoun-Hamed <en15hamed1992@gmail.com>
2024-07-18 13:02:50 +0300

    Bug 63344: Restricted: Request search: Search by date not working

--- src/app/model-interceptors/banned-person-search-interceptor.ts
+++ src/app/model-interceptors/banned-person-search-interceptor.ts
@@ -5,8 +5,8 @@ import { IMyDateModel } from '@nodro7/angular-mydatepicker';
 
 export class BannedPersonSearchInterceptor implements IModelInterceptor<BannedPersonSearch> {
     send(model: Partial<BannedPersonSearch>): Partial<BannedPersonSearch> {
-        model.dateFrom && (model.dateFrom = DateUtils.changeDateFromDatepicker(model.dateFrom as unknown as IMyDateModel)?.toISOString());
-        model.dateTo && (model.dateTo = DateUtils.changeDateFromDatepicker(model.dateTo as unknown as IMyDateModel)?.toISOString());
+        model.dateFrom && (model.dateFrom = DateUtils.setEndOfDay(model.dateFrom as unknown as IMyDateModel)?.toISOString());
+        model.dateTo && (model.dateTo = DateUtils.setEndOfDay(model.dateTo as unknown as IMyDateModel)?.toISOString());
         return model;
     }
     receive(model: BannedPersonSearch): BannedPersonSearch {
