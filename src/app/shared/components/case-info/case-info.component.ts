16d41ddae39779fcf6beedbe243df277ae214bc6
Mamoun-Hamed <en15hamed1992@gmail.com>
2024-07-22 12:20:27 +0300

    Bug 63085: Project Complete: · Page 9 Item 14: No project completion license document is issued.

--- src/app/shared/components/case-info/case-info.component.ts
+++ src/app/shared/components/case-info/case-info.component.ts
@@ -93,7 +93,7 @@ export class CaseInfoComponent implements OnInit {
     CaseTypes.PROJECT_IMPLEMENTATION,
     CaseTypes.FINANCIAL_TRANSFERS_LICENSING,
     CaseTypes.ORGANIZATION_ENTITIES_SUPPORT,
-    CaseTypes.PROJECT_COMPLETION,
+    // CaseTypes.PROJECT_COMPLETION,
   ];
 
   // this should be updated when ever you will add a new document service
