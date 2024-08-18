c3807bc48e7930e8519c02fedf443f3a94330a7d
Mamoun-Hamed <en15hamed1992@gmail.com>
2024-08-18 10:53:13 +0300

    add bulk acknowledge to risk level requests

--- src/app/services/risk-level-determination.service.ts
+++ src/app/services/risk-level-determination.service.ts
@@ -18,99 +18,102 @@ import { FactoryService } from "./factory.service";
 import { UrlService } from "./url.service";
 
 @CastResponseContainer({
-    $default: {
-      model: () => RiskLevelDetermination
-    },
-    $pagination: {
-      model: () => Pagination,
-      shape: { 'rs.*': () => RiskLevelDetermination }
-    }
-  })
-@Injectable({
-    providedIn:'root'
+  $default: {
+    model: () => RiskLevelDetermination
+  },
+  $pagination: {
+    model: () => Pagination,
+    shape: { 'rs.*': () => RiskLevelDetermination }
+  }
 })
-export class RiskLevelDeterminationService extends CrudWithDialogGenericService<RiskLevelDetermination>{
-    list: RiskLevelDetermination[] = [];
+@Injectable({
+  providedIn: 'root'
+})
+export class RiskLevelDeterminationService extends CrudWithDialogGenericService<RiskLevelDetermination> {
+  list: RiskLevelDetermination[] = [];
 
-    constructor(public http: HttpClient,
-      private urlService: UrlService,
-      public dialog: DialogService) {
-      super();
-      FactoryService.registerService('RiskLevelDeterminationService', this);
-    }
-  
-    _getModel(): new () => RiskLevelDetermination {
-      return RiskLevelDetermination;
-    }
-  
-    _getDialogComponent(): ComponentType<any> {
-      return RiskLevelDeterminationPopupComponent;
-    }
-  
-    _getServiceURL(): string {
-      return this.urlService.URLS.RISK_LEVEL_DETERMINATION;
-    }
-  
-    openViewDialog(modelId: number): Observable<DialogRef> {
-      return this.getByIdComposite(modelId).pipe(
-        switchMap((RiskLevelDetermination: RiskLevelDetermination) => {
-          return of(this.dialog.show<IDialogData<RiskLevelDetermination>>(RiskLevelDeterminationPopupComponent, {
-            model: RiskLevelDetermination,
-            operation: OperationTypes.VIEW
+  constructor(public http: HttpClient,
+    private urlService: UrlService,
+    public dialog: DialogService) {
+    super();
+    FactoryService.registerService('RiskLevelDeterminationService', this);
+  }
+
+  _getModel(): new () => RiskLevelDetermination {
+    return RiskLevelDetermination;
+  }
+
+  _getDialogComponent(): ComponentType<any> {
+    return RiskLevelDeterminationPopupComponent;
+  }
+
+  _getServiceURL(): string {
+    return this.urlService.URLS.RISK_LEVEL_DETERMINATION;
+  }
+
+  openViewDialog(modelId: number): Observable<DialogRef> {
+    return this.getByIdComposite(modelId).pipe(
+      switchMap((RiskLevelDetermination: RiskLevelDetermination) => {
+        return of(this.dialog.show<IDialogData<RiskLevelDetermination>>(RiskLevelDeterminationPopupComponent, {
+          model: RiskLevelDetermination,
+          operation: OperationTypes.VIEW
+        }));
+      })
+    );
+  }
+
+  @HasInterception
+  @CastResponse(undefined)
+  add(@InterceptParam() model: RiskLevelDetermination): Observable<RiskLevelDetermination> {
+    return this.http.post<RiskLevelDetermination>(this._getServiceURL() + '', model);
+  }
+  approve(id: number) {
+    return this.http.put<any>(this._getServiceURL() + '/request/' + id + '/approve', {});
+  }
+  reject(id: number) {
+    return this.http.put<any>(this._getServiceURL() + '/request/' + id + '/reject', {});
+  }
+  approveBulk(ids: number[]) {
+    return this.http.put<any>(this._getServiceURL() + '/request/approve/bulk', ids);
+  }
+  rejectBulk(ids: number[]) {
+    return this.http.put<any>(this._getServiceURL() + '/request/reject/bulk', ids);
+  }
+  return(requestId: number, comment: string) {
+    return this.http.put<any>(this._getServiceURL() + '/request/return', {
+      requestId,
+      comment
+    });
+  }
+  acknowledge(id: number) {
+    return this.http.put<any>(this._getServiceURL() + '/request/' + id + '/acknowledge', {});
+  }
+  acknowledgeBulk(ids: number[]) {
+    return this.http.put<any>(this._getServiceURL() + '/request/acknowledge/bulk', ids);
+  }
+
+  @CastResponse(undefined, {
+    fallback: '$default',
+    unwrap: 'rs'
+  })
+  getByRequestStatus(requestStatus: number) {
+    return this.http.get<RiskLevelDetermination[]>(this._getServiceURL() + '/request/request-status/' + requestStatus)
+  }
+  @CastResponse(undefined, {
+    fallback: '$default',
+    unwrap: 'rs'
+  })
+  getAudits(id: number): Observable<RiskLevelDetermination[]> {
+    return this.http.get<RiskLevelDetermination[]>(this._getServiceURL() + '/request/request-audit/' + id)
+  }
+  openAuditLogsDialog(id: number): Observable<DialogRef> {
+    return this.getAudits(id)
+      .pipe(
+        switchMap((logList: RiskLevelDetermination[]) => {
+          return of(this.dialog.show(RiskLevelDeterminationLogPopupComponent, {
+            logList
           }));
         })
       );
-    }
-  
-    @HasInterception
-    @CastResponse(undefined)
-    add(@InterceptParam() model: RiskLevelDetermination): Observable<RiskLevelDetermination> {
-      return this.http.post<RiskLevelDetermination>(this._getServiceURL() + '', model);
-    }
-    approve(id:number){
-      return this.http.put<any>(this._getServiceURL() + '/request/' + id + '/approve', {});
-    }
-    reject(id:number){
-      return this.http.put<any>(this._getServiceURL() + '/request/' + id + '/reject', {});
-    }
-    approveBulk(ids:number[]){
-      return this.http.put<any>(this._getServiceURL() + '/request/approve/bulk', ids);
-    }
-    rejectBulk(ids:number[]){
-      return this.http.put<any>(this._getServiceURL() + '/request/reject/bulk', ids);
-    }
-    return(requestId:number,comment:string){
-      return this.http.put<any>(this._getServiceURL() + '/request/return', {
-        requestId,
-        comment
-      });
-    }
-    acknowledge(){
-      return this.http.put<any>(this._getServiceURL() + '/request/acknowledge' , {});
-    }
-   
-    @CastResponse(undefined, {
-      fallback: '$default',
-      unwrap: 'rs'
-    })
-    getByRequestStatus(requestStatus:number){
-     return this.http.get<RiskLevelDetermination[]>(this._getServiceURL() + '/request/request-status/'+ requestStatus)
-    }
-    @CastResponse(undefined, {
-      fallback: '$default',
-      unwrap: 'rs'
-    })
-    getAudits(id:number):Observable<RiskLevelDetermination[]>{
-      return this.http.get<RiskLevelDetermination[]>(this._getServiceURL() + '/request/request-audit/'+ id)
-     }
-     openAuditLogsDialog(id: number): Observable<DialogRef> {
-      return this.getAudits(id)
-        .pipe(
-          switchMap((logList: RiskLevelDetermination[]) => {
-            return of(this.dialog.show(RiskLevelDeterminationLogPopupComponent, {
-              logList
-            }));
-          })
-        );
-    }
   }
+}
