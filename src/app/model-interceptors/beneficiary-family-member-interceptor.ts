1b75adb46383ee33d08bd2b82c6bdc927fbf9278
abdulaziz-eblacorp <ashahrour@eblacorp.com>
2024-10-13 13:23:33 +0300

    Bug 63756: Sanadi- need to remove from the family data the aid classification and aid type

--- src/app/model-interceptors/beneficiary-family-member-interceptor.ts
+++ src/app/model-interceptors/beneficiary-family-member-interceptor.ts
@@ -1,31 +1,42 @@
-import {IModelInterceptor} from '@app/interfaces/i-model-interceptor';
-import {BeneficiaryFamilyMember} from '@models/beneficiary-family-member';
-import {FactoryService} from '@services/factory.service';
-import {LookupService} from '@services/lookup.service';
-import {Lookup} from '@models/lookup';
+import { IModelInterceptor } from '@app/interfaces/i-model-interceptor';
+import { BeneficiaryFamilyMember } from '@models/beneficiary-family-member';
+import { FactoryService } from '@services/factory.service';
+import { LookupService } from '@services/lookup.service';
+import { Lookup } from '@models/lookup';
 
-export class BeneficiaryFamilyMemberInterceptor implements IModelInterceptor<BeneficiaryFamilyMember>{
+export class BeneficiaryFamilyMemberInterceptor
+  implements IModelInterceptor<BeneficiaryFamilyMember>
+{
   receive(model: BeneficiaryFamilyMember): BeneficiaryFamilyMember {
-    let lookupService = FactoryService.getService<LookupService>('LookupService');
-    model.primaryIdTypeInfo = lookupService.listByCategory.BenIdType.find(x => x.lookupKey === model.primaryIdType) || new Lookup();
-    model.relativeTypeInfo = lookupService.listByCategory.BenRequestorRelationType.find(x => x.lookupKey === model.relativeType) || new Lookup();
+    let lookupService =
+      FactoryService.getService<LookupService>('LookupService');
+    model.primaryIdTypeInfo =
+      lookupService.listByCategory.BenIdType.find(
+        (x) => x.lookupKey === model.primaryIdType
+      ) || new Lookup();
+    model.relativeTypeInfo =
+      lookupService.listByCategory.BenRequestorRelationType.find(
+        (x) => x.lookupKey === model.relativeType
+      ) || new Lookup();
 
     return model;
   }
 
-  send(model: Partial<BeneficiaryFamilyMember>): Partial<BeneficiaryFamilyMember> {
+  send(
+    model: Partial<BeneficiaryFamilyMember>
+  ): Partial<BeneficiaryFamilyMember> {
     BeneficiaryFamilyMemberInterceptor._deleteBeforeSend(model);
     return model;
   }
 
-  private static _deleteBeforeSend(model: Partial<BeneficiaryFamilyMember> | any): void {
+  private static _deleteBeforeSend(
+    model: Partial<BeneficiaryFamilyMember> | any
+  ): void {
     delete model.service;
     delete model.langService;
     delete model.searchFields;
     delete model.beneficiaryService;
     delete model.primaryIdTypeInfo;
     delete model.relativeTypeInfo;
-    delete model.aidLookupInfo;
-    delete model.aidLookupParentInfo;
   }
 }
