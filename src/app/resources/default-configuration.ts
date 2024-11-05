3da3a30883833570bbc68fc206ce36744ea58d6e
Mamoun-Hamed <en15hamed1992@gmail.com>
2024-11-05 19:44:05 +0300

    2.11.8

--- src/app/resources/default-configuration.ts
+++ src/app/resources/default-configuration.ts
@@ -4,7 +4,7 @@ import {ConfigurationMergingScope} from '@app/types/types';
 export const configurationMergingLevel: ConfigurationMergingScope = 'limited';
 
 export const defaultConfiguration: Partial<IAppConfig> = {
-  VERSION: 'v2.11.7',
+  VERSION: 'v2.11.8',
   PRIVATE_VERSION: '',
   API_VERSION: 'v1',
   BASE_ENVIRONMENT: 'BAW_UI',
